6b434461101992ce02cad1f2438eee55948193a3
Jack Parmer <jparmer09@gmail.com>
2022-03-11 09:41:02 -0500

    cleanup

--- PYTHON/WATCH/watch.py
+++ PYTHON/WATCH/watch.py
@@ -4,12 +4,19 @@ import numpy as np
 from plotly import plot
 from redis import Redis
 from rq import Queue
+from rq.job import Job
 
-queue = Queue(connection=Redis())
+from GENERATORS import *
+from TRANSFORMERS import *
+from VISORS import *
+
+from utils import PlotlyJSONEncoder
+
+conn = Redis()
+q = Queue(connection=conn)
 
 import sys
 sys.path.append('../FUNCTIONS/')
-from generators import *
 
 # Load React flow chart object from JSON file
 
@@ -17,7 +24,7 @@ f = open('../WATCH/fc.json')
 fc = json.loads(f.read())
 elems = fc['elements']
 
-# Now, we replicate the React Flow chart in Python's networkx
+# Replicate the React Flow chart in Python's networkx
 
 DG = nx.DiGraph()
 
@@ -29,6 +36,8 @@ for i in range(len(elems)):
         DG.add_node(i+1, pos=(el['position']['x'], el['position']['y']))
         elems[i]['index'] = i+1
         elems[i]['label'] = el['id'].split('-')[0]
+
+pos = nx.get_node_attributes(DG,'pos')
 
 # Add edges to networkx directed graph
 
@@ -52,7 +61,19 @@ for i in range(len(elems)):
         e = get_tuple(el)
         DG.add_edge(*e)
 
-# Add labls and data to each node object
+# Add labels (commands) to networkx nodes
+
+labels = {}
+
+for el in elems:
+    # if element is not a node
+    if 'source' not in el:
+        labels[el['index']] = el['data']['label']
+                
+nx.set_node_attributes(DG, labels, 'cmd')
+nx.draw(DG, pos, with_labels=True, labels = labels)
+
+# Add labels and data to each node object
 
 def get_node_data_by_id():
     nodes_by_id = dict()
@@ -61,12 +82,66 @@ def get_node_data_by_id():
             nodes_by_id[n] = nd
     return nodes_by_id
 
-nodes_by_id = get_node_data_by_id()
-
 # Traverse tree in reverse topological sort 
 # and add jobs to Redis queue
 
-for n in nx.topological_sort(DG):
-    print('Adding to queue:', nodes_by_id[n])
-    for p in DG.predecessors(n):
-        print(DG.nodes[p]['cmd'], 'is a predecessor to', DG.nodes[n]['cmd'])
+topological_sorting = nx.topological_sort(DG)
+nodes_by_id = get_node_data_by_id()
+
+print(nodes_by_id)
+
+for n in topological_sorting:
+
+    cmd = nodes_by_id[n]['cmd']
+    ctrls = dict()
+
+    if cmd.replace('.','',1).isdigit():
+        ctrls['constant'] = cmd
+        cmd = 'CONSTANT'   
+    
+    func = getattr(globals()[cmd], cmd)
+    job_id = 'job_id_{0}'.format(n)
+
+    print('>>> visiting node *** {0} *** ({1})'.format(cmd, n))
+    print('Queueing function... ...', func)
+    
+    node_predecessors = DG.predecessors(n)
+    
+    if len(list(node_predecessors)) == 0:
+        print ('{0} ({1}) has no predecessors'.format(cmd, n))
+        q.enqueue(func, job_id = job_id, kwargs={'ctrls': ctrls})
+    else:
+        previous_job_ids = []
+        for p in DG.predecessors(n):
+            prev_cmd = DG.nodes[p]['cmd']
+            prev_job_id = 'job_id_{0}'.format(p)
+            previous_job_ids.append(prev_job_id)
+            print(prev_cmd, 'is a predecessor to', cmd)
+        q.enqueue(func,
+            job_id = job_id,
+            kwargs={'ctrls': ctrls, 'previous_job_ids': previous_job_ids},            
+            depends_on = previous_job_ids)
+
+# Get the end node results
+
+end_nodes = [x for x in DG.nodes() if DG.out_degree(x)==0 and DG.in_degree(x)==1]
+
+print(end_nodes)
+
+end_node_results = []
+
+for n in end_nodes:
+    job_id = 'job_id_{0}'.format(n)
+    nd = get_node_data_by_id()[n]
+    print(nd)
+    job = Job.fetch(job_id, connection=Redis())
+    payload = job.result
+    print(str(payload['data'][0]['type']))
+    end_node_results.append({nd['cmd']: payload})
+    
+# Write the end node results to file
+
+f = open("results.json", "w")
+f.write(json.dumps(end_node_results, cls=PlotlyJSONEncoder))
+f.close()
+
