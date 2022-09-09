387e75c4074af71d06617304aa5fbb8f4a6dc1fa
Md Rashedul Alam Anik <rashedulalam112112@gmail.com>
2022-09-10 00:45:58 +0600

    Rename type scatter to scatter3d in SCATTER.py

--- PYTHON/FUNCTIONS/VISORS/SCATTER3D.py
+++ PYTHON/FUNCTIONS/VISORS/SCATTER3D.py
@@ -10,7 +10,7 @@ def SCATTER3D(**kwargs):
             x = list(payload['x0']),
             y = list(payload['y0']),
             z = list(payload['z0']) if 'z0' in payload else list([0] * len(payload['x0'])),
-            type='scatter',
+            type='scatter3d',
             mode='markers'
         )],
         layout = dict(template = init_template())
