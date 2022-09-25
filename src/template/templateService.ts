ed15f98a048aaf91797ce535daaa0fe5b9aeca34
dev-nested <dev.nested@gmail.com>
2022-09-25 16:43:18 -0500

    add a sample component structure

---
+++ src/template/templateService.ts
@@ -0,0 +1,21 @@
+import {SomeObj} from './types'
+
+export function getSome(id: string) {
+    // axios calls
+}
+
+export function putSome(someObj: SomeObj) {
+    // axios calls
+}
+
+export function deleteSome(id: string) {
+    // axios calls
+}
+
+export function updateSomePropertyA(someObj) {
+    // axios calls
+}
+
+export function updateSomePropertyB(someObj) {
+    // axios calls
+}
---
+++ src/template/views/subView2.tsx
@@ -0,0 +1,6 @@
+export function SubView2 ({result}) {
+    return (
+        <>
+        </>
+    )
+}
