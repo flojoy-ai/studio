3b1c606f5e842f1407198ae6e34ec8f5af5eb0d5
Shahad Ishraq <shahad@shahad-nuc.speedport.ip>
2022-09-30 10:06:03 +0200

    Reduce ping frequency

--- src/App.js
+++ src/App.js
@@ -75,17 +75,17 @@ const App = () => {
               if (res.msg === STATUS_CODES["RQ_RUN_COMPLETE"]) {
                 // grab program result from redis
                 setServerStatus(STATUS_CODES["RQ_RUN_COMPLETE"]);
-                console.log(STATUS_CODES["RQ_RUN_COMPLETE"]);
+                // console.log(STATUS_CODES["RQ_RUN_COMPLETE"]);
                 pingBackendAPI("/io").then((res) => {
-                  console.log("io", res);
+                  // console.log("io", res);
                   if (res.msg === STATUS_CODES["MISSING_RQ_RESULTS"]) {
                     setServerStatus(res.msg);
                   } else {
                     setServerStatus(STATUS_CODES["RQ_RESULTS_RETURNED"]);
-                    console.log("setting results state", res);
+                    // console.log("setting results state", res);
                     setProgramResults(res);
 
-                    console.warn("new program results", res);
+                    // console.warn("new program results", res);
                   }
                 });
               } else if (res.msg !== undefined && res.msg !== "") {
@@ -93,7 +93,7 @@ const App = () => {
                 setServerStatus(res.msg);
               }
             });
-          }, 100);
+          }, 1000);
         } else {
           setServerStatus(STATUS_CODES["SERVER_OFFLINE"]);
         }
