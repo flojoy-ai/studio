759ae6bb982df917bafff15fdb37816ded36dac6
Sifat Ibna Amin <sifatibna.amin9@gmail.com>
2022-10-07 20:11:32 +0600

    modified the cypress config file

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,14 +3,14 @@ import { addMatchImageSnapshotPlugin } from 'cypress-image-snapshot/plugin';
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:3000",
+    baseUrl: "http://localhost:3000/",
     video: false,
     setupNodeEvents(on, config) {
       addMatchImageSnapshotPlugin(on, config);
     },
     requestTimeout : 30000,
     numTestsKeptInMemory: 0,
-    responseTimeout : 50000,
+    responseTimeout : 120e3,
     pageLoadTimeout: 100000
   },
 });
