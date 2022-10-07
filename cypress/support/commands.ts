bc439262e544a227fc184bb427ca8aabc5be76f4
Sifat Ibna Amin <sifatibna.amin9@gmail.com>
2022-10-08 00:58:18 +0600

    modified the params of addMatchImageSnapshotCommand to overcome mismatch size

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -41,6 +41,7 @@ import 'cypress-file-upload';
 import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
 
 addMatchImageSnapshotCommand({
+    allowSizeMismatch: true,
     failureThreshold: 0.03, // threshold for entire image
     failureThresholdType: 'percent', // percent of image or number of pixels
     customDiffConfig: { threshold: 0.1 }, // threshold for each pixel
