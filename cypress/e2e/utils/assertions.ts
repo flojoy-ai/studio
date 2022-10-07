b58e4873d09c905819b4569c50450d9034a9a001
Sifat Ibna Amin <sifatibna.amin9@gmail.com>
2022-10-08 00:11:47 +0600

    added params for addMatchImageSnapshotcommand and matchImageSnapshot

--- cypress/e2e/utils/assertions.ts
+++ cypress/e2e/utils/assertions.ts
@@ -1,6 +1,11 @@
 const matchPlotSnapshot = (selector: string, name: string) => {
   return cy.waitUntil(function() {
-    return cy.get(selector).should('be.visible').matchImageSnapshot(name);
+    return cy.get(selector).should('be.visible').matchImageSnapshot(name,{
+      failureThreshold: 0.03, // threshold for entire image
+      failureThresholdType: 'percent', // percent of image or number of pixels
+      customDiffConfig: { threshold: 0.1 }, // threshold for each pixel
+      capture: 'viewport', // capture viewport in screenshot
+    });
   }, { timeout: 1000 });
 }
 
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -40,8 +40,13 @@ import 'cypress-wait-until';
 import 'cypress-file-upload';
 import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
 
-addMatchImageSnapshotCommand();
+addMatchImageSnapshotCommand({
+    failureThreshold: 0.03, // threshold for entire image
+    failureThresholdType: 'percent', // percent of image or number of pixels
+    customDiffConfig: { threshold: 0.1 }, // threshold for each pixel
+    capture: 'viewport', // capture viewport in screenshot
+  });
 
 Cypress.Commands.add('visitHomepage', ()=>{
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:3000/')
 })
