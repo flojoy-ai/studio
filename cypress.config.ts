49d1189aeb5f26b9751391ab53e797532ccd53a1
Syed Mahbub <xsmahmed776@gmail.com>
2022-11-04 03:35:35 +0600

    remove cypress snapshot plugin

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from "cypress";
-import { addMatchImageSnapshotPlugin } from 'cypress-image-snapshot/plugin';
 
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000/",
     video: false,
-    setupNodeEvents(on, config) {
-      addMatchImageSnapshotPlugin(on, config);
-    },
     requestTimeout : 30000,
     numTestsKeptInMemory: 0,
     responseTimeout : 120e3,
