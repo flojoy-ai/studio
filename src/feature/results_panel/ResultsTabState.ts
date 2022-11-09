85be8ca5fdde8ad90e8a45b38dab5ec60e69bb03
azmainadel <azmainadel47@gmail.com>
2022-11-09 21:00:44 +0600

    :zap: Update ResultTabState

--- src/feature/results_panel/ResultsTabState.ts
+++ src/feature/results_panel/ResultsTabState.ts
@@ -1,7 +1,15 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
+import { useWindowSize } from "react-use";
+import { useFlowChartState } from "../../hooks/useFlowChartState";
 
 export function useTemplateState() {
+  const { width: windowWidth } = useWindowSize();
+  const { rfInstance } = useFlowChartState();
   const [resultElements, setResultElements] = useState<any[]>([]);
+  const nodeResults = useMemo(
+    () => ("io" in results ? JSON.parse(results.io).reverse() : []),
+    [results]
+  );
   const [a, setA] = useState<string>('');
   const [result, setResult, isLoading] = useReactQuery();
 
