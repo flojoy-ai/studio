0d17d03926ebd9abb839edf582998c2acf57182e
Sakib Abrar <sakib.abrar@optimizely.com>
2022-11-12 03:37:33 +0600

    Add test socket connections and response

--- project/consumers.py
+++ project/consumers.py
@@ -1,21 +1,43 @@
 import json
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
-class FlojoyConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
+
+class FlojoyConsumer(AsyncJsonWebsocketConsumer):
+    
+    async def connect(self):
+        await self.accept()
         
-        self.send(text_data=json.dumps({
+        await self.send_json({
             'type': 'connection_established',
-            'message': 'You are now connected to flojoy server'
-        }))
-        
-    def receive(self, text_data):
+            'message': 'You are now connected to flojoy servers'
+        })
+
+    async def receive(self, text_data=None, bytes_data=None, **kwargs):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+        print(text_data_json)
+        if text_data_json['type'] == 'ping':
+            await self.send_json({
+                'type': 'pong',
+                'message': 'You are now getting pinged'
+            })
+    
+    # def connect(self):
+    #     self.accept()
         
-        print('Message:', message)
+    #     self.send(text_data=json.dumps({
+    #         'type': 'connection_established',
+    #         'message': 'You are now connected to flojoy server'
+    #     }))
+    
+    # def receive(self, text_data):
+    #     text_data_json = json.loads(text_data)
+    #     message = text_data_json['message']
         
-        self.send(text_data=json.dumps({
-            'type': 'chat', 
-        }))
+    #     print('Message:', message)
+        
+    #     self.send(text_data=json.dumps({
+    #         'type': 'chat', 
+    #     }))
+        
+    # def disconnect(self, code):
+    #     pass
