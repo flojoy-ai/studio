1bde20ae791c3b8135213fa9fa5a2e676676b40f
Sifat Ibna Amin <sifatibna.amin9@gmail.com>
2022-11-12 17:07:54 +0600

    Conditional WIP: jobs failing unexpectedly (bug)

--- PYTHON/FUNCTIONS/VISORS/HISTOGRAM.py
+++ PYTHON/FUNCTIONS/VISORS/HISTOGRAM.py
@@ -2,8 +2,16 @@ from .VCTR import fetch_inputs
 from .template import init_template
 
 def HISTOGRAM(**kwargs):
-    previous_job_results = fetch_inputs(kwargs['previous_job_ids'])
-    payload = previous_job_results[0]
+
+    if 'previous_job_ids' in kwargs:
+        previous_job_ids = kwargs['previous_job_ids']
+        if len(previous_job_ids) >0:
+            previous_job_results = fetch_inputs(kwargs['previous_job_ids'])
+            payload = previous_job_results[0]
+    if 'special_type_node_result' in kwargs:
+        special_type_node_result = kwargs['special_type_node_result']
+        if special_type_node_result is not None:
+            payload = kwargs['special_type_node_result']
 
     fig = dict(
         data = [dict(
