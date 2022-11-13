4521e7ad85923c9c49f2eb03361ab63fdd8541b6
azmainadel <azmainadel47@gmail.com>
2022-11-13 10:13:09 +0600

    🧪 #73 Add basic rendering test for ControlsTab

--- src/feature/controls_panel/ControlsTab.tsx
+++ src/feature/controls_panel/ControlsTab.tsx
@@ -171,7 +171,7 @@ const ControlsTab = ({ results, theme, setOpenCtrlModal, openCtrlModal }) => {
   }, [rfInstance]);
 
   return (
-    <div>
+    <div data-testid="controls-tab">
       {/* <SampleRGL/> */}
       <ControlGrid
         controlProps={{
---
+++ src/feature/controls_panel/test/ControlsTab.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import ControlsTab from '../ControlsTab'
+
+describe('ControlsTab', () => {
+    it('should render the controls tab', () => {
+        render(<ControlsTab
+            results={{}}
+            theme='dark'
+            setOpenCtrlModal={{}}
+            openCtrlModal={false}
+        />);
+
+        const controlTab = screen.getByTestId(/controls-tab/i);
+        expect(controlTab).toBeInTheDocument();
+    })
+});
