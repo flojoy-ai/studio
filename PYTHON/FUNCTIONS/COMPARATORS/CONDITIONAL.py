1ef3ee9da88a96de63797d1800706f7c08db7eb8
SifatIbna <sifatibna.amin9@gmail.com>
2022-11-16 22:45:45 +0600

    CONDITIONAL Feature inegrated with Scheduler (test)

--- PYTHON/FUNCTIONS/COMPARATORS/CONDITIONAL.py
+++ PYTHON/FUNCTIONS/COMPARATORS/CONDITIONAL.py
@@ -3,22 +3,29 @@ from .VCTR import fetch_inputs
 
 def CONDITIONAL(**kwargs):
     ''' Add 2 input vectors and return the result '''
-    previous_job_result = kwargs['previous_job_ids']
-    previous_job_result = fetch_inputs(previous_job_ids)
+    previous_job_ids = kwargs['previous_job_ids']
+    previous_job_results = fetch_inputs(previous_job_ids)
 
-    print('JOB RESULT 1', previous_job_result[0])
-    print('JOB RESULT 2', previous_job_result[1])
+    operator = ""
 
-    operator = kwargs['ctrls']['CONDITIONAL_OPERATOR']['OPERATOR']
-    if operator == null:
+    if 'ctrls' in kwargs:
+        ctrls = kwargs['ctrls']
+        for key, input in ctrls.items():
+            operator = input['operator']
+
+    print(operator)
+
+    if operator == None:
         raise Exception("OPERATOR NULL")
     else:
         if operator == 'IS_GREATER_THAN':
+            return {'x0':previous_job_results[0]['x0'],'y0':previous_job_results[0]['y0'],'direction':'true'}
 
-
+            '''
             if len(previous_job_result) == 2:
                 pass
             elif len(previous_job_result) == 1:
                 pass
             else:
                 raise Exception("Only Two Input at a time is acceptable")
+            '''
--- PYTHON/FUNCTIONS/VISORS/HISTOGRAM.py
+++ PYTHON/FUNCTIONS/VISORS/HISTOGRAM.py
@@ -5,13 +5,9 @@ def HISTOGRAM(**kwargs):
 
     if 'previous_job_ids' in kwargs:
         previous_job_ids = kwargs['previous_job_ids']
-        if len(previous_job_ids) >0:
-            previous_job_results = fetch_inputs(kwargs['previous_job_ids'])
-            payload = previous_job_results[0]
-    if 'special_type_node_result' in kwargs:
-        special_type_node_result = kwargs['special_type_node_result']
-        if special_type_node_result is not None:
-            payload = kwargs['special_type_node_result']
+
+        previous_job_results = fetch_inputs(previous_job_ids)
+        payload = previous_job_results[0]
 
     fig = dict(
         data = [dict(
