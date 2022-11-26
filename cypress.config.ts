85d3b144e7593e95bb41e46fd554d05dc7e39391
Sadman Yasar Ridit <sadmanyasarridit@Sadmans-MacBook-Air.local>
2022-11-26 20:06:48 +0600

    [modify] cypress configs

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,9 +3,13 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     video : false,
-    baseUrl: "http://localhost:3000",
+    baseUrl: "http://localhost:3000/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    requestTimeout : 30000,
+    numTestsKeptInMemory: 0,
+    responseTimeout : 120e3,
+    pageLoadTimeout: 100000
   },
 });
