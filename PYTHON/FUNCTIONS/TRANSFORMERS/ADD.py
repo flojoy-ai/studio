8406a616b2938e8ef26f7da7f850e590b9e47e6a
azmainadel <azmainadel47@gmail.com>
2022-12-06 15:21:20 +0600

    :zap: Update backend function for ADD nodes

--- PYTHON/FUNCTIONS/TRANSFORMERS/ADD.py
+++ PYTHON/FUNCTIONS/TRANSFORMERS/ADD.py
@@ -4,13 +4,13 @@ from joyflo import flojoy, VectorXY
 @flojoy
 def ADD(v, params):
     ''' Add 2 input vectors and return the result '''
-    a = [0]
-    b = [0]
+    x0 = [0]
+    x1 = [0]
     
     if len(v) == 2:
-        a = v[0].y
-        b = v[1]['y']
+        x0 = v[0].y
+        x1 = v[1].y
         
-    y = np.add(a, b)
+    y = np.add(x0, x1)
 
-    return VectorXY(x = [a, b], y = y)
+    return VectorXY(x = {x0, x1}, y = y)
