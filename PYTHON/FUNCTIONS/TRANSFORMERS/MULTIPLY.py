2f0fff36d6be40a9be4fdd729ef86ecd3cc95d38
azmainadel <azmainadel47@gmail.com>
2022-12-11 22:42:09 +0600

    :bug: Remove debug print call

--- PYTHON/FUNCTIONS/TRANSFORMERS/MULTIPLY.py
+++ PYTHON/FUNCTIONS/TRANSFORMERS/MULTIPLY.py
@@ -4,7 +4,6 @@ from joyflo import flojoy, VectorXY
 @flojoy
 def MULTIPLY(v, params):
     ''' Takes 2 input vectors, multiplies them, and returns the result '''
-    print(v)
     x0 = v[0].y
     x1 = v[1].y
 
