5046047e500b65e78cfa57939bacf31a65956198
Syed Mahbub <xsmahmed776@gmail.com>
2023-01-07 02:38:10 +0600

    return static result from Rand node

--- PYTHON/FUNCTIONS/GENERATORS/RAND.py
+++ PYTHON/FUNCTIONS/GENERATORS/RAND.py
@@ -8,7 +8,10 @@ def RAND(v, params):
     x = None
     if len(v) > 0:
         x = v[0].y
-        y = np.random.normal(size=len(x))
+        # For now return fixed value for y key
+        # Later on, A env variable will be used to return fixed value for testing
+        y = x
+        # y = np.random.normal(size=len(x))
     else:
         y = np.random.normal(size=1000)
 
