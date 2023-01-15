47d744d4c8d8a97b2043a29c5367e243231dd2e7
SifatIbna <sifatibna.amin9@gmail.com>
2023-01-15 22:56:16 +0600

    removed mocks folder & added spy on localforage to spy on parameters

--- src/__mocks__/localforage.ts
+++
@@ -1,9 +0,0 @@
-import {expect, jest, test} from '@jest/globals';
-
-const mock = {
-    setItem: ()=> "hello",
-    getItem: () => "data",
-    removeItem: () => jest.fn(),
-};
-
-export default mock;
