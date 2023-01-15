410d8c2c11b892fd433b22e5e1d07a50e7327355
SifatIbna <sifatibna.amin9@gmail.com>
2023-01-16 01:35:48 +0600

    flow chart services unit testing done with mock api & spy

--- src/tests/services/FlowChartServices.test.tsx
+++ src/tests/services/FlowChartServices.test.tsx
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom'
 import { expect, jest, test } from '@jest/globals';
-import { saveFlowChartToLocalStorage } from '../../services/FlowChartServices';
-import localforage from "localforage";
+import { saveFlowChartToLocalStorage, saveAndRunFlowChartInServer } from '../../services/FlowChartServices';
+import localforage, { iterate } from "localforage";
 
 jest.mock('localforage', () => ({
   setItem: jest.fn(),
@@ -30,4 +30,60 @@ describe("FlowChartServices", () => {
     expect(localforage.setItem).toHaveBeenCalledWith(key, obj)
     expect(localforage.getItem(key)).toEqual("data")
   });
+
+  it('calling fetch api with success', async () => {
+
+    global.fetch = jest.fn(url => Promise.resolve({
+      ok: true,
+      status: 200,
+      json: () => Promise.resolve({})
+    })) as any;
+
+    const obj: any = {
+      "elements": "fake"
+    }
+
+    const jobsetId: string = "random"
+
+    const param: any = {
+      rfInstance: obj,
+      jobsetId: jobsetId
+    }
+
+    const fetchParams = {
+      "body": "{\"fc\":\"{\\\"elements\\\":\\\"fake\\\"}\",\"cancelExistingJobs\":true}",
+      headers: { "Content-type": "application/json; charset=UTF-8" },
+      method: "POST",
+    }
+
+    const data = await saveAndRunFlowChartInServer(param)
+
+    const fetchSpy = jest.spyOn(global, "fetch")
+
+    expect(data).toEqual({})
+    expect(fetchSpy).toHaveBeenCalled()
+    expect(fetchSpy).toHaveBeenCalledWith("/wfc", fetchParams)
+  });
+
+  it("calling fetch api with error message", async ()=>{
+
+    const fetchMock = jest
+			.spyOn(global, 'fetch')
+			.mockImplementation(() => Promise.reject({status:false }) as any)
+
+    const obj: any = {
+      "elements": "fake"
+    }
+
+    const jobsetId: string = "random"
+
+    const param: any = {
+      rfInstance: obj,
+      jobsetId: jobsetId
+    }
+
+    expect(fetchMock).toBeCalled()
+    await expect(saveAndRunFlowChartInServer(param)).rejects.toEqual({status:false});
+
+  })
 })
