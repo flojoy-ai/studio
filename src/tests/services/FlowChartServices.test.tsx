c6ca76bb9647ea612e81acee758fd55c00ba0e9b
SifatIbna <sifatibna.amin9@gmail.com>
2023-01-18 02:19:42 +0600

    added private functions

--- src/tests/services/FlowChartServices.test.tsx
+++ src/tests/services/FlowChartServices.test.tsx
@@ -31,11 +31,7 @@ describe("FlowChartServices", () => {
 
       const key:string = "flow-joy"
 
-      const value: any = {
-        "elements": "fake"
-      }
-
-      saveFlowChartToLocalStorage(value)
+      callSaveFlowChartToLocalStorageFunc()
 
       expect(localforage.getItem(key)).toEqual("data")
 
@@ -43,14 +39,13 @@ describe("FlowChartServices", () => {
 
     it('checks the setItem function parameter by setting spy on the localforage', () => {
       const key:string = "flow-joy"
-
       const value: any = {
         "elements": "fake"
       }
 
       const setItemSpy = jest.spyOn(localforage, 'setItem');
 
-      saveFlowChartToLocalStorage(value)
+      callSaveFlowChartToLocalStorageFunc()
 
       expect(localforage.setItem).toHaveBeenCalledWith(key, value)
     });
@@ -59,26 +54,13 @@ describe("FlowChartServices", () => {
   describe("saveAndRunFlowChartInServer", ()=>{
 
     it("calls /wfc api endpoint using fetch successfully & matches the response with test reponse message",async ()=>{
-      const param: any = {
-        rfInstance: {
-          "elements": "test"
-        },
-        jobsetId: "random"
-      }
 
-      const data = await saveAndRunFlowChartInServer(param)
+      const data = await callSaveAndRunFlowChartInServerFunc()
 
       expect(data).toEqual({})
     })
 
-    it('checks parameters of the api by setting spy on fetch method', async () => {
-
-      const param: any = {
-        rfInstance: {
-          "elements": "test"
-        },
-        jobsetId: "random"
-      }
+    it('checks parameters of the api by setting spy on fetch method',async () => {
       const fetchParams: any = {
         "body": "{\"fc\":\"{\\\"elements\\\":\\\"test\\\"}\",\"cancelExistingJobs\":true}",
         headers: { "Content-type": "application/json; charset=UTF-8" },
@@ -87,19 +69,12 @@ describe("FlowChartServices", () => {
       const api_endPoint: string = "/wfc"
 
       const fetchSpy = jest.spyOn(global, "fetch")
-      await saveAndRunFlowChartInServer(param)
+      callSaveAndRunFlowChartInServerFunc()
 
       expect(fetchSpy).toHaveBeenCalledWith(api_endPoint, fetchParams)
     });
 
-    it("checks the response of unsuccessful api calling using fetch method with test response", async ()=>{
-
-      const param: any = {
-        rfInstance: {
-          "elements": "fake"
-        },
-        jobsetId: "random"
-      }
+    it("checks the response of unsuccessful api calling using fetch method with test response", async()=>{
       const testResponse = {ok:false,status:404 }
 
       jest
@@ -107,7 +82,7 @@ describe("FlowChartServices", () => {
         .mockImplementation(() => Promise.reject(testResponse) as any)
 
       try{
-        await saveAndRunFlowChartInServer(param)
+        await callSaveAndRunFlowChartInServerFunc()
       } catch(error){
         expect(error).toEqual(testResponse) //https://jestjs.io/docs/tutorial-async#error-handling
       }
@@ -118,3 +93,23 @@ describe("FlowChartServices", () => {
   })
 
 })
+
+const callSaveFlowChartToLocalStorageFunc = () =>{
+  const value: any = {
+    "elements": "fake"
+  }
+
+  saveFlowChartToLocalStorage(value)
+}
+
+const callSaveAndRunFlowChartInServerFunc = async ()=>{
+  const param: any = {
+    rfInstance: {
+      "elements": "test"
+    },
+    jobsetId: "random"
+  }
+
+  const data = await saveAndRunFlowChartInServer(param)
+  return data
+}
