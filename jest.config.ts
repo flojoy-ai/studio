752d8216fa90dd48ef5b930413ea2355c0f9719d
SifatIbna <sifatibna.amin9@gmail.com>
2023-01-19 02:56:41 +0600

    modified configs & added new scripts for jest watch

--- jest.config.ts
+++ jest.config.ts
@@ -1,25 +1,31 @@
 import type { Config } from "@jest/types";
 export default async (): Promise<Config.InitialOptions> => {
-    return {
-        preset: "ts-jest",
-        displayName: {
-            name: "flojoy unit-testing",
-            color: "greenBright",
-        },
-        moduleNameMapper: {
-            '^.+\\.(css|less)$': '<rootDir>/config/CSStub.js'
-        },
-        verbose: true,
-        setupFiles: ["dotenv/config"],
-        testMatch: ["**/**/*.test.{ts,tsx}"],
-        testEnvironment: "jsdom",
-        detectOpenHandles: true,
-        collectCoverage: true,
-        transform: {
-            "^.+\\.tsx?$": "ts-jest",
-            '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-                '<rootDir>/config/fileTransformer.js',
-        },
-        forceExit: true,
-    };
+  return {
+    preset: "ts-jest",
+    displayName: {
+      name: "flojoy unit-testing",
+      color: "greenBright",
+    },
+    moduleNameMapper: {
+      "^.+\\.(css|less)$": "<rootDir>/config/CSStub.js",
+    },
+    verbose: true,
+    setupFiles: ["dotenv/config"],
+    testMatch: ["**/**/*.test.{ts,tsx,js}"],
+    testEnvironment: "jsdom",
+    detectOpenHandles: true,
+    collectCoverage: true,
+    transform: {
+      "^.+\\.tsx?$": "ts-jest",
+      "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+        "<rootDir>/config/fileTransformer.js",
+    },
+    forceExit: true,
+    clearMocks: true,
+    coverageThreshold: {
+      global: {
+        lines: 90,
+      },
+    },
+  };
 };
