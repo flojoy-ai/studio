85de0726bfcebd3494b3148c1e445fb23e90412f
SifatIbna <sifatibna.amin9@gmail.com>
2023-01-29 10:05:01 +0600

    added module name mapper to avoid the es6 import errors

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,6 @@
 import type { Config } from "@jest/types";
+import { pathsToModuleNameMapper } from "ts-jest";
+import { compilerOptions } from "./tsconfig.json";
 export default async (): Promise<Config.InitialOptions> => {
   return {
     preset: "ts-jest",
@@ -19,5 +21,8 @@ export default async (): Promise<Config.InitialOptions> => {
         lines: 0, // TODO: increamentially increase coverage threshold
       },
     },
+    roots: ["<rootDir>"],
+    modulePaths: [compilerOptions.baseUrl],
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
   };
 };
