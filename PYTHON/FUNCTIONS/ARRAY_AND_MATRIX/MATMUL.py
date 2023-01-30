5fa63be016dbce6138ea90effe111e73a0a5e1cb
Tristan Britt <tristan.britt@mail.mcgill.ca>
2023-01-30 17:27:35 -0500

    misc edits

--- PYTHON/FUNCTIONS/ARRAY_AND_MATRIX/MATMUL.py
+++ PYTHON/FUNCTIONS/ARRAY_AND_MATRIX/MATMUL.py
@@ -4,10 +4,7 @@ from flojoy import flojoy, DataContainer
 @flojoy
 def MATMUL(v, params):
     ''' Takes 2 input matrices, multiplies them, and returns the result '''
-    a = np.zeros(3)
-    b = np.zeros(3)
-    if len(v) == 2:
-        a = v[0].y
-        b = v[1]['y']
+    a = v[0].y
+    b = v[1].y
     y = np.matmul(a, b)
     return DataContainer(x=[a, b], y=y)
--- PYTHON/FUNCTIONS/ARRAY_AND_MATRIX/__init__.py
+++ PYTHON/FUNCTIONS/ARRAY_AND_MATRIX/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ["MATMUL"]
+__all__= ["MATMUL"]
