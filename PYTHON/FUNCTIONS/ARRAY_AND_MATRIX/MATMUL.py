ea0af045e2999adb587dbaecf221b6ab54e9492a
Tristan Britt <tristan.britt@mail.mcgill.ca>
2023-01-30 17:25:08 -0500

    name change

--- PYTHON/FUNCTIONS/ARRAY_AND_MATRIX/MATMUL.py
+++ PYTHON/FUNCTIONS/ARRAY_AND_MATRIX/MATMUL.py
@@ -4,7 +4,10 @@ from flojoy import flojoy, DataContainer
 @flojoy
 def MATMUL(v, params):
     ''' Takes 2 input matrices, multiplies them, and returns the result '''
-    a = v[0].y
-    b = v[1].y
+    a = np.zeros(3)
+    b = np.zeros(3)
+    if len(v) == 2:
+        a = v[0].y
+        b = v[1]['y']
     y = np.matmul(a, b)
     return DataContainer(x=[a, b], y=y)
--- jsonify_funk.py
+++ jsonify_funk.py
@@ -2,7 +2,7 @@ import json
 from os import listdir
 from os.path import isfile, join
 
-dirs = ['SIMULATIONS', 'TRANSFORMERS', 'VISORS','CONDITIONALS','LOOPS','TIMERS','ARRAY_MATRIX']
+dirs = ['SIMULATIONS', 'TRANSFORMERS', 'VISORS','CONDITIONALS','LOOPS','TIMERS','ARRAY_AND_MATRIX']
 
 path = 'PYTHON/FUNCTIONS'
 
