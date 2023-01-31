745943d863d55ec266fd0a8f033bdd14718eac29
Jack Parmer <jparmer09@gmail.com>
2023-01-30 22:56:10 -0500

    add plotly layout type

--- src/feature/common/defaultPlotLayout.ts
+++ src/feature/common/defaultPlotLayout.ts
@@ -1,14 +1,16 @@
+import { Layout } from "plotly.js";
+
 const styledPlotLayout = (theme: "light" | "dark") => {
   const plotFeatureColor = theme === "light" ? "#282c34" : "#fff";
   const plotBackgroundColor = theme === "light" ? "#fff" : "#282c34";
 
-  const dfltLayout = {
+  const dfltLayout:Partial<Layout> = { = {
     paper_bgcolor: "rgba(0,0,0,0)",
     plot_bgcolor: plotBackgroundColor,
     autosize: true,
     font: { color: plotFeatureColor },
     margin: { t: 40, r: 40, b: 40, l: 40 },
-    xaxis: {zeroline: false},
+    xaxis: {zeroline: false, type: "linear"},
     // yaxis: {zeroline: false, color: plotFeatureColor}
   };
 
