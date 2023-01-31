4f8b00b8a35afbecb052dc2a52a679581c56dd8f
Tristan Britt <tristan.britt@mail.mcgill.ca>
2023-01-31 15:24:29 -0500

    proper type control for inversion

--- PYTHON/FUNCTIONS/ARRAY_AND_MATRIX/INVERT.py
+++ PYTHON/FUNCTIONS/ARRAY_AND_MATRIX/INVERT.py
@@ -4,19 +4,27 @@ from flojoy import flojoy, DataContainer
 @flojoy
 def INVERT(v, params):
     ''' Takes 2 inputs, one matrix and one rcond if not square matrix.
-    Inverts them (or pseudo invert) and returns the result '''
+    Inverts them (or pseudo invert) and returns the result. 
+    If the entered value is a scalar it returns the multiplciative
+    inverse 1/x '''
+    print(f'INVERT params: {params}')
     a = np.eye(3)
-    b = 1.0
-    if len(v) == 2:
+    if v.__len__ >0:
         a = v[0].y
-        b = v[1]['y']
-    if (not a.shape[0] == a.shape[1]):
-        print('Not square matrix! Using pseudoinversion...')
-        assert type(b)==float, "Need scalar value to compare SVDs for pseudoinversion"
-        retval = np.linalg.pinv(a, rcond=b, hermitian=False)
+        b = float(params['rcond'])
+    if ((type(a)==float) or (type(a)==int)):
+        print('Performing simple inversion')
+        retval = 1/a
+    elif (type(a) == np.ndarray):
+        if (not a.shape[0] == a.shape[1]):
+            print('Not square matrix! Using pseudoinversion...')
+            assert type(b)==float, "Need scalar value to compare SVDs for pseudoinversion"
+            retval = np.linalg.pinv(a, rcond=b, hermitian=False)
+        else:
+            try:
+                retval = np.linalg.inv(a)
+            except np.linalg.LinAlgError:
+                raise ValueError('Inversion failed! Singular matrix returned...')
     else:
-        try:
-            retval = np.linalg.inv(a)
-        except np.linalg.LinAlgError:
-            raise ValueError('Inversion failed! Singular matrix returned...')
+        raise ValueError('Incorrect input type.')
     return DataContainer(type='matrix',m=retval)
