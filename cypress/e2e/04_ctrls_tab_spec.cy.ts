aed1b881f1820a96a2c1d7ca08d33f0d64a9eb32
Syed Mahbub <xsmahmed776@gmail.com>
2023-03-01 05:24:02 +0600

    reform test cases

--- cypress/e2e/04_ctrls_tab_spec.cy.ts
+++ cypress/e2e/04_ctrls_tab_spec.cy.ts
@@ -1,56 +1,65 @@
 const ctrlParameters = [
-    [
-      { title: "Linspace ▶ START", value: 10 },
-      { title: "Linspace ▶ END", value: 34 },
-      { title: "Linspace ▶ STEP", value: 3000 },
-      { title: "SINE ▶ FREQUENCY", value: 85 },
-      { title: "SINE ▶ OFFSET", value: 0 },
-      { title: "SINE ▶ AMPLITUDE", value: 25 },
-      // { title: "SINE ▶ WAVEFORM", value: "sine" },
-      { title: "2.0 ▶ CONSTANT", value: 8 },
-    ],
-  ];
+  [
+    { title: "Linspace ▶ START", value: 10 },
+    { title: "Linspace ▶ END", value: 34 },
+    { title: "Linspace ▶ STEP", value: 3000 },
+    { title: "SINE ▶ FREQUENCY", value: 85 },
+    { title: "SINE ▶ OFFSET", value: 0 },
+    { title: "SINE ▶ AMPLITUDE", value: 25 },
+    // { title: "SINE ▶ WAVEFORM", value: "sine" },
+    { title: "2.0 ▶ CONSTANT", value: 8 },
+  ],
+];
 
-describe('Ctrl Tab management', () => {
-    it("Should load default flow chart", () => {
-        // eslint-disable-next-line cypress/no-unnecessary-waiting
-        cy.visit("/").wait(1000);
-        cy.get("[data-testid=react-flow]", { timeout: 20000 });;
-        cy.get(`[data-cy="app-status"]`)
-        .find('code')
-        .then( ($ele) => {
-          if ($ele.text().includes("🐢 awaiting a new job") || 
-          $ele.text().includes("⏰ server uptime:")) {
-              return true;
-          } else {
-            throw new Error("not correct status")
-          }
-        });
+describe("Ctrl Tab management", () => {
+  it("Should load default flow chart", () => {
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.visit("/").wait(1000);
+    cy.get("[data-testid=react-flow]", { timeout: 20000 });
+    cy.get(`[data-cy="app-status"]`)
+      .find("code")
+      .then(($ele) => {
+        if (
+          $ele.text().includes("🐢 awaiting a new job") ||
+          $ele.text().includes("⏰ server uptime:")
+        ) {
+          return true;
+        } else {
+          throw new Error("not correct status");
+        }
+      });
 
-    cy.get("body").then($body => {
-      if ($body.find(".ctrl-close-btn").length > 0) {   
-        cy.get(".ctrl-close-btn").click({ force: true }); 
+    cy.get("body").then(($body) => {
+      if ($body.find(".ctrl-close-btn").length > 0) {
+        cy.get(".ctrl-close-btn").click({ force: true });
       }
     });
-    cy.get(`[data-cy="ctrls-btn"]`)
-      .click({timeout : 10000});
+    cy.get(`[data-cy="ctrls-btn"]`).click({ timeout: 10000 });
 
     cy.get("[data-cy=operation-switch]")
       .contains("Edit")
       .click()
       .should("have.css", "color", "rgb(255, 165, 0)");
 
-    Cypress.on('uncaught:exception', (err, runnable) => {
-      return false
-    })
+    Cypress.on("uncaught:exception", (err, runnable) => {
+      return false;
+    });
     cy.get("button[id=INPUT_PLACEHOLDER]").click();
 
-    cy.get("[data-cy=add-ctrl]").click().get("button").contains("Numeric Input").first().click();
+    cy.get("[data-cy=add-ctrl]")
+      .click()
+      .get("button")
+      .contains("Numeric Input")
+      .first()
+      .click();
     ctrlParameters.forEach((singleIter, index) => {
       singleIter.forEach((item) => {
         cy.get("[data-cy=ctrls-select]").click();
-        cy.contains("[data-cy=ctrl-grid-item]", item.title).within(($ele) => {
-          cy.contains(`${item.title}`).click({force: true});
+        cy.contains(
+          "[data-cy=ctrl-grid-item]",
+          item.title.toUpperCase()
+        ).within(($ele) => {
+          cy.contains(`${item.title.toUpperCase()}`).click({ force: true });
           if (item.title === "SINE ▶ WAVEFORM") {
             return cy
               .get(`input[value="${item.value}"]`)
@@ -59,9 +68,9 @@ describe('Ctrl Tab management', () => {
           return cy
             .get(`input[type=number]`)
             .click()
-            .type(`{selectall}${item.value.toString()}`)
+            .type(`{selectall}${item.value.toString()}`);
         });
       });
     });
   });
-})
+});
