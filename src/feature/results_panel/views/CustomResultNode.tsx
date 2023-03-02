d8760cd658922667817cb0acac9872d82a52f0f6
Syed Mahbub <xsmahmed776@gmail.com>
2023-03-03 02:11:15 +0600

    [WIP] ResultPanel UT: update type definitions of Result_panel and some fixes

--- src/feature/results_panel/views/CustomResultNode.tsx
+++ src/feature/results_panel/views/CustomResultNode.tsx
@@ -2,19 +2,25 @@ import { Handle, Position } from "reactflow";
 import { useFlowChartState } from "../../../hooks/useFlowChartState";
 import styledPlotLayout from "../../common/defaultPlotLayout";
 import PlotlyComponent from "../../common/PlotlyComponent";
-
-const CustomResultNode = ({ data }) => {
+import { ResultNodeData } from "../types/ResultsType";
+import { Buffer } from "buffer";
+interface CustomResultNodeProp {
+  data: ResultNodeData;
+}
+const CustomResultNode: React.FC<CustomResultNodeProp> = ({ data }) => {
   const { uiTheme } = useFlowChartState();
   const styledLayout = styledPlotLayout(uiTheme);
 
-  if (data?.resultData && (data.resultData.type !== undefined && 
-      (data.resultData.type === "file" || data.resultData.type === "image")) && 
-      data.resultData.y !== undefined) {
-        const fileType = data.resultData.file_type[0];
-        const fileContent = data.resultData.y[0];
-        data.resultData.type = fileType;
-        data.resultData.source = convertToDataUrl(fileContent, fileType);       
-  }          
+  if (
+    data?.resultData &&
+    data.resultData.type &&
+    data.resultData.type === "image" &&
+    data.resultData.y !== undefined
+  ) {
+    const fileType = data.resultData.file_type![0];
+    const fileContent = data.resultData.y![0];
+    data.resultData.source = convertToDataUrl(fileContent, fileType);
+  }
 
   return (
     <div style={{ position: "relative" }} data-testid="result-node">
@@ -32,16 +38,11 @@ const CustomResultNode = ({ data }) => {
       </>
 
       {!data?.resultData ? (
-        <p> `NO Result`</p>
+        <p> NO Result </p>
       ) : (
         <PlotlyComponent
           id={data.id}
-          data={
-            !data.resultData?.data
-              ? [{ x: data.resultData["x"], y: data.resultData["y"], 
-              source: data.resultData["source"], type: data.resultData["type"] }]
-              :  data.resultData.data
-          }
+          data={[data.resultData]}
           layout={
             !data.resultData?.layout
               ? Object.assign({}, { title: `${data.func}` }, styledLayout)
@@ -65,12 +66,11 @@ const CustomResultNode = ({ data }) => {
 
 export default CustomResultNode;
 
-
 function convertToDataUrl(fileContent: any, fileType: string): string {
-  let dataUrl = '';
+  let dataUrl = "";
   switch (fileType) {
-    case 'image':
-      dataUrl = "data:image/jpeg;base64," + convertToBase64(fileContent)
+    case "image":
+      dataUrl = "data:image/jpeg;base64," + convertToBase64(fileContent);
       break;
     default:
       break;
@@ -78,13 +78,5 @@ function convertToDataUrl(fileContent: any, fileType: string): string {
   return dataUrl;
 }
 
-function convertToBase64(content: any): string {
-  let binary = ''
-  const bytes = new Uint8Array(content);
-  const len = bytes.byteLength;
-  for (let i = 0; i < len; i++) {
-    binary += String.fromCharCode(bytes[i]);
-  }
-  return btoa(binary);
-
-}
+const convertToBase64 = (content: any) =>
+  Buffer.from(content).toString("base64");
