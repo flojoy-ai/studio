af80b7b701739f2370f7a34637fc132bf1bc6249
Syed Mahbub <xsmahmed776@gmail.com>
2023-03-13 05:24:17 +0600

    implement support for numpy arrays and pd dframes in smallMemory class

--- PYTHON/dao/redis_dao.py
+++ PYTHON/dao/redis_dao.py
@@ -1,6 +1,8 @@
 import json
 from redis import Redis
 import os
+import numpy as np
+import pandas as pd
 
 MAX_LIST_SIZE = 1000
 REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
@@ -21,7 +23,35 @@ class RedisDao:
             RedisDao._instance = RedisDao()
         return RedisDao._instance
 
-    def get_redis_obj(self, key:str):
+    def set_np_array(self, memo_key: str, value: np.ndarray):
+        encoded = self.serialize_np(value)
+        return self.r.set(memo_key, encoded)
+
+    def set_pandas_dataframe(self, key: str, dframe: pd.DataFrame):
+        encode = dframe.to_json()
+        self.r.set(key, encode)
+        return
+
+    def set_str(self, key: str, value: str):
+        self.r.set(key, value)
+        return
+
+    def get_pd_dataframe(self, key: str):
+        encoded = self.r.get(key)
+        decode = encoded.decode("utf-8") if encoded is not None else ''
+        read_json = pd.read_json(decode)
+        return read_json.head()
+
+    def get_np_array(self, memo_key: str):
+        encoded = self.r.get(memo_key)
+        decode = self.desirialize_np(encoded, memo_key)
+        return decode
+
+    def get_str(self, key: str):
+        encoded = self.r.get(key)
+        return encoded.decode("utf-8") if encoded is not None else None
+
+    def get_redis_obj(self, key: str):
         get_obj = self.r.get(key)
         parse_obj = json.loads(get_obj) if get_obj is not None else {}
         return parse_obj
@@ -41,7 +71,34 @@ class RedisDao:
 
     def remove_item_from_list(self, key, item):
         self.r.lrem(key, 1, item)
+
     def add_to_set(self, key, value):
         self.r.sadd(key, value)
+
     def get_set_list(self, key):
-       return self.r.smembers(key)
+        return self.r.smembers(key)
+
+    def serialize_np(self, np_array: np.ndarray):
+        return np_array.ravel().tostring()
+
+    def desirialize_np(self, encoded: str, memo_key: str):
+        if encoded is None:
+            return []
+        split_key = memo_key.split('|')[1].split('#')
+        d_len = len(split_key)
+        if d_len == 0:
+            return []
+        d_type = split_key[0]
+        nd_array = np.fromstring(encoded, dtype=d_type)
+        if d_len == 6:
+            return nd_array.reshape(int(split_key[1]), int(split_key[2]), int(split_key[3]), int(split_key[4]), int(split_key[5]))
+        if d_len == 5:
+            return nd_array.reshape(int(split_key[1]), int(split_key[2]), int(split_key[3]), int(split_key[4]))
+        if d_len == 4:
+            return nd_array.reshape(int(split_key[1]), int(split_key[2]), int(split_key[3]))
+        if d_len == 3:
+            return nd_array.reshape(int(split_key[1]), int(split_key[2]))
+        if d_len == 2:
+            return nd_array.reshape(int(split_key[1]))
+        raise ValueError(
+            "Currently RedisDao only supports up to 5d numpy arrays!")
--- PYTHON/node_sdk/small_memory.py
+++ PYTHON/node_sdk/small_memory.py
@@ -1,6 +1,6 @@
-import json
-from dao.redis_dao import RedisDao
 import traceback
+
+from dao.redis_dao import RedisDao
 
 
 class SmallMemory:
@@ -8,6 +8,7 @@ class SmallMemory:
     SmallMemory - available during jobset execution - intended to be used inside node functions
     '''
     tracing_key = 'ALL_MEMORY_KEYS'
+
     def add_to_tracing_list(self, memory_key):
         RedisDao.get_instance().add_to_set(self.tracing_key, memory_key)
 
@@ -21,7 +22,7 @@ class SmallMemory:
             print(Exception, traceback.format_exc())
             pass
 
-    def write_object(self, job_id, key, obj):
+    def write_to_memory(self, job_id, key, value):
         '''
         Stores object in internal DB.
         The memory will be available for the duration the jobset is running.
@@ -32,14 +33,52 @@ class SmallMemory:
 
         # for thread safety, we'll have to implement Global Locking feature of Redis
         memory_key = F'{job_id}-{key}'
-        RedisDao.get_instance().set_redis_obj(memory_key, obj)
-        self.add_to_tracing_list(memory_key)
+        s = str(type(value))
+        v_type = s.split("'")[1]
+        match v_type:
+            case "numpy.ndarray":
+                np_memo_key = f"{memory_key}_np_memo_key"
+                array_dtype = str(value.dtype)
+                memo_key = f'{key}|{array_dtype}'
+                for d in value.shape:
+                    memo_key += f"#{d}"
+                RedisDao.get_instance().set_str(np_memo_key, memo_key)
+                self.add_to_tracing_list(np_memo_key)
+                RedisDao.get_instance().set_np_array(memo_key, value)
+                self.add_to_tracing_list(memo_key)
+            case 'pandas.core.frame.DataFrame':
+                pd_memo_key = f"{memory_key}_pd_memo_key"
+                RedisDao.get_instance().set_str(pd_memo_key, "df_stored")
+                self.add_to_tracing_list(pd_memo_key)
+                RedisDao.get_instance().set_pandas_dataframe(memory_key, value)
+                self.add_to_tracing_list(memory_key)
+            case 'str':
+                RedisDao.get_instance().set_str(memory_key, value)
+                self.add_to_tracing_list(memory_key)
+            case 'dict':
+                RedisDao.get_instance().set_redis_obj(memory_key, value)
+                self.add_to_tracing_list(memory_key)
+            case _:
+                raise ValueError(
+                    f"SmallMemory currently does not support '{v_type}' type data!")
 
-    def read_object(self, job_id, key):
+    def read_memory(self, job_id, key):
         '''
         Reads object stored in internal DB by the given key. The memory is job specific.
         '''
         memory_key = F'{job_id}-{key}'
+        get_str = RedisDao.get_instance().get_str(memory_key)
+        if get_str:
+            return get_str
+        np_memo_key = RedisDao.get_instance().get_str(
+            f"{memory_key}_np_memo_key")
+        if np_memo_key:
+            np_array = RedisDao.get_instance().get_np_array(np_memo_key)
+            return np_array
+        pd_memo_key = f"{memory_key}_pd_memo_key"
+        if pd_memo_key:
+            pd_dframe = RedisDao.get_instance().get_pd_dataframe(memory_key)
+            return pd_dframe
         obj = RedisDao.get_instance().get_redis_obj(memory_key)
         return obj
 
