26aae83f2be81ecf4d5ea87791fd129e25ee61af
shifu <sifatibna.amin9@gmail.com>
2023-03-20 18:38:46 +0600

    modified submodule script

--- add_submodules.sh
+++ add_submodules.sh
@@ -3,7 +3,7 @@ CWD="$PWD"
 branch_name_apps=$1
 echo "Pulling latest from branch '$branch_name_apps' for APPS"
 if [ "$(ls -A $CWD/apps)" ]; then  # check if folder is not empty
-    cd $CWD/apps && git pull origin main && git checkout $branch_name_apps
+    cd $CWD/apps && git pull origin main && git checkout $branch_name_apps && git pull origin $branch_name_apps
 else
     cd $CWD && git clone -b $branch_name_apps https://github.com/flojoy-io/apps.git
 fi
@@ -17,7 +17,7 @@ cp -R "$source_dir"/* "$target_dir"/
 branch_name_nodes=$2
 echo "Pulling latest from branch '$branch_name_nodes' for NODES"
 if [ "$(ls -A $CWD/PYTHON/nodes)" ]; then  # check if folder is not empty
-    cd $CWD/PYTHON/nodes && git pull origin main && git checkout $branch_name_nodes
+    cd $CWD/PYTHON/nodes && git pull origin main && git checkout $branch_name_nodes && git pull origin $branch_name_nodes
 else
     cd $CWD/PYTHON && git clone -b $branch_name_nodes https://github.com/flojoy-io/nodes.git
 fi
