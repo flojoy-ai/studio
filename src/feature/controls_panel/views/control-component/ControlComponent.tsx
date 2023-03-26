f1d2a16ed7e7fbfe3d5769ee40e74971a01ac45c
Tristan Britt <tristan.britt@mail.mcgill.ca>
2023-03-26 18:22:51 -0400

    allowed for input for float values into numeric in

--- src/feature/controls_panel/views/control-component/ControlComponent.tsx
+++ src/feature/controls_panel/views/control-component/ControlComponent.tsx
@@ -263,8 +263,6 @@ const ControlComponent = ({
       {ctrlObj.name === ControlNames.NumericInput && (
         <div className="ctrl-input-body" data-cy="numeric-input">
           <input
-            type="number"
-            step="any"
             placeholder="Enter a number"
             className="ctrl-numeric-input border-color"
             onChange={(e) => {
