07109c05a1669daa8c8f5913e2d2f45aa1c61dbc
shifu <sifatibna.amin9@gmail.com>
2023-03-27 04:16:44 +0600

    wip

--- mac_start_up.sh
+++ mac_start_up.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 success_color=71
-warning_color=145
+warning_color=3
 error_color=92
 message_color=61
+general_color=212
 
 gum style \
-	--foreground 212 --border-foreground 212 --border double \
+	--foreground $general_color --border-foreground 212 --border double \
 	--align center --width 60 --margin "1 2" --padding "2 4" \
 	'Welcome to Flojoy!' 'For Installation, Follow the Link Below' 'https://docs.flojoy.io/getting-started/install/'
 
@@ -60,10 +61,10 @@ do
     esac
 done
 
-gum spin --spinner dot --title 'update ES6 status codes file...' -- python3 -c 'import yaml, json; f=open("src/STATUS_CODES.json", "w"); f.write(json.dumps(yaml.safe_load(open("STATUS_CODES.yml").read()), indent=4)); f.close();'
+gum spin --spinner dot --title 'update ES6 status codes file...' --title.foreground="$general_color" -- python3 -c 'import yaml, json; f=open("src/STATUS_CODES.json", "w"); f.write(json.dumps(yaml.safe_load(open("STATUS_CODES.yml").read()), indent=4)); f.close();'
 gum style --foreground $success_color ':heavy_check_mark: update update ES6 status codes file ...' | gum format -t emoji
 
-gum spin --spinner dot --title 'create symlinks...' -- sleep 1
+gum spin --spinner dot --title 'create symlinks...' --title.foreground="$general_color" -- sleep 1
 FILE=$PWD/PYTHON/WATCH/STATUS_CODES.yml
 if test -f "$FILE"; then
    gum style --foreground $warning_color ":point_right: $FILE exists." | gum format -t emoji
@@ -79,40 +80,39 @@ else
 fi
 gum style --foreground $success_color ':heavy_check_mark: create symlinks...' | gum format -t emoji
 
-gum spin --spinner dot --title 'jsonify python functions and write to JS-readable directory' -- python3 write_python_metadata.py
+gum spin --spinner dot --title 'jsonify python functions and write to JS-readable directory' --title.foreground="$general_color" -- python3 write_python_metadata.py
 gum style --foreground $success_color ':heavy_check_mark: jsonify python functions and write to JS-readable directory' | gum format -t emoji
 
-gum spin --spinner dot --title 'generate manifest for python nodes to frontend' -- python3 generate_manifest.py
+gum spin --spinner dot --title 'generate manifest for python nodes to frontend' --title.foreground="$general_color" -- python3 generate_manifest.py
 gum style --foreground $success_color ':heavy_check_mark: generate manifest for python nodes to frontend' | gum format -t emoji
 
 if [ $initNodePackages = true ]
 then
    gum style --foreground $message_color ' -n flag is not provided'
-   gum spin --spinner dot --title 'Node packages will be installed from package.json!' -- npm install --legacy-peer-deps
+   gum spin --spinner dot --title 'Node packages will be installed from package.json!' --title.foreground="$general_color" -- npm install --legacy-peer-deps
    gum style --foreground $success_color ':heavy_check_mark: Node packages will be installed from package.json!' | gum format -t emoji
 fi
 
 
-# if [ $initRedis ]
-# then
-#     echo 'shutting down any existing redis server and clearing redis memory...'
-#     npx ttab -t 'REDIS-CLI' redis-cli SHUTDOWN
-#     sleep 2
-#     redis-cli FLUSHALL
-#     sleep 2
+if [ $initRedis ]
+then
 
-#     echo 'spining up a fresh redis server...'
-#     npx ttab -t 'REDIS' redis-server
-#     sleep 2
-# fi
+   gum spin --spinner dot --title 'shutting down any existing redis server and clearing redis memory...' --title.foreground="$general_color" -- npx ttab -t 'REDIS-CLI' "redis-cli SHUTDOWN;sleep 2;redis-cli FLUSHALL;exit"
 
-# venvCmd=""
-# if [ ! -z "$venv" ]
-# then
-#    echo "virtualenv path is provided, will use: ${venv}";
-#    venvCmd="source ${venv}/bin/activate &&"
-#    echo "venv cmd: ${venvCmd}"
-# fi
+   gum style --foreground $success_color ':heavy_check_mark: shutting down any existing redis server and clearing redis memory...' | gum format -t emoji
+
+   gum spin --spinner dot --title 'spining up a fresh redis server...' --title.foreground="$general_color" -- npx ttab -t 'REDIS-CLI' "redis-server;sleep 2;exit"
+
+   gum style --foreground $success_color ':heavy_check_mark: spining up a fresh redis server...' | gum format -t emoji
+fi
+
+venvCmd=""
+if [ ! -z "$venv" ]
+then
+   gum style --foreground $warning_color ":point_right: virtualenv path is provided, will use: ${venv}" | gum format -t emoji
+   venvCmd="source ${venv}/bin/activate &&"
+   gum style --foreground $warning_color ":point_right: venv cmd: ${venvCmd}" | gum format -t emoji
+fi
 
 # CWD="$PWD"
 
