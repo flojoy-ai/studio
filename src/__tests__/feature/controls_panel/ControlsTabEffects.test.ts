48498ab4e4b14f64e5068361fcb26ccb3c465215
AshfaqKabir <ashfaqkabir@gmail.com>
2023-04-01 16:52:06 +0000

    Unit Tested ControlsTabEffect.test.ts

--- src/__tests__/feature/controls_panel/ControlsTabEffects.test.ts
+++ src/__tests__/feature/controls_panel/ControlsTabEffects.test.ts
@@ -1,18 +1,45 @@
 import { renderHook } from "@testing-library/react-hooks";
-import { useControlsTabEffects } from "../../../feature/controls_panel/ControlsTabEffects";
-import { useFlowChartState } from "../../../hooks/useFlowChartState";
+import { useControlsTabEffects } from "@src/feature/controls_panel/ControlsTabEffects";
+import { useFlowChartState } from "@src/hooks/useFlowChartState";
 
 jest.mock("@src/hooks/useFlowChartState");
 
-describe("useControlsTabEffects tests", () => {
+describe("UseControlsTabEffects", () => {
+  let saveAndRun = jest.fn().mockReturnValue(undefined);
   let setCtrlsManifest = jest.fn();
   let rfInstance = {
-    nodes: [{ id: 1, type: "start" }],
+    nodes: [],
   };
 
-  it("should not call setCtrlsManifest when rfInstance.nodes is not empty", () => {
-    rfInstance.nodes = [{ id: 1, type: "start" }];
-    renderHook(() => useControlsTabEffects);
-    expect(setCtrlsManifest).not.toHaveBeenCalledWith([]);
+  beforeEach(() => {
+    (useFlowChartState as jest.Mock).mockReturnValue({
+      setCtrlsManifest: setCtrlsManifest,
+      rfInstance: rfInstance,
+    });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("ControlsTabEffects should render with Undefined", () => {
+    let { result } = renderHook(() => useControlsTabEffects(saveAndRun));
+    expect(result.current).toBe(saveAndRun());
+  });
+
+  it("setCtrlManifest should be empty if rfInstance has no nodes", () => {
+    renderHook(() => useControlsTabEffects(saveAndRun));
+    expect(setCtrlsManifest).toHaveBeenCalledWith([]);
+  });
+
+  it("setCtrlManifest should not be called if rfInstance has nodes", () => {
+    let rfInstanceWithNode = [{ id: 1, type: "start" }];
+    (useFlowChartState as jest.Mock)
+      .mockReturnValue({
+        setCtrlsManifest: setCtrlsManifest,
+        rfInstance: rfInstanceWithNode,
+      });
+    renderHook(() => useControlsTabEffects(saveAndRun));
+    expect(setCtrlsManifest).not.toHaveBeenCalled();
   });
 });
