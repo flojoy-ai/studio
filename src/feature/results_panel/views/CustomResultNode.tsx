a3847dd8471552f12f5151504d7d60ed6c598b11
Syed Mahbub <xsmahmed776@gmail.com>
2023-04-03 23:36:54 +0100

    remove unecessary codes from CustomResultNode.tsx

--- src/feature/results_panel/views/CustomResultNode.tsx
+++ src/feature/results_panel/views/CustomResultNode.tsx
@@ -3,24 +3,12 @@ import { useFlowChartState } from "../../../hooks/useFlowChartState";
 import styledPlotLayout from "@src/feature/common/defaultPlotLayout";
 import PlotlyComponent from "@src/feature/common/PlotlyComponent";
 import { ResultNodeData } from "../types/ResultsType";
-import { Buffer } from "buffer";
 interface CustomResultNodeProp {
   data: ResultNodeData;
 }
 const CustomResultNode: React.FC<CustomResultNodeProp> = ({ data }) => {
   const { uiTheme } = useFlowChartState();
   const styledLayout = styledPlotLayout(uiTheme);
-
-  if (
-    data?.resultData &&
-    data.resultData.type &&
-    data.resultData.type === "image" &&
-    data.resultData.y !== undefined
-  ) {
-    const fileType = data.resultData.file_type![0];
-    const fileContent = data.resultData.y![0];
-    data.resultData.source = convertToDataUrl(fileContent, fileType);
-  }
 
   return (
     <div style={{ position: "relative" }} data-testid="result-node">
@@ -56,18 +44,3 @@ const CustomResultNode: React.FC<CustomResultNodeProp> = ({ data }) => {
 };
 
 export default CustomResultNode;
-
-function convertToDataUrl(fileContent: any, fileType: string): string {
-  let dataUrl = "";
-  switch (fileType) {
-    case "image":
-      dataUrl = "data:image/jpeg;base64," + convertToBase64(fileContent);
-      break;
-    default:
-      break;
-  }
-  return dataUrl;
-}
-
-const convertToBase64 = (content: any) =>
-  Buffer.from(content).toString("base64");
