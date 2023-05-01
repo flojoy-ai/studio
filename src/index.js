027ca2c0436e58bf92d43d0cd3f997f6f5b2986b
JeffDotPng <zhangjeff2014@gmail.com>
2023-05-01 16:02:23 -0400

    Use React 18's `createRoot`

--- src/index.js
+++ src/index.js
@@ -6,17 +6,19 @@ import reportWebVitals from "./reportWebVitals";
 import { SocketContextProvider } from "./context/socket.context";
 // default styling
 import "reactflow/dist/style.css";
+import { createRoot } from "react-dom/client";
 
 // or if you just want basic styles
 import "reactflow/dist/base.css";
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <SocketContextProvider>
       <App />
     </SocketContextProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
