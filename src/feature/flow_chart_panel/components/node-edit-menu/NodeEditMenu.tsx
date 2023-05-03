b7436001ec998f601ed404920aaa495e76bca6df
JeffDotPng <zhangjeff2014@gmail.com>
2023-05-03 17:32:04 -0400

    Cursor pointer on close button hover

--- src/feature/flow_chart_panel/components/node-edit-menu/NodeEditMenu.tsx
+++ src/feature/flow_chart_panel/components/node-edit-menu/NodeEditMenu.tsx
@@ -4,7 +4,8 @@ import { ElementsData } from "../../types/CustomNodeProps";
 import { FUNCTION_PARAMETERS } from "../../manifest/PARAMETERS_MANIFEST";
 import ParamField, { ParamType } from "./ParamField";
 import styled from "styled-components";
-import { IconPencil } from "@tabler/icons-react";
+import { IconPencil, IconX } from "@tabler/icons-react";
+import { useFlowChartState } from "@src/hooks/useFlowChartState";
 
 type NodeEditMenuProps = {
   nodes: Node<ElementsData>[];
@@ -17,7 +18,7 @@ const StyledMenu = styled.div`
   z-index: 10;
   height: 684px;
   width: 324px;
-  padding: 32px 24px;
+  padding: 8px 8px;
   background-color: ${(props) =>
     props.theme === "dark" ? "#141313ff" : "white"};
   box-shadow: ${(props) =>
@@ -43,37 +44,50 @@ const StyledParamName = styled.div`
   margin-bottom: 4px;
 `;
 
+const StyledCloseButton = styled.div`
+  width: fit-content;
+  height: 18px;
+  margin-left: auto;
+  cursor: pointer;
+`;
+
 const NodeEditMenu = ({ nodes }: NodeEditMenuProps) => {
   const theme = useMantineTheme();
+  const { setIsEditMode } = useFlowChartState();
 
   return (
     <StyledMenu theme={theme.colorScheme}>
-      {nodes.map((node) => (
-        <div>
-          <StyledTitleContainer>
-            <StyledTitle>{node.data.func.toUpperCase()}</StyledTitle>
-            <IconPencil
-              size={18}
-              style={{ marginLeft: "1rem", marginBottom: "4px" }}
-            />
-          </StyledTitleContainer>
-          {Object.entries(FUNCTION_PARAMETERS[node.data.func]).map(
-            ([name, param]) => (
-              <div key={node.id + name}>
-                <StyledParamName>{`${name.toUpperCase()}:`}</StyledParamName>
-                <ParamField
-                  nodeId={node.id}
-                  paramId={name}
-                  functionName={node.data.func}
-                  type={param.type as ParamType}
-                  value={node.data.ctrls[name].value}
-                  options={param.options}
-                />
-              </div>
-            )
-          )}
-        </div>
-      ))}
+      <StyledCloseButton onClick={() => setIsEditMode(false)}>
+        <IconX size={18} />
+      </StyledCloseButton>
+      <div style={{ padding: "0px 16px 24px 16px" }}>
+        {nodes.map((node) => (
+          <div>
+            <StyledTitleContainer>
+              <StyledTitle>{node.data.func.toUpperCase()}</StyledTitle>
+              <IconPencil
+                size={18}
+                style={{ marginLeft: "1rem", marginBottom: "4px" }}
+              />
+            </StyledTitleContainer>
+            {Object.entries(FUNCTION_PARAMETERS[node.data.func]).map(
+              ([name, param]) => (
+                <div key={node.id + name}>
+                  <StyledParamName>{`${name.toUpperCase()}:`}</StyledParamName>
+                  <ParamField
+                    nodeId={node.id}
+                    paramId={name}
+                    functionName={node.data.func}
+                    type={param.type as ParamType}
+                    value={node.data.ctrls[name].value}
+                    options={param.options}
+                  />
+                </div>
+              )
+            )}
+          </div>
+        ))}
+      </div>
     </StyledMenu>
   );
 };
