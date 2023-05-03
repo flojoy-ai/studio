4daa8153e9a071158428e2c46e5b1f7aa4ea1df7
JeffDotPng <zhangjeff2014@gmail.com>
2023-05-03 10:59:08 -0400

    Get all tests passing

--- src/__tests__/feature/flow_chart_panel/FlowChartTabEffects.test.tsx
+++ src/__tests__/feature/flow_chart_panel/FlowChartTabEffects.test.tsx
@@ -39,19 +39,25 @@ jest.mock("@src/services/FlowChartServices", () => {
   };
 });
 
-const { result, rerender } = renderHook(() => useFlowChartTabEffects(params));
-
 describe("FlowChartTabState", () => {
   it("should call useEffect 4 times", () => {
+    const { rerender } = renderHook(() => useFlowChartTabEffects(params));
+
     const spy = jest.spyOn(React, "useEffect");
     rerender();
     expect(spy).toHaveBeenCalledTimes(4);
   });
   it("checks if the function is called", () => {
+    const { rerender } = renderHook(() => useFlowChartTabEffects(params));
+
     rerender();
     expect(params.setNd).toHaveBeenCalled();
   });
   it("checks if the hook returns anything", () => {
+    const { result, rerender } = renderHook(() =>
+      useFlowChartTabEffects(params)
+    );
+
     rerender();
     expect(result.current).toBe(undefined);
   });
