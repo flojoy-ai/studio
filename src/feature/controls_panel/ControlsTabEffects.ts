cc522fa76bc3982a3d12deb8a8219fb0cd2c54f2
JeffDotPng <zhangjeff2014@gmail.com>
2023-05-16 15:34:49 -0400

    Add back a useEffect

--- src/feature/controls_panel/ControlsTabEffects.ts
+++ src/feature/controls_panel/ControlsTabEffects.ts
@@ -2,14 +2,13 @@ import { useEffect } from "react";
 import { useFlowChartState } from "../../hooks/useFlowChartState";
 
 export function useControlsTabEffects() {
-  const { setCtrlsManifest, rfInstance } = useFlowChartState();
+  const { setCtrlsManifest, nodes } = useFlowChartState();
 
-  // TODO: Find out what this does and add it back if needed
-
-  // useEffect(() => {
-  //   if (rfInstance?.nodes.length === 0) {
-  //     setCtrlsManifest([]);
-  //   }
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [rfInstance?.nodes]);
+  useEffect(() => {
+    console.log("40");
+    if (nodes.length === 0) {
+      setCtrlsManifest([]);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [nodes]);
 }
