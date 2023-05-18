8b57bef242b9ec3d91cd9d82e782136e8f158139
Jinwon Lee <jinwonlee0916@gmail.com>
2023-05-18 15:46:51 -0400

    Added expand icon and updated css

--- src/feature/flow_chart_panel/components/node-edit-menu/NodeEditButtons.tsx
+++ src/feature/flow_chart_panel/components/node-edit-menu/NodeEditButtons.tsx
@@ -1,6 +1,6 @@
 import { Box, createStyles } from "@mantine/core";
 import { useFlowChartState } from "@src/hooks/useFlowChartState";
-import { IconPencil, IconX } from "@tabler/icons-react";
+import { IconArrowsMaximize, IconPencil, IconX } from "@tabler/icons-react";
 import { ElementsData } from "../../types/CustomNodeProps";
 const useStyles = createStyles((theme) => ({
   Edit: {
@@ -15,6 +15,18 @@ const useStyles = createStyles((theme) => ({
         ? theme.colors.gray[1]
         : theme.colors.dark[8],
   },
+  leftEdit: {
+    position: "absolute",
+    top: 8,
+    left: 7,
+    zIndex: 150,
+    display: "flex",
+    cursor: "pointer",
+    color:
+      theme.colorScheme === "dark"
+        ? theme.colors.gray[1]
+        : theme.colors.dark[8],
+  }
 }));
 
 type NodeEditButtonsProps = {
@@ -32,10 +44,15 @@ const NodeEditButtons = ({
   const { classes } = useStyles();
 
   return (
-    <Box className={classes.Edit}>
+    <div>
+    <Box className={classes.leftEdit}>
+      <IconArrowsMaximize />
       {showPencil && <IconPencil onClick={() => setIsEditMode(true)} />}
+    </Box>
+    <Box className={classes.Edit}>
       <IconX onClick={() => handleRemove(data.id)} />
     </Box>
+  </div>
   );
 };
 
