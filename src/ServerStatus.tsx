ef5a7805c247e847d14233821805694c998f51c2
Joey Yu <joey@itsjoeoui.com>
2023-05-24 16:27:33 -0400

    Fix derp
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

--- src/ServerStatus.tsx
+++ src/ServerStatus.tsx
@@ -2,13 +2,11 @@ import { Text, createStyles } from "@mantine/core";
 import { IServerStatus } from "./context/socket.context";
 import { memo } from "react";
 
-export const STATUS_HEIGHT = 30;
-
 const useStyles = createStyles((theme) => ({
   status: {
     backgroundColor: theme.colorScheme === "dark" ? "#14131361" : "#58454517",
     fontSize: "14px",
-    minHeight: STATUS_HEIGHT,
+    minHeight: "30px",
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
@@ -23,7 +21,7 @@ const ServerStatus = ({ serverStatus }: ServerStatusProps) => {
   const { classes } = useStyles();
   return (
     <Text data-cy="app-status" className={classes.status}>
-      <code className={classes.text}>{serverStatus}</code>
+      <code>{serverStatus}</code>
     </Text>
   );
 };
