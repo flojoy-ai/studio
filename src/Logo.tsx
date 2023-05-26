5c6543bd893923f105cfdba8580d519a0004b773
Jinwon Lee <jinwonlee0916@gmail.com>
2023-05-26 13:34:20 -0400

    format

--- src/Logo.tsx
+++ src/Logo.tsx
@@ -2,6 +2,11 @@ import { Image } from "@mantine/core";
 
 export const Logo = () => {
   return (
-    <Image width={80} height={80} src="/public/assets/favicon.png" alt="Logo"></Image>
+    <Image
+      width={80}
+      height={80}
+      src="/public/assets/favicon.png"
+      alt="Logo"
+    ></Image>
   );
 };
