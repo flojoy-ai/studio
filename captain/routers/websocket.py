0bb9d7e243f6e1ce8b8be303e3dfe738e0eeb913
Hristo <sandevh@gmail.com>
2023-05-26 15:16:23 -0400

    template for apis

---
+++ captain/routers/websocket.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter, WebSocket
+router = APIRouter()
+
+@router.websocket("/ws/socket-server/")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
