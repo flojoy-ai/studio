f02d209f0dc50cf7941456a8e50707e1052484b4
Joey Yu <joey@itsjoeoui.com>
2023-05-28 12:23:12 -0400

    Update zod according to allowed schema type

--- src/feature/flow_chart_panel/manifest/PARAMETERS_MANIFEST.ts
+++ src/feature/flow_chart_panel/manifest/PARAMETERS_MANIFEST.ts
@@ -3,14 +3,16 @@ import { z } from "zod";
 
 const paramsSchema = z.record(
   z.string(),
-  z.record(
-    z.string(),
-    z.object({
-      type: z.string(),
-      default: z.union([z.string(), z.number()]),
-      options: z.optional(z.array(z.string())),
-    })
-  )
+  z
+    .record(
+      z.string(),
+      z.object({
+        type: z.string(),
+        default: z.union([z.string(), z.number(), z.boolean()]).nullish(),
+        options: z.optional(z.array(z.string())),
+      })
+    )
+    .nullish()
 );
 
 type FuncParamsType = z.infer<typeof paramsSchema>;
