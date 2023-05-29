493f004b4e3f3642ff58fc7e635f7f2d58c2707d
Hristo <sandevh@gmail.com>
2023-05-29 15:07:41 -0400

    format

--- captain/routes/worker.py
+++ captain/routes/worker.py
@@ -5,8 +5,9 @@ from captain.types.worker import WorkerJobResponse
 
 router = APIRouter(tags=["flowchart"])
 
+
 @router.post("/worker_response", summary="worker response")
 async def worker_response(request: WorkerJobResponse):
-    #forward response from worker to the front-end
+    # forward response from worker to the front-end
     await manager.ws.broadcast(json.dumps(request))
     return Response(status_code=200)
