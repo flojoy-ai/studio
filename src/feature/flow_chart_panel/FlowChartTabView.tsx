0df568a65fbcbb849184f54074b0b3aa380c4372
JeffDotPng <zhangjeff2014@gmail.com>
2023-05-30 16:37:02 -0400

    Fix dependency array

--- src/feature/flow_chart_panel/FlowChartTabView.tsx
+++ src/feature/flow_chart_panel/FlowChartTabView.tsx
@@ -97,7 +97,7 @@ const FlowChartTab = () => {
   const addNewNode = useAddNewNode(setNodes, getNodeFuncCount);
   const sidebarCustomContent = useMemo(
     () => <SidebarCustomContent onAddNode={addNewNode} />,
-    []
+    [addNewNode]
   );
 
   const handleNodeRemove = useCallback(
