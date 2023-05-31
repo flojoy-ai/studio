6349c8073e5ee2738cab6356ae6802e340bbe435
Joey Yu <joey@itsjoeoui.com>
2023-05-31 12:00:04 -0400

    Add a helpful prompt

--- src/ErrorPage.tsx
+++ src/ErrorPage.tsx
@@ -8,6 +8,10 @@ type ErrorPageProps = {
 export const ErrorPage = ({ error, resetErrorBoundary }: ErrorPageProps) => (
   <Container size="sm">
     <Title align="center">Oops! Something went wrong.</Title>
+    <Text align="center" style={{ marginTop: "1rem" }}>
+      Note: if the error is a Validation error, then you should check if the
+      manifest file is valid.
+    </Text>
     <Button
       style={{ marginTop: "2rem" }}
       fullWidth
