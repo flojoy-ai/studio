1bad56f3b40365d66fcc3245579fe3a4d9928d00
mehedihasansabbir220 <mohammadmehedihasansabbir@gmail.com>
2023-06-01 11:42:54 +0600

    fixed type issue

--- cypress/e2e/ControlsTabEffects_spec.cy.ts
+++ cypress/e2e/ControlsTabEffects_spec.cy.ts
@@ -1,38 +1,58 @@
 /// <reference types="cypress" />
 
-describe("useControlsTabEffects", () => {
-  before(() => {
-    cy.eyesOpen({
-      appName: "test app ",
-      testName: "useControlsTabEffects Test",
-    });
-  });
+import { useControlsTabEffects } from "../../src/feature/controls_panel/ControlsTabEffects";
 
-  beforeEach(() => {
-    cy.visit("/");
-    cy.eyesCheckWindow("Initial Render");
-  });
+describe( "useControlsTabEffects", () => {
+  beforeEach( () => {
+    cy.eyesOpen( {
+      appName: 'Studio',
+      testName: 'useControlsTabEffects Test',
+    } )
+  } );
 
-  after(() => {
-    cy.eyesClose();
-  });
+  afterEach( () => {
+    cy.eyesClose()
+  } );
 
-  it("should set controls manifest to empty array when rfInstance nodes length is 0", () => {
-    // Stub the useFlowChartState hook
-    cy.stub(window, "useFlowChartState").returns({
-      setCtrlsManifest: cy.stub().as("setCtrlsManifest"),
-      rfInstance: {
-        nodes: [],
-      },
-    });
+  it( "should call setCtrlsManifest with an empty array when nodes length is 0", () => {
+    const setCtrlsManifestSpy = cy.spy();
 
-    // Mount the component that uses the useControlsTabEffects hook
-    cy.visit("your_component_url"); // Replace 'your_component_url' with the URL of the component that uses the hook
+    cy.stub( useControlsTabEffects ).returns( {
+      setCtrlsManifest: setCtrlsManifestSpy,
+    } );
+    cy.stub( useControlsTabEffects ).returns( {
+      nodes: [],
+    } );
 
-    // Check if setCtrlsManifest was called with an empty array
-    cy.get("@setCtrlsManifest").should("have.been.calledOnceWith", []);
+    cy.mount( () => useControlsTabEffects() );
 
-    // Visual validation using Applitools
-    cy.eyesCheckWindow("Empty Controls Manifest");
-  });
-});
+    cy.wrap( setCtrlsManifestSpy ).should( "be.calledWith", [] );
+
+    cy.eyesCheckWindow( {
+      tag: "Empty nodes",
+      target: "window",
+      fully: true,
+    } );
+  } );
+
+  it( "should not call setCtrlsManifest when nodes length is not 0", () => {
+    const setCtrlsManifestSpy = cy.spy();
+
+    cy.stub( useControlsTabEffects ).returns( {
+      setCtrlsManifest: setCtrlsManifestSpy,
+    } );
+    cy.stub( useControlsTabEffects, ).returns( {
+      nodes: [ { id: 1 } ],
+    } );
+
+    cy.mount( () => useControlsTabEffects() );
+
+    cy.wrap( setCtrlsManifestSpy ).should( "not.be.called" );
+
+    cy.eyesCheckWindow( {
+      tag: "Non-empty nodes",
+      target: "window",
+      fully: true,
+    } );
+  } );
+} );
