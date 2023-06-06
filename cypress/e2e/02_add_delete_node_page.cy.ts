e51f4ef90e99de903d8ef896f4d7b1f1fe603292
Igor Grinberg <igor.grinberg@toptal.com>
2023-06-06 17:32:13 -0400

    Add Node Cypress test case fixes added

--- cypress/e2e/02_add_delete_node_page.cy.ts
+++ cypress/e2e/02_add_delete_node_page.cy.ts
@@ -1,27 +1,13 @@
 /// <reference types="cypress" />
 require('cypress-xpath')
 describe("Verify Add and Delete node", () => {
-  // This method performs setup before each test.
   beforeEach(() => {
-    // Open Eyes to start visual testing.
-    // Each test should open its own Eyes for its own snapshots.
     cy.eyesOpen({
-      // The name of the application under test.
-      // All tests for the same app should share the same app name.
-      // Set this name wisely: Applitools features rely on a shared app name across tests.
       appName: "studio",
-
-      // The name of the test case for the given application.
-      // Additional unique characteristics of the test may also be specified as part of the test name,
-      // such as localization information ("Home Page - EN") or different user permissions ("Login by admin").
       testName: Cypress.currentTest.title,
     });
   });
 
-  // The interactions use typical Cypress calls,
-  // but the verifications use one-line snapshot calls with Applitools Eyes.
-  // If the page ever changes, then Applitools will detect the changes and highlight them in the Eyes Test Manager.
-  // Traditional assertions that scrape the page for text values are not needed here.
   it("should open flojoy studio's main page", () => {
     cy.visit("/").wait(1000);
 
@@ -30,7 +16,7 @@ describe("Verify Add and Delete node", () => {
       target: "window",
       fully: true,
     });
-
+// Click add node
     cy.get('[data-testid="add-node-button"]').click();
 
     cy.eyesCheckWindow({
@@ -38,15 +24,20 @@ describe("Verify Add and Delete node", () => {
       target: "window",
       fully: true,
     });
+    //Select container Loaders
     cy.xpath("//div[contains(text(), 'Loaders')]").click();
+    // Select LOADER node
     cy.xpath("//button[.='LOADER']").click();
+    // Close sidebar
     cy.get('[data-testid="sidebar-close"]').click();
     cy.eyesCheckWindow({
       tag: "dark flow page with node loader",
       target: "window",
       fully: true,
     });
+    // Click on added container LOADER
     cy.xpath("//div[contains(text(), 'LOADER')]").click();
+    //Delete node LOADER
     cy.get('.tabler-icon-x[width=\'24\']').click();
     cy.eyesCheckWindow({
       tag: "dark flow page",
@@ -54,10 +45,7 @@ describe("Verify Add and Delete node", () => {
       fully: true,
     });
   });
-
-  // This method performs cleanup after each test.
   afterEach(() => {
-    // Close Eyes to tell the server it should display the results.
     cy.eyesClose();
   });
 });
