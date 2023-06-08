858ca35c5998510cafbc4136f4ff0be1e442f790
Jinwon Lee <jinwonlee0916@gmail.com>
2023-06-08 10:37:19 -0400

    format

--- src/feature/flow_chart_panel/components/custom-nodes/VisorNode.tsx
+++ src/feature/flow_chart_panel/components/custom-nodes/VisorNode.tsx
@@ -43,7 +43,7 @@ const chartElemMap: { [func: string]: JSX.Element } = {
   BOX: <BoxPlot />,
   BIG_NUMBER: <BigNumber />,
   MATRIX_VIEW: <PlotlyTable />,
-  ARRAY_VIEW : <PlotlyTable />,
+  ARRAY_VIEW: <PlotlyTable />,
 };
 
 const VisorNode = ({ data }: CustomNodeProps) => {
