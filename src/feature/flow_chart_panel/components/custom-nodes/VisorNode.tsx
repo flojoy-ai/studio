dbb1ab35e9110988aabb20ed13a1f25c1e0f1058
Ben Epstein <ben.epstein97@gmail.com>
2023-06-11 12:53:48 -0400

    prophet viz node

--- src/feature/flow_chart_panel/components/custom-nodes/VisorNode.tsx
+++ src/feature/flow_chart_panel/components/custom-nodes/VisorNode.tsx
@@ -43,6 +43,8 @@ const chartElemMap: { [func: string]: JSX.Element } = {
   BOX: <BoxPlot />,
   BIG_NUMBER: <BigNumber />,
   MATRIX_VIEW: <PlotlyTable />,
+  ARRAY_VIEW: <PlotlyTable />,
+  PROPHET: <LineChart />,
 };
 
 const VisorNode = ({ data }: CustomNodeProps) => {
