99a9771b2068800ed3b6548ebfe212303e6d8130
Youngsun Yoo <youngsun123q@gmail.com>
2023-06-15 19:15:09 +0900

    change style

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,13 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), EnvironmentPlugin("all"), eslint({ emitWarning: false })],
+  plugins: [
+    react(),
+    EnvironmentPlugin("all"),
+    eslint({
+      emitWarning: false,
+    }),
+  ],
   server: {
     port: 3000,
     watch: {
