c953fd296208941081b8cb894ac0c05f7a6fac29
Youngsun Yoo <youngsun123q@gmail.com>
2023-06-15 18:47:16 +0900

    disabled eslint warnings

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), EnvironmentPlugin("all"), eslint()],
+  plugins: [react(), EnvironmentPlugin("all"), eslint({ emitWarning: false })],
   server: {
     port: 3000,
     watch: {
