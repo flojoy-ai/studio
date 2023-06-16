1d09d93808e563aeaaf262259ad5a809083dd93e
dev.nested <dev.nested@gmail.com>
2023-06-15 23:14:18 -0500

    #529 pass the input names of the previous jobs to flojoy decorator
    It is backward compatible. It sends previous jobs dictionary in addition to previous job ids which were sent previously

--- PYTHON/services/job_service.py
+++ PYTHON/services/job_service.py
@@ -83,10 +83,8 @@ class JobService:
         iteration_id,
         ctrls,
         previous_job_ids,
-        input_job_ids=None,
+        previous_jobs,
     ):
-        input_job_ids = input_job_ids if input_job_ids is not None else previous_job_ids
-
         if Job.exists(job_id, self.redis_dao.r):
             job = Job.fetch(job_id, connection=self.redis_dao.r)
             job.delete()
@@ -98,12 +96,13 @@ class JobService:
             job_id=iteration_id,
             kwargs={
                 "ctrls": ctrls,
-                "previous_job_ids": input_job_ids,
+                "previous_job_ids": previous_job_ids,
+                "previous_jobs": previous_jobs,
                 "jobset_id": jobset_id,
                 "node_id": job_id,
                 "job_id": iteration_id,
             },
-            depends_on=previous_job_ids,
+            depends_on=[],
         )
         self.add_job(iteration_id, jobset_id)
 
