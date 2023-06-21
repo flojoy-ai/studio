c4cbac9c6d65cde1f11e9d28e35446e712c47fef
dev.nested <dev.nested@gmail.com>
2023-06-20 23:47:32 -0500

    fix flojoy python library version and rq worker debugging setup

--- PYTHON/rq_worker.py
+++ PYTHON/rq_worker.py
@@ -11,32 +11,34 @@ from dao.redis_dao import RedisDao
 
 sys.path.append(os.path.abspath(os.getcwd()))
 
+DEBUG_PORT_WORKER = 5678
+DEBUG_PORT_WATCH = 5679
 
-DEFAULT_PORT = 5679
 
-
-def debugger():
-    print("Checking whether to run debugger for rq watch")
+def debugger(port: int, worker_name: str):
     if os.environ.get("DEBUG", None) == "True":
-        port = int(os.environ.get("DEBUG_PORT", DEFAULT_PORT))
         try:
             endpoint = debugpy.listen(port)
-            print(f"Started debugger for rq watch on {endpoint}")
+            print(f"Started debug server for rq:worker:{worker_name} on {endpoint}")
             is_wait = os.environ.get("DEBUG_WAIT", False) == "True"
             if is_wait:
-                print(f"Waiting for debugger for rq watch to attach on port: {port}")
+                print(
+                    f"Waiting on debugger for rq:worker:{worker_name} to attach on port: {port}"
+                )
                 debugpy.wait_for_client()
         except Exception:
-            print("Failed to start debugging for rq watch or its already running")
+            print(
+                f"Failed to start debug server for rq:worker:{worker_name} or its already running"
+            )
     else:
-        print("Debugging for rq watch is off")
-
-
-debugger()
+        print(f"Debugging for rq:worker:{worker_name} is disabled")
 
 
 if __name__ == "__main__":
     worker_name = sys.argv[1]
+    debugger(
+        DEBUG_PORT_WORKER if worker_name == "flojoy" else DEBUG_PORT_WATCH, worker_name
+    )
     queue = Queue(worker_name, connection=RedisDao().r)
-    worker = Worker([queue], connection=RedisDao().r, name=worker_name)
+    worker = Worker([queue], connection=RedisDao().r)
     worker.work()
