bc1e673aa8e506d2ef887b985082dddc8287771c
angelchauffray <ac@enclose.solar>
2023-06-21 14:49:34 -0400

    added CPU RAM and modified CPU TEMP

--- src/utils/ManifestLoader.ts
+++ src/utils/ManifestLoader.ts
@@ -154,7 +154,8 @@ const CMND_TREE: CommandSection = {
         { title: "Serial", key: "SERIAL", children: null },
         { title: "Stepper driver Tic", key: "STEPPER", children: null },
         { title: "Stepper driver Tic knob", key: "STEPPER2", children: null },
-        { title: "Cpu Temp Ram", key: "CPU", children: null },
+        { title: "Cpu Temp", key: "CPU", children: null },
+        { title: "Cpu Ram", key: "CPUR", children: null },
       ],
     },
     {
