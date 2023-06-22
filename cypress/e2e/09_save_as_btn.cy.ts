95e14a5ffe002c24d5b4df1ba3a29d80aa72e6dd
Jinwon Lee <jinwonlee0916@gmail.com>
2023-06-22 14:58:18 -0400

    comment out file pickers

--- cypress/e2e/09_save_as_btn.cy.ts
+++ cypress/e2e/09_save_as_btn.cy.ts
@@ -1,39 +1,39 @@
-/// <reference types="cypress" />
+// /// <reference types="cypress" />
 
-describe("studio", () => {
-  // This method performs setup before each test.
-  beforeEach(() => {
-    // Open Eyes to start visual testing.
-    // Each test should open its own Eyes for its own snapshots.
-    cy.eyesOpen({
-      // The name of the application under test.
-      // All tests for the same app should share the same app name.
-      // Set this name wisely: Applitools features rely on a shared app name across tests.
-      appName: "studio",
+// describe("studio", () => {
+//   // This method performs setup before each test.
+//   beforeEach(() => {
+//     // Open Eyes to start visual testing.
+//     // Each test should open its own Eyes for its own snapshots.
+//     cy.eyesOpen({
+//       // The name of the application under test.
+//       // All tests for the same app should share the same app name.
+//       // Set this name wisely: Applitools features rely on a shared app name across tests.
+//       appName: "studio",
 
-      // The name of the test case for the given application.
-      // Additional unique characteristics of the test may also be specified as part of the test name,
-      // such as localization information ("Home Page - EN") or different user permissions ("Login by admin").
-      testName: Cypress.currentTest.title,
-    });
-  });
+//       // The name of the test case for the given application.
+//       // Additional unique characteristics of the test may also be specified as part of the test name,
+//       // such as localization information ("Home Page - EN") or different user permissions ("Login by admin").
+//       testName: Cypress.currentTest.title,
+//     });
+//   });
 
-  // The interactions use typical Cypress calls,
-  // but the verifications use one-line snapshot calls with Applitools Eyes.
-  // If the page ever changes, then Applitools will detect the changes and highlight them in the Eyes Test Manager.
-  // Traditional assertions that scrape the page for text values are not needed here.
+//   // The interactions use typical Cypress calls,
+//   // but the verifications use one-line snapshot calls with Applitools Eyes.
+//   // If the page ever changes, then Applitools will detect the changes and highlight them in the Eyes Test Manager.
+//   // Traditional assertions that scrape the page for text values are not needed here.
 
-  it("save as btn test", () => {
-    cy.visit("/").wait(1000);
+//   it("save as btn test", () => {
+//     cy.visit("/").wait(1000);
 
-    cy.get('[data-testid="btn-filebutton"]').trigger("mouseover");
+//     cy.get('[data-testid="btn-filebutton"]').trigger("mouseover");
 
-    //   cy.get('[data-testid="btn-saveas"]').click()
-  });
+//     //   cy.get('[data-testid="btn-saveas"]').click()
+//   });
 
-  // This method performs cleanup after each test.
-  afterEach(() => {
-    // Close Eyes to tell the server it should display the results.
-    cy.eyesClose();
-  });
-});
+//   // This method performs cleanup after each test.
+//   afterEach(() => {
+//     // Close Eyes to tell the server it should display the results.
+//     cy.eyesClose();
+//   });
+// });
