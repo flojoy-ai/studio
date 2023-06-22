b707e7d034059ac73eba136908ee98f503a2c38f
Youngsun Yoo <youngsun123q@gmail.com>
2023-06-22 15:36:09 -0400

    included code coverage for cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
     video: false,
     baseUrl: "http://localhost:3000/",
     setupNodeEvents(on, config) {
+      require("@cypress/code-coverage/task")(on, config);
       // implement node event listeners here
       on("task", {
         readExampleFiles() {
--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -14,6 +14,7 @@
 // ***********************************************************
 
 import "@applitools/eyes-cypress/commands";
+import "@cypress/code-coverage/support";
 
 // Import commands.js using ES2015 syntax:
 import "./commands";
