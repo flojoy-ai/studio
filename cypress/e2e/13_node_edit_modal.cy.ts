c1b6bb2101729d462d5fd57cdb804fc4a145df0a
Jinwon Lee <jinwonlee0916@gmail.com>
2023-06-26 17:23:59 -0400

    Added node edit test

---
+++ cypress/e2e/13_node_edit_modal.cy.ts
@@ -0,0 +1,46 @@
+/// <reference types="cypress" />
+
+describe("studio", () => {
+  // This method performs setup before each test.
+  beforeEach(() => {
+    // Open Eyes to start visual testing.
+    // Each test should open its own Eyes for its own snapshots.
+    cy.eyesOpen({
+      // The name of the application under test.
+      // All tests for the same app should share the same app name.
+      // Set this name wisely: Applitools features rely on a shared app name across tests.
+      appName: "studio",
+
+      // The name of the test case for the given application.
+      // Additional unique characteristics of the test may also be specified as part of the test name,
+      // such as localization information ("Home Page - EN") or different user permissions ("Login by admin").
+      testName: Cypress.currentTest.title,
+    });
+  });
+
+  // The interactions use typical Cypress calls,
+  // but the verifications use one-line snapshot calls with Applitools Eyes.
+  // If the page ever changes, then Applitools will detect the changes and highlight them in the Eyes Test Manager.
+  // Traditional assertions that scrape the page for text values are not needed here.
+
+  it("node edit modal/menu test", () => {
+    cy.visit("/").wait(1000);
+
+    // Click clear canvas button
+    cy.get(
+      '[data-testid="rf__node-SINE-2cd08316-0a0c-4c13-9b1d-382ba4d74cbd"]'
+    ).click();
+
+    cy.get('[data-testid="node-edit-menu"]').should("eq");
+
+    cy.get('[data-testid="node-edit-modal-params"]').should("have.length", 5);
+
+    cy.get('[data-testid="node-edit-modal-close-btn"]').click();
+  });
+
+  // This method performs cleanup after each test.
+  afterEach(() => {
+    // Close Eyes to tell the server it should display the results.
+    cy.eyesClose();
+  });
+});
