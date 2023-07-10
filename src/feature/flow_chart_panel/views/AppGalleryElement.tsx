0492a666517e7cae21ef5a42703cc581c2c77866
Jinwon Lee <jinwonlee0916@gmail.com>
2023-07-10 15:20:13 -0400

    Display app

--- src/feature/flow_chart_panel/views/AppGalleryElement.tsx
+++ src/feature/flow_chart_panel/views/AppGalleryElement.tsx
@@ -1,5 +1,12 @@
 import { createStyles, Image, Flex, UnstyledButton } from "@mantine/core";
+import { useControlsState } from "@src/hooks/useControlsState";
+import { useFlowChartGraph } from "@src/hooks/useFlowChartGraph";
 import { IconBrandYoutube } from "@tabler/icons-react";
+import { useFlowChartState } from "@hooks/useFlowChartState";
+import { AppGalleryModal } from "./AppGalleryModal";
+import raw from "./flojoy.json";
+import { ElementsData } from "../types/CustomNodeProps";
+import { ReactFlowJsonObject } from "reactflow";
 
 export interface AppGalleryElementProps {
   linkText: string;
@@ -39,21 +46,35 @@ export const AppGalleryElement = ({
   youtubeLink = "https://www.youtube.com",
 }: AppGalleryElementProps) => {
   const { classes } = AppGalleryElementStyles();
+  const { loadFlowExportObject } = useFlowChartGraph();
+  const { ctrlsManifest, setCtrlsManifest } = useControlsState();
+  const { setIsGalleryOpen } = useFlowChartState();
+
+  const open = async () => {
+    const file = "flojoy";
+    const raw = await import(`./${file}.json`);
+    const flow = raw.rfInstance as ReactFlowJsonObject;
+    setCtrlsManifest(raw.ctrlsManifest || ctrlsManifest);
+    loadFlowExportObject(flow);
+    setIsGalleryOpen(false);
+  };
 
   return (
-    <Flex className={classes.elementLayout}>
-      <Image height={120} width={"8.5vw"} fit="contain" src={imagePath} />
-      <h4 className={classes.elementTitle}>
-        {elementTitle}
-        <div className={classes.icon}>
-          <UnstyledButton component="a" target="_blank" href={youtubeLink}>
-            <IconBrandYoutube />
-          </UnstyledButton>
-        </div>
-      </h4>
-      <a href={link} className={classes.link}>
-        {linkText}
-      </a>
-    </Flex>
+    <UnstyledButton onClick={open}>
+      <Flex className={classes.elementLayout}>
+        <Image height={120} width={"8.5vw"} fit="contain" src={imagePath} />
+        <h4 className={classes.elementTitle}>
+          {elementTitle}
+          <div className={classes.icon}>
+            <UnstyledButton component="a" target="_blank" href={youtubeLink}>
+              <IconBrandYoutube />
+            </UnstyledButton>
+          </div>
+        </h4>
+        <a href={link} className={classes.link}>
+          {linkText}
+        </a>
+      </Flex>
+    </UnstyledButton>
   );
 };
