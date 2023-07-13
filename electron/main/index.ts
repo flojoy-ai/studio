0fa71ea737adffa00f78059f86bef9a8a674d5fb
Joey Yu <joey@itsjoeoui.com>
2023-07-13 19:22:02 -0400

    Only hide menu for prod

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -45,7 +45,7 @@ async function createWindow() {
   win = new BrowserWindow({
     title: "Main window",
     icon: join(process.env.PUBLIC, "favicon.ico"),
-    autoHideMenuBar: true,
+    autoHideMenuBar: app.isPackaged ? true : false,
     webPreferences: {
       preload,
       // Warning: Enable nodeIntegration and disable contextIsolation is not secure in production
