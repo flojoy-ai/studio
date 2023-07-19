5d02804ff9bacf454390b4e6ff35680c72aea382
Jinwon Lee <89884098+Jin1L@users.noreply.github.com>
2023-07-19 17:09:13 -0400

    Jinwonlee/stu 10 remove visual test temporarily (#715)

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -6,5 +6,5 @@ const BACKEND_PORT = process.env.VITE_BACKEND_PORT
   : 8000;
 export const API_URI = "http://" + BACKEND_HOST + ":" + BACKEND_PORT;
 const SOCKET_HOST = process.env.VITE_SOCKET_HOST || "127.0.0.1";
-export const SOCKET_URL = `ws://${SOCKET_HOST}:${BACKEND_PORT}/ws`
-export const REQUEST_NODE_URL = "https://toqo276pj36.typeform.com/to/F5rSHVu1"
+export const SOCKET_URL = `ws://${SOCKET_HOST}:${BACKEND_PORT}/ws`;
+export const REQUEST_NODE_URL = "https://toqo276pj36.typeform.com/to/F5rSHVu1";
