fec7927da7ecec2e04a768bd19478f8ae901fec0
Youngsun Yoo <youngsun123q@gmail.com>
2023-07-19 15:27:30 -0400

    completed API key modal styles

--- src/feature/flow_chart_panel/views/APIKeyModal.tsx
+++ src/feature/flow_chart_panel/views/APIKeyModal.tsx
@@ -136,25 +136,36 @@ const APIKeyModal = ({ isOpen, onClose }: APIKeyModelProps) => {
       <div className="fixed inset-0 bg-gray-900 bg-opacity-75 transition-opacity"></div>
       <div className="fixed inset-0 z-10 overflow-y-auto">
         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-          <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-            <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+          <div className="relative transform overflow-hidden rounded-lg text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+            <div className="bg-modal px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
               <div className="sm:flex sm:items-start">
-                <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                <div className="my-5 text-center sm:ml-4 sm:mt-0 sm:text-left">
                   <h3
-                    className="text-base font-semibold leading-6 text-gray-900"
+                    className="mb-4 flex text-base font-semibold leading-6 text-gray-300"
                     id="modal-title"
                   >
                     Environment Variable
                   </h3>
-                  <input
-                    className="block w-full rounded-md border border-slate-300 bg-white py-2 pl-9 pr-3 shadow-sm placeholder:italic placeholder:text-slate-400 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm"
-                    placeholder="e.g. CLIENT_KEY"
-                    type="text"
-                    name="search"
-                  />
+                  <div className="inline-block">
+                    <span className="text-accent1 sm:text-sm">Key:</span>
+                    <input
+                      className="mt-1 block w-full rounded-md border border-slate-900 py-2 pl-9 pr-3 shadow-sm placeholder:italic placeholder:text-slate-400 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm"
+                      placeholder="e.g. CLIENT_KEY"
+                      type="text"
+                      name="search"
+                    />
+                  </div>
+                  <div className="ml-4 inline-block">
+                    <span className="text-accent1 sm:text-sm">Value:</span>
+                    <input
+                      className="mt-1 block w-full rounded-md border border-slate-900 py-2 pl-9 pr-3 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm"
+                      type="text"
+                      name="search"
+                    />
+                  </div>
                 </div>
               </div>
-              <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+              <div className="px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                 <button
                   type="button"
                   className="inline-flex w-full justify-center rounded-md px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
