2208615c8a41dfc6e5c018cbaaa2efe260ef0185
Joey Yu <joey@itsjoeoui.com>
2023-07-20 19:30:02 -0400

    Get Electron working again (#588)
    * electron is working now and will cleanup later
    
    * Add dist-electron to gitignore
    
    * Update submodule
    
    * Maximize screen
    
    * Cleanup package.json and build
    
    * don't open devconsole by default
    
    * Add back mathjax script
    
    * Update build script
    
    * Reorganize and update packages
    
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>
    
    * Restore icons
    
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>
    
    * remove npm run open
    
    * remove node.svg
    
    * autoHideMenuBar
    
    * Update script and tsconfig
    
    * Add missing package
    
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>
    
    * Only hide menu for prod
    
    * FIx merge derp
    
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>
    
    * try
    
    * fix packages merge
    
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>
    
    * fix start scripts
    
    * Update submodules
    
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>
    
    ---------
    
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

--- vite.config.ts
+++ vite.config.ts
@@ -5,32 +5,92 @@ import eslint from "vite-plugin-eslint";
 import istanbul from "vite-plugin-istanbul";
 import path from "path";
 
+import { rmSync } from "node:fs";
+import electron from "vite-plugin-electron";
+import renderer from "vite-plugin-electron-renderer";
+import pkg from "./package.json";
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    istanbul({
-      include: ["src/**/*.ts", "src/**/*.tsx"],
-    }),
-    react(),
-    EnvironmentPlugin("all"),
-    eslint({
-      emitWarning: false,
-    }),
-  ],
-  server: {
-    port: 3000,
-    open: true,
-    watch: {
-      ignored: ["**/venv/**"],
+export default defineConfig(({ command }) => {
+  rmSync("dist-electron", { recursive: true, force: true });
+
+  const isServe = command === "serve";
+  const isBuild = command === "build";
+  const sourcemap = isServe || !!process.env.VSCODE_DEBUG;
+
+  return {
+    plugins: [
+      react(),
+      istanbul({
+        include: ["src/**/*.ts", "src/**/*.tsx"],
+      }),
+      EnvironmentPlugin("all"),
+      eslint({ emitWarning: true }),
+      electron([
+        {
+          // Main-Process entry file of the Electron App.
+          entry: "electron/main/index.ts",
+          onstart(options) {
+            if (process.env.VSCODE_DEBUG) {
+              console.log(
+                /* For `.vscode/.debug.script.mjs` */ "[startup] Electron App"
+              );
+            } else {
+              options.startup();
+            }
+          },
+          vite: {
+            build: {
+              sourcemap,
+              minify: isBuild,
+              outDir: "dist-electron/main",
+              rollupOptions: {
+                external: Object.keys(
+                  "dependencies" in pkg ? pkg.dependencies : {}
+                ),
+              },
+            },
+          },
+        },
+        {
+          entry: "electron/preload/index.ts",
+          onstart(options) {
+            // Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete,
+            // instead of restarting the entire Electron App.
+            options.reload();
+          },
+          vite: {
+            build: {
+              sourcemap: sourcemap ? "inline" : undefined, // #332
+              minify: isBuild,
+              outDir: "dist-electron/preload",
+              rollupOptions: {
+                external: Object.keys(
+                  "dependencies" in pkg ? pkg.dependencies : {}
+                ),
+              },
+            },
+          },
+        },
+      ]),
+      // Use Node.js API in the Renderer-process
+      renderer(),
+    ],
+    server: {
+      port: 3000,
+      open: false,
+      watch: {
+        ignored: ["**/venv/**"],
+      },
     },
-  },
-  resolve: {
-    alias: {
-      "@src": path.resolve(__dirname, "src"),
-      "@hooks": path.resolve(__dirname, "src/hooks"),
-      "@feature": path.resolve(__dirname, "src/feature"),
-      "@component": path.resolve(__dirname, "src/component"),
-      "@/": path.resolve(__dirname, "src/"),
+    resolve: {
+      alias: {
+        "@src": path.resolve(__dirname, "src"),
+        "@hooks": path.resolve(__dirname, "src/hooks"),
+        "@feature": path.resolve(__dirname, "src/feature"),
+        "@component": path.resolve(__dirname, "src/component"),
+        "@/": path.resolve(__dirname, "src/"),
+      },
     },
-  },
+  };
 });
