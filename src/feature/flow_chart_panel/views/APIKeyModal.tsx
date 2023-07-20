98234f2154e3724046d994d7fb845ba354192314
Jinwon Lee <jinwonlee0916@gmail.com>
2023-07-20 10:06:20 -0400

    Update modal css

--- src/feature/flow_chart_panel/views/APIKeyModal.tsx
+++ src/feature/flow_chart_panel/views/APIKeyModal.tsx
@@ -133,12 +133,12 @@ const APIKeyModal = ({ isOpen, onClose }: APIKeyModelProps) => {
       role="dialog"
       aria-modal="true"
     >
-      <div className="fixed inset-0 bg-gray-900 bg-opacity-75 transition-opacity"></div>
-      <div className="fixed inset-0 z-10 w-full overflow-y-auto">
-        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-          <div className="relative transform overflow-hidden rounded-lg text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-2xl">
+      <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+      <div className="fixed inset-0 z-10 overflow-y-auto">
+        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0 ">
+          <div className="relative transform overflow-hidden rounded-lg border-2 border-gray-500 text-left shadow-2xl transition-all sm:my-8 sm:w-full sm:max-w-2xl">
             <div
-              className="bg-modal px-4 pb-4 pt-5 sm:p-6 sm:pb-4"
+              className="h-52 h-52 bg-modal px-4 pb-4 pt-5 sm:p-6 sm:pb-4"
               id="defaultModal"
             >
               <div className="sm:flex sm:items-start">
