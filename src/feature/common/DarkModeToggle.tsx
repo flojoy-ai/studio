5075593c4c2f98e8715026c7c8ad2a10f9b88e6e
Joey Yu <joey@itsjoeoui.com>
2023-07-21 23:39:03 -0400

    WIP

--- src/feature/common/DarkModeToggle.tsx
+++ src/feature/common/DarkModeToggle.tsx
@@ -1,16 +1,22 @@
 import { useMantineColorScheme } from "@mantine/core";
+import { Button } from "@src/components/ui/button";
 import { Moon, Sun } from "lucide-react";
 
 export const DarkModeToggle = () => {
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
 
   return (
-    <button onClick={() => toggleColorScheme()} data-testid="darkmode-toggle">
+    <Button
+      size="icon"
+      variant="ghost"
+      onClick={() => toggleColorScheme()}
+      data-testid="darkmode-toggle"
+    >
       {colorScheme === "dark" ? (
         <Moon className="stroke-accent1" />
       ) : (
         <Sun className="stroke-accent1" />
       )}
-    </button>
+    </Button>
   );
 };
