f6f2b7b45bb49ee7f035ef3f2301d4daea53a0b7
Jinwon Lee <jinwonlee0916@gmail.com>
2023-07-24 12:23:42 -0400

    Removing applitools settings

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -13,7 +13,6 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 import "@cypress/code-coverage/support";
-import "@applitools/eyes-cypress/commands";
 
 // Import commands.js using ES2015 syntax:
 import "./commands";
