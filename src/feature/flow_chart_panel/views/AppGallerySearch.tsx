afa566f56d4c23a80635017f6c658c68ee5af062
Tyler Li <sh0risu179@outlook.com>
2023-07-30 18:30:50 -0700

    temp change for merge

--- src/feature/flow_chart_panel/views/AppGallerySearch.tsx
+++ src/feature/flow_chart_panel/views/AppGallerySearch.tsx
@@ -6,38 +6,75 @@ import { useFlowChartState } from "@hooks/useFlowChartState";
 import { useControlsState } from "@hooks/useControlsState";
 import Turnstone from "turnstone";
 
+interface listBox {
+  name: string;
+  displayField: string;
+  data: nodeName[];
+  id: string;
+  ratio: number;
+  searchType?: "startswith" | "contains";
+}
+interface nodeName {
+  name: string;
+  type: string;
+  parent: string;
+}
+
 const linkTest =
   "https://raw.githubusercontent.com/flojoy-io/docs/main/docs/nodes/AI_ML/CLASSIFICATION/ACCURACY/examples/EX1/app.txt";
 
-const listbox = {
-  name: "Popular Cities",
-  displayField: "name",
-  data: [
-    { name: "Paris, France", coords: "48.86425, 2.29416" },
-    { name: "Rome, Italy", coords: "41.89205, 12.49209" },
-    { name: "Orlando, Florida, United States", coords: "28.53781, -81.38592" },
-    { name: "London, England", coords: "51.50420, -0.12426" },
-    { name: "Barcelona, Spain", coords: "41.40629, 2.17555" },
-    {
-      name: "New Orleans, Louisiana, United States",
-      coords: "29.95465,-90.07507",
-    },
-    { name: "Chicago, Illinois, United States", coords: "41.85003,-87.65005" },
-    { name: "Manchester, England", coords: "53.48095,-2.23743" },
-  ],
-  id: "popular",
-  ratio: 1,
-};
+const listbox: listBox[] = [
+  {
+    name: "AI/ML",
+    displayField: "name",
+    data: [
+      {
+        name: "ACCURACY",
+        type: "CLASSIFICATION",
+        parent: "AI/ML",
+      },
+      {
+        name: "LEAST_SQUARES",
+        type: "REGRESSION",
+        parent: "AL/ML",
+      },
+    ],
+    id: "AI",
+    ratio: 1,
+    searchType: "startswith",
+  },
+  {
+    name: "TRANSFORMERS",
+    displayField: "name",
+    data: [
+      {
+        name: "FFT",
+        type: "SIGNAL_PROCESSING",
+        parent: "TRANSFORMERS",
+      },
+      {
+        name: "ADD",
+        type: "ARITHMETIC",
+        parent: "TRANSFORMERS",
+      },
+    ],
+    id: "TRANSFORMERS",
+    ratio: 1,
+    searchType: "startswith",
+  },
+];
 
 const style = {
   //input: "w-full border rounded border-gray-500 bg-gray-800 px-7 py-3 pl-10 outline-none",
   input:
-    "w-full h-12 border bg-stone-800 border-slate-300 py-2 pl-10 pr-7 text-xl outline-none rounded",
+    "w-full h-12 border bg-stone-800 dark:bg-stone-800 border-slate-300 py-2 pl-10 pr-7 text-xl outline-none rounded",
   listbox:
     "w-full bg-stone-800 sm:border sm:border-slate-300 sm:rounded text-left sm:mt-2 p-2 sm:drop-shadow-xl",
   item: "cursor-pointer overflow-hidden overflow-ellipsis",
   highlightedItem:
     "cursor-pointer rounded overflow-hidden bg-cyan-100 text-stone-700",
+  match: "font-semibold",
+  groupHeading: "cursor-default px-1.5 uppercase text-purple-300",
 };
 
 export const AppGallerySearch = () => {
@@ -50,8 +87,10 @@ export const AppGallerySearch = () => {
   const { setIsGalleryOpen } = useFlowChartState();
   const { ctrlsManifest, setCtrlsManifest } = useControlsState();
 
-  const handleKeyDown = async () => {
-    const response = await fetch(linkTest);
+  const handleSelect = async (selectItem: nodeName) => {
+    const response = await fetch(
+      `https://raw.githubusercontent.com/flojoy-io/docs/main/docs/nodes/${selectItem.parent}/${selectItem.type}/${selectItem.name}/examples/EX1/app.txt`
+    );
     const raw = await response.json();
     const flow = raw.rfInstance as ReactFlowJsonObject<ElementsData, any>;
     setCtrlsManifest(raw.ctrlsManifest || ctrlsManifest);
@@ -76,11 +115,14 @@ export const AppGallerySearch = () => {
       {/*  ></path>*/}
       {/*</svg>*/}
       <Turnstone
+        id="autocomplete"
         placeholder="Search node name"
         noItems="No node found"
         listbox={listbox}
+        listboxIsImmutable={true}
         styles={style}
-        onEnter={handleKeyDown}
+        onEnter={handleSelect}
+        onSelect={handleSelect}
         matchText={true}
       />
     </div>
