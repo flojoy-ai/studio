f3d5c4f3ee8ceeed95203e33a8b02a560293026d
JeffDotPng <zhangjeff2014@gmail.com>
2023-08-03 17:55:59 -0400

    Comments

--- cypress/e2e/14_load_app.cy.ts
+++ cypress/e2e/14_load_app.cy.ts
@@ -6,16 +6,18 @@ describe("Loading an app", () => {
   it("Can load app correctly", () => {
     cy.visit("/").wait(1000);
 
-    //test dark mode
+    // Open the dropdown menu and click the load app button
     cy.get('[data-testid="dropdown-button"]').click({ force: true });
-
     cy.get('[id="load-app-btn"]').click({ force: true });
+
+    // Upload the file
     cy.get("body")
       .get("input[type=file]")
       .selectFile("cypress/fixtures/load_test.txt", { force: true });
 
     cy.percySnapshot("Flow page after loading app");
 
+    // Check that the app contains exactly 1 node
     cy.get('[data-testid="node-wrapper"]').should("have.length", 1);
   });
 });
