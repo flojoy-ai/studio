c3089b585fc01e4e625418d6948a4be9384ac129
JeffDotPng <zhangjeff2014@gmail.com>
2023-08-03 17:55:27 -0400

    remove comment

--- cypress/e2e/14_load_app.cy.ts
+++ cypress/e2e/14_load_app.cy.ts
@@ -3,11 +3,6 @@
 //** Tests loading an app.*/
 
 describe("Loading an app", () => {
-  // The interactions use typical Cypress calls,
-  // but the verifications use one-line snapshot calls with Applitools Eyes.
-  // If the page ever changes, then Applitools will detect the changes and highlight them in the Eyes Test Manager.
-  // Traditional assertions that scrape the page for text values are not needed here.
-
   it("Can load app correctly", () => {
     cy.visit("/").wait(1000);
 
