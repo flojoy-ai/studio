20af4105cdfeaf52b556425003e5edb66dbb489e
Joey Yu <joey@itsjoeoui.com>
2023-08-04 12:28:33 -0400

    watch mode (#757)
    * watch mode
    
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>
    
    * Forgot that the eye icon existed
    
    * Add a toolip hover help page
    
    * Add tooltip
    
    * just to keep things simple
    
    ---------
    
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

---
+++ src/feature/flow_chart_panel/components/WatchBtn.tsx
@@ -0,0 +1,56 @@
+import { useFlowChartState } from "@src/hooks/useFlowChartState";
+import { useEffect, useState } from "react";
+import { Node, Edge } from "reactflow";
+import { ElementsData } from "flojoy/types";
+import { useFlowChartGraph } from "@src/hooks/useFlowChartGraph";
+import { Label } from "@src/components/ui/label";
+import { Switch } from "@src/components/ui/switch";
+import {
+  Tooltip,
+  TooltipProvider,
+  TooltipTrigger,
+  TooltipContent,
+} from "@/components/ui/tooltip";
+
+interface WatchBtnProps {
+  playFC: (nodes: Node<ElementsData>[], edges: Edge[]) => void;
+  cancelFC: () => void;
+}
+
+const WatchBtn = ({ playFC, cancelFC }: WatchBtnProps) => {
+  const { nodeParamChanged } = useFlowChartState();
+  const { nodes, edges } = useFlowChartGraph();
+  const [isWatching, setIsWatching] = useState(false);
+
+  useEffect(() => {
+    if (isWatching && nodeParamChanged) {
+      cancelFC();
+      playFC(nodes, edges);
+    }
+  }, [nodeParamChanged, isWatching]);
+
+  const handleClick = () => {
+    setIsWatching(!isWatching);
+  };
+
+  return (
+    <div className="flex items-center space-x-2">
+      <Switch checked={isWatching} onCheckedChange={handleClick} />
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>
+            <Label>Watch Mode</Label>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>
+              The flowchart will automatically run everytime you change a node
+              parameter
+            </p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    </div>
+  );
+};
+
+export default WatchBtn;
