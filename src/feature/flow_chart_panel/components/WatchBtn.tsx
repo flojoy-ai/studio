55f961823c283e619ddfc7694490d3778c5c98f3
Joey Yu <joey@itsjoeoui.com>
2023-08-04 12:17:18 -0400

    Add a toolip hover help page

--- src/feature/flow_chart_panel/components/WatchBtn.tsx
+++ src/feature/flow_chart_panel/components/WatchBtn.tsx
@@ -1,10 +1,16 @@
-import { Button } from "@src/components/ui/button";
 import { useFlowChartState } from "@src/hooks/useFlowChartState";
-import { Ban, Eye } from "lucide-react";
 import { useEffect, useState } from "react";
 import { Node, Edge } from "reactflow";
 import { ElementsData } from "flojoy/types";
 import { useFlowChartGraph } from "@src/hooks/useFlowChartGraph";
+import { Label } from "@src/components/ui/label";
+import { Switch } from "@src/components/ui/switch";
+import {
+  Tooltip,
+  TooltipProvider,
+  TooltipTrigger,
+  TooltipContent,
+} from "@/components/ui/tooltip";
 
 interface WatchBtnProps {
   playFC: (nodes: Node<ElementsData>[], edges: Edge[]) => void;
@@ -15,6 +21,7 @@ const WatchBtn = ({ playFC, cancelFC }: WatchBtnProps) => {
   const { nodeParamChanged } = useFlowChartState();
   const { nodes, edges } = useFlowChartGraph();
   const [isWatching, setIsWatching] = useState(false);
+  const [runOnceRightAfterToggle, setRunOnceRightAfterToggle] = useState(false);
 
   useEffect(() => {
     if (isWatching && nodeParamChanged) {
@@ -23,28 +30,36 @@ const WatchBtn = ({ playFC, cancelFC }: WatchBtnProps) => {
     }
   }, [nodeParamChanged, isWatching]);
 
+  useEffect(() => {
+    if (isWatching && runOnceRightAfterToggle) {
+      cancelFC();
+      playFC(nodes, edges);
+      setRunOnceRightAfterToggle(false);
+    }
+  }, [runOnceRightAfterToggle, isWatching]);
+
   const handleClick = () => {
     setIsWatching(!isWatching);
+    setRunOnceRightAfterToggle(true);
   };
 
-  return !isWatching ? (
-    <Button size="sm" variant="outline" onClick={handleClick}>
-      <Eye size="20" />
-      <div className="px-1"></div>
-      <div>Watch</div>
-    </Button>
-  ) : (
-    <Button
-      size="sm"
-      variant="destructive"
-      onClick={() => {
-        setIsWatching(false);
-      }}
-    >
-      <Ban size="20" />
-      <div className="px-1"></div>
-      <div>Cancel</div>
-    </Button>
+  return (
+    <div className="flex items-center space-x-2">
+      <Switch checked={isWatching} onCheckedChange={handleClick} />
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>
+            <Label>Watch Mode</Label>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>
+              The flowchart will automatically run everytime you change a node
+              parameter
+            </p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    </div>
   );
 };
 
