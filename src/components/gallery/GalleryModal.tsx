20f6334544984c666c52769620c9dcba17519597
Joey Yu <joey@itsjoeoui.com>
2023-08-08 21:35:26 -0400

    move the filter logic to outside

--- src/components/gallery/GalleryModal.tsx
+++ src/components/gallery/GalleryModal.tsx
@@ -12,7 +12,7 @@ import { getGalleryData } from "@src/utils/GalleryLoader";
 import { Separator } from "../ui/separator";
 import { GalleryElement } from "./GalleryElement";
 import { Input } from "../ui/input";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 type AppGalleryModalProps = {
   isGalleryOpen: boolean;
@@ -29,6 +29,23 @@ export const GalleryModal = ({
 
   const [searchQuery, setSearchQuery] = useState("");
   const data = getGalleryData();
+  const [filteredData, setFilteredData] = useState(Object.entries(data));
+
+  useEffect(() => {
+    setFilteredData(
+      Object.entries(data).map(([k, v]) => [
+        k,
+        v.filter(
+          (app) =>
+            app.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            app.description.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            app.relevantNodes.some((node) =>
+              node.name.toLowerCase().includes(searchQuery.toLowerCase())
+            )
+        ),
+      ])
+    );
+  }, [searchQuery, data]);
 
   return (
     <Dialog open={isGalleryOpen} onOpenChange={setIsGalleryOpen}>
@@ -60,36 +77,23 @@ export const GalleryModal = ({
         />
 
         <ScrollArea className="">
-          {Object.entries(data).map(([k, v]) => (
-            <div key={k}>
-              <div className="text-3xl font-bold">{k}</div>
-              <Separator className="my-1" />
-              <div className="grid grid-cols-2">
-                {v
-                  .filter(
-                    (app) =>
-                      app.title
-                        .toLowerCase()
-                        .includes(searchQuery.toLowerCase()) ||
-                      app.description
-                        .toLowerCase()
-                        .includes(searchQuery.toLowerCase()) ||
-                      app.relevantNodes.some((node) =>
-                        node.name
-                          .toLowerCase()
-                          .includes(searchQuery.toLowerCase())
-                      )
-                  )
-                  .map((app) => (
+          {filteredData
+            .filter(([_, v]) => v.length > 0)
+            .map(([k, v]) => (
+              <div key={k}>
+                <div className="text-3xl font-bold">{k}</div>
+                <Separator className="my-1" />
+                <div className="grid grid-cols-2">
+                  {v.map((app) => (
                     <GalleryElement
                       galleryApp={app}
                       setIsGalleryOpen={setIsGalleryOpen}
                     />
                   ))}
+                </div>
+                <div className="py-2" />
               </div>
-              <div className="py-2" />
-            </div>
-          ))}
+            ))}
         </ScrollArea>
       </DialogContent>
     </Dialog>
