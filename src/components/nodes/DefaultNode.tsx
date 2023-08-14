64386039cf9fe2c9066b181551657330519f45c4
JeffDotPng <zhangjeff2014@gmail.com>
2023-08-14 12:54:59 -0400

    fmt

--- src/components/nodes/DefaultNode.tsx
+++ src/components/nodes/DefaultNode.tsx
@@ -19,7 +19,7 @@ const DefaultNode = (props: CustomNodeProps) => {
     <NodeWrapper wrapperProps={props}>
       <div
         className={clsx(
-          "flex min-h-[160px] items-center justify-center rounded-2xl border-2 border-solid border-accent1 bg-accent1/5 break-words p-2",
+          "flex min-h-[160px] items-center justify-center break-words rounded-2xl border-2 border-solid border-accent1 bg-accent1/5 p-2",
           { "shadow-around shadow-accent1": isRunning || data.selected },
           { "shadow-around shadow-red-700": nodeError },
         )}
