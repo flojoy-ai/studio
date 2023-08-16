f314a8806eedf49f5f81fefa5dbea357bcc284b9
Jeff Zhang <47371088+JeffDotPng@users.noreply.github.com>
2023-08-16 14:55:14 -0400

    Allow node to return none (#803)
    * wip
    
    * Allow optional return value
    
    * wip
    
    * Allow optional return value
    
    * Print logs when debug mode is enabled with startup script (#802)
    
    * print logs when debug mode is enabled
    
    * set envs to print debug logs
    
    * CI fixes
    
    ---------
    
    Co-authored-by: Syed Mahbub <86223939+smahmed776@users.noreply.github.com>

--- src/feature/flow_chart_panel/hooks/useNodeTypes.tsx
+++ src/feature/flow_chart_panel/hooks/useNodeTypes.tsx
@@ -1,6 +1,6 @@
 import { useSocket } from "@src/hooks/useSocket";
 import { nodeTypesMap } from "@/components/nodes/nodeTypesMap";
-import React, { useMemo } from "react";
+import { useMemo } from "react";
 import { NodeTypes } from "reactflow";
 import { MouseEvent } from "react";
 
