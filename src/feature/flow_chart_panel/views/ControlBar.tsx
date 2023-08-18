359734f5ddfb96830a8e08c2058eb2ee22163ac8
Joey Yu <joey@itsjoeoui.com>
2023-08-18 13:23:51 -0400

    Code split control bar

--- src/feature/flow_chart_panel/views/ControlBar.tsx
+++ src/feature/flow_chart_panel/views/ControlBar.tsx
@@ -1,27 +1,12 @@
-import { IServerStatus } from "@src/context/socket.context";
-import { useFlowChartState } from "@src/hooks/useFlowChartState";
-import { useSocket } from "@src/hooks/useSocket";
-import {
-  cancelFlowChartRun,
-  saveAndRunFlowChartInServer,
-  saveFlowChartToLocalStorage,
-} from "@src/services/FlowChartServices";
-import { sendProgramToMix } from "@src/services/MixpanelServices";
 import localforage from "localforage";
 import { memo, useState } from "react";
 import "react-tabs/style/react-tabs.css";
-import { Edge, Node } from "reactflow";
-import PlayBtn from "../components/PlayBtn";
-import CancelBtn from "../components/CancelBtn";
-import { ElementsData } from "@/types";
 import KeyboardShortcutModal from "./KeyboardShortcutModal";
 import { NodeSettingsModal } from "./NodeSettingsModal";
-import { useSettings } from "@src/hooks/useSettings";
 import EnvVarModal from "./EnvVarModal";
 // import useKeyboardShortcut from "@src/hooks/useKeyboardShortcut";
 import SaveFlowChartBtn from "./SaveFlowChartBtn";
 import { DarkModeToggle } from "@src/feature/common/DarkModeToggle";
-import WatchBtn from "../components/WatchBtn";
 import {
   Menubar,
   MenubarContent,
@@ -35,6 +20,7 @@ import { EditorSettingsModal } from "./EditorSettingsModal";
 import { SaveAsButton } from "./ControlBar/SaveAsButton";
 import { LoadButton } from "./ControlBar/LoadButton";
 import { ExportResultButton } from "./ControlBar/ExportResultButton";
+import FlowControlButtons from "./ControlBar/FlowControlButtons";
 
 localforage.config({
   name: "react-flow",
@@ -45,55 +31,11 @@ localforage.config({
 // rerenders due to calling useFlowChartGraph.
 
 const ControlBar = () => {
-  const { states } = useSocket();
-  const { socketId, serverStatus } = states;
   const [isKeyboardShortcutOpen, setIsKeyboardShortcutOpen] =
     useState<boolean>(false);
   const [isEnvVarModalOpen, setIsEnvVarModalOpen] = useState<boolean>(false);
-  const { settings } = useSettings("backend");
   const [isNodeSettingsOpen, setIsNodeSettingsOpen] = useState(false);
   const [isEditorSettingsOpen, setIsEditorSettingsOpen] = useState(false);
-
-  const { rfInstance, setRfInstance, setNodeParamChanged } =
-    useFlowChartState();
-
-  const onRun = async (nodes: Node<ElementsData>[], edges: Edge[]) => {
-    if (rfInstance && rfInstance.nodes.length > 0) {
-      // Only update the react flow instance when required.
-      const updatedRfInstance = {
-        ...rfInstance,
-        nodes,
-        edges,
-      };
-      setRfInstance(updatedRfInstance);
-
-      saveFlowChartToLocalStorage(updatedRfInstance);
-      sendProgramToMix(rfInstance.nodes, true, false);
-      // setProgramResults([]);
-      saveAndRunFlowChartInServer({
-        rfInstance: updatedRfInstance,
-        jobId: socketId,
-        settings: settings.filter((setting) => setting.group === "backend"),
-      });
-      setNodeParamChanged(false);
-    } else {
-      alert(
-        "There is no program to send to server. \n Please add at least one node first.",
-      );
-    }
-  };
-
-  const cancelFC = () => {
-    if (rfInstance && rfInstance.nodes.length > 0) {
-      cancelFlowChartRun(rfInstance, socketId);
-    } else {
-      alert("There is no running job on server.");
-    }
-  };
-
-  const playBtnDisabled =
-    serverStatus === IServerStatus.CONNECTING ||
-    serverStatus === IServerStatus.OFFLINE;
 
   const handleUpdate = async () => {
     const resp = await fetch(`${API_URI}/update/`, {
@@ -139,15 +81,7 @@ const ControlBar = () => {
         isSettingsModalOpen={isEditorSettingsOpen}
       />
 
-      {playBtnDisabled || serverStatus === IServerStatus.STANDBY ? (
-        <PlayBtn onPlay={onRun} />
-      ) : (
-        <CancelBtn cancelFC={cancelFC} />
-      )}
-
-      <div className="px-0.5" />
-      <WatchBtn playFC={onRun} cancelFC={cancelFC} />
-      <div className="px-0.5" />
+      <FlowControlButtons />
 
       <div className="flex">
         <Menubar>
