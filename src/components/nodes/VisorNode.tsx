1784ffc04e6e37d9ee5a956d611f923c3dbf3d00
Tristan Britt <53545754+trbritt@users.noreply.github.com>
2023-08-19 12:13:50 -0400

    Added new image processing nodes with built-in visualization (#807)
    * added lipsum SVG for peak finder visualizers
    
    * added extrema determination svg
    
    * added region properties svg
    
    * formatting

--- src/components/nodes/VisorNode.tsx
+++ src/components/nodes/VisorNode.tsx
@@ -21,6 +21,8 @@ import Bar from "@/assets/nodes/Bar";
 import Table from "@/assets/nodes/Table";
 import Image from "@/assets/nodes/Image";
 import MarkDownText from "@/components/common/MarkDownText";
+import PeakFinder from "@/assets/nodes/PeakFinder";
+import RegionInspector from "@/assets/nodes/RegionInspector";
 import { useTheme } from "@src/providers/theme-provider";
 
 const chartElemMap: { [func: string]: React.JSX.Element } = {
@@ -40,6 +42,8 @@ const chartElemMap: { [func: string]: React.JSX.Element } = {
   PROPHET_COMPONENTS: <ProphetComponents />,
   COMPOSITE: <CompositePlot />,
   TEXT_VIEW: <Table />,
+  EXTREMA_DETERMINATION: <PeakFinder />,
+  REGION_PROPERTIES: <RegionInspector />,
 };
 
 const VisorNode = (props: CustomNodeProps) => {
