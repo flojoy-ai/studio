09a6e1f52189f9c0739ee39b120e6a5865ba9e0e
JeffDotPng <zhangjeff2014@gmail.com>
2023-08-22 17:59:03 -0400

    fix electron types

--- electron/api/fileSave.ts
+++ electron/api/fileSave.ts
@@ -5,7 +5,9 @@ export function saveFile(path: string, data: string) {
   writeFileSync(path, data);
 }
 
-export async function saveFileAs(data: string): Promise<string | undefined> {
+export async function saveFileAs(
+  data: string,
+): Promise<Electron.SaveDialogReturnValue> {
   const result = await ipcRenderer.invoke("show-save-as-dialog");
   if (result.path) {
     saveFile(result.path, data);
