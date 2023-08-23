072e72e11692489303778de5b7372054a5a953df
Tyler Li <sh0risu179@outlook.com>
2023-08-23 07:14:57 -0700

    temp commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,7 +21,10 @@ export default {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
+        background: {
+          DEFAULT: "hsl(var(--background))",
+          hover: "hsl(var(--background-hover))",
+        },
         foreground: "hsl(var(--foreground))",
         primary: {
           DEFAULT: "hsl(var(--primary))",
