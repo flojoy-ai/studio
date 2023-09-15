07d6c99e0a9c6132a8cad62179f31608d687960c
JeffDotPng <zhangjeff2014@gmail.com>
2023-09-15 15:51:07 -0400

    Gracefully handle websocket crashes

--- captain/internal/manager.py
+++ captain/internal/manager.py
@@ -6,10 +6,13 @@ from captain.models.topology import Topology
 from captain.types.worker import PoisonPill
 from typing import Any, Union
 import json
+import threading
 from captain.types.worker import WorkerJobResponse
 
 
 """ Acts as a bridge between backend components """
+
+socket_connection_lock = threading.Lock()
 
 
 class Manager(object):
@@ -33,13 +36,22 @@ class ConnectionManager:
 
     async def connect(self, websocket: WebSocket, socket_id: str):
         await websocket.accept()
-        self.active_connections_map[socket_id] = websocket
+
+        with socket_connection_lock:
+            self.active_connections_map[socket_id] = websocket
+
         logger.debug(
             f"Connected! Amt of active connections: {len(self.active_connections_map.keys())}"
         )
 
     async def disconnect(self, socket_id: str):
-        self.active_connections_map.__delitem__(socket_id)
+        with socket_connection_lock:
+            if socket_id not in self.active_connections_map:
+                logger.debug(f"Client {socket_id} was already disconnected, skipping")
+                return
+
+            del self.active_connections_map[socket_id]
+
         logger.debug(
             f"Client {socket_id} is Disconnected :( ! Amt of active connections: {len(self.active_connections_map.keys())}"
         )
@@ -49,10 +61,25 @@ class ConnectionManager:
         if not isinstance(message, WorkerJobResponse):
             return
 
-        socket_id = message.jobsetId
-        for _, connection in self.active_connections_map.items():
-            try:
-                await connection.send_text(json.dumps(message, cls=PlotlyJSONEncoder))
-            except RuntimeError:
-                logger.error("RuntimeError in broadcast")
-                await self.disconnect(socket_id=socket_id)
+        dead_connections = set()
+
+        with socket_connection_lock:
+            for id, connection in self.active_connections_map.items():
+                if id in dead_connections:
+                    continue
+
+                try:
+                    await connection.send_text(
+                        json.dumps(message, cls=PlotlyJSONEncoder)
+                    )
+                except RuntimeError:
+                    logger.error(
+                        f"RuntimeError in broadcast to {id}, adding to dead connections"
+                    )
+                    dead_connections.add(id)
+
+        for connection in dead_connections:
+            logger.error(
+                f"Removing dead connection {connection} from active connections"
+            )
+            await self.disconnect(socket_id=connection)
