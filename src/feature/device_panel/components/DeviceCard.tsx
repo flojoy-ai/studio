6e74ae110db316ddb3562fdcb0d71d6680908b25
JeffDotPng <zhangjeff2014@gmail.com>
2023-09-19 17:16:24 -0400

    make text wrap

--- src/feature/device_panel/components/DeviceCard.tsx
+++ src/feature/device_panel/components/DeviceCard.tsx
@@ -6,9 +6,9 @@ export type DeviceCardProps = {
 
 export const DeviceCard = ({ name, description, port }: DeviceCardProps) => {
   return (
-    <div className="w-64 rounded-md bg-secondary p-2">
+    <div className="w-72 rounded-md bg-secondary p-2">
       <h3 className="font-semibold text-muted-foreground">{name}</h3>
-      <div className="font-semibold">{port}</div>
+      <div className="max-w-fit break-all font-semibold">{port}</div>
       <div className="py-1" />
       <div className="text-sm text-muted-foreground">{description}</div>
     </div>
