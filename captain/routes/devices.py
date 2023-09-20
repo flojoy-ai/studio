33de5a380ce827fd8293838370e593576d8ec442
JeffDotPng <zhangjeff2014@gmail.com>
2023-09-20 14:52:43 -0400

    simplify

--- captain/routes/devices.py
+++ captain/routes/devices.py
@@ -9,12 +9,9 @@ router = APIRouter(tags=["devices"])
 @router.get("/devices")
 async def get_devices():
     device_finder = get_device_finder()
-    cameras = device_finder.get_cameras()
-    serial_devices = device_finder.get_serial_devices()
-    visa_devices = device_finder.get_visa_devices()
 
     return DeviceInfo(
-        cameras=cameras,
-        serialDevices=serial_devices,
-        visaDevices=visa_devices,
+        cameras=device_finder.get_cameras(),
+        serialDevices=device_finder.get_serial_devices(),
+        visaDevices=device_finder.get_visa_devices(),
     )
