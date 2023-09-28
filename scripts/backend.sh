fc6114f81d499d95d136c7924ed9421410c3af41
Syed Mahbub <xsmahmed776@gmail.com>
2023-09-28 01:33:22 +0100

    working on Linux

--- scripts/backend.sh
+++ scripts/backend.sh
@@ -5,19 +5,12 @@ current_dir="$(dirname "$(readlink -f "$0")")"
 flojoy_dir="$HOME/.flojoy"
 mamba_dir="$flojoy_dir/mamba"
 python_exec="$mamba_dir/bin/python3"
-venv_name="404fc545_flojoy"
-venv_dir="$flojoy_dir/flojoy_root_venv"
-venv_path="$venv_dir/$venv_name"
-
-if [ "$(uname)" == "Darwin" ]; then
-  # if [ "$(uname -m)" == "x86_64" ]; then
-    mamba_installer="$current_dir/mamba/mamba_mac_x86_64.sh"
-  # fi
-elif [ "$(uname)" == "Linux" ]; then
-  mamba_installer="$current_dir/mamba/mamba_linux_x86_64.sh"
-else
-  echo "Unsupported operating system"
-fi
+venv_name="flojoy_root"
+venv_dir="$mamba_dir/envs/$venv_name"
+venv_executable="$venv_dir/bin/python"
+platform=$(uname)
+arch=$(uname -m)
+mamba_executable="$current_dir/bin/micromamba-$platform-$arch"
 
 echo "flojoy dir: $flojoy_dir"
 
@@ -26,30 +19,38 @@ if [ ! -d "$flojoy_dir" ]; then
   mkdir "$flojoy_dir"
 fi
 
-if [ ! -f "$python_exec" ]; then
-  if [ -d "$mamba_dir" ]; then
-    rm -rf $mamba_dir
-  fi
-  echo "Installing mamba to local directory..."
-  # Extract the zip file to the destination directory
-  $mamba_installer -b
-fi
-
 cd "$flojoy_dir"
 echo "Location set to $flojoy_dir"
-if [ ! -d "$venv_dir" ]; then
-  echo "$venv_dir doesn't exist.."
-  mkdir "$venv_dir"
-  echo "Created virtual env directory: $venv_dir"
+
+if [ ! -f "$venv_executable" ]; then
+  if [ -d "$venv_dir" ]; then
+    rm -rf $venv_dir
+  fi
+  echo "Creating micromamba env..."
+  cmd="$mamba_executable create -n $venv_name conda-forge::python=3.10 -r $mamba_dir -y"
+  $cmd
+  if [ "$?" -eq 0 ]; then
+      echo "Micromamba env $venv_name created successfully."
+  else
+    echo "Micromamba env creation failed."
+    exit 1
+  fi
 fi
-cd "$venv_dir"
-echo "location set to $venv_dir"
-if [ ! -d "$venv_path" ]; then
-  echo "Virtual env not found, creating a virtual env at $venv_dir"
-  $python_exec -m venv $venv_name
+
+if [ $platform == "Linux" ]; then
+  # Linux/bash:
+  eval "$($mamba_executable shell hook --shell bash)"
+  # sourcing the bashrc file incorporates the changes into the running session.
+  # better yet, restart your terminal!
+  source ~/.bashrc
+elif [ $platform == "Darwin" ]; then
+  # macOS/zsh:
+  eval "$($mamba_executable shell hook --shell zsh)"
+  source ~/.zshrc
 fi
-source $venv_name/bin/activate
-echo "Virtual env: $venv_name is activated!"
+export MAMBA_ROOT_PREFIX=$mamba_dir
+micromamba activate $venv_name
+echo "Env $venv_name is activated!"
 cd "$current_dir"
 echo "Installing pip dependencies..."
 pip install -r requirements.txt
--- scripts/get_micromamba.sh
+++ scripts/get_micromamba.sh
@@ -16,16 +16,16 @@ download_and_rename() {
 MICROMAMBA_URL="https://micro.mamba.pm/api/micromamba"
 
 # Linux Intel (x86_64)
-download_and_rename "$MICROMAMBA_URL/linux-64/latest" "linux-64"
+download_and_rename "$MICROMAMBA_URL/linux-64/latest" "Linux-x86_64"
 
 # Linux ARM64
-download_and_rename "$MICROMAMBA_URL/linux-aarch64/latest" "linux-aarch64" 
+download_and_rename "$MICROMAMBA_URL/linux-aarch64/latest" "Linux-arm64" 
 
 # Linux Power
-download_and_rename "$MICROMAMBA_URL/linux-ppc64le/latest" "linux-ppc64le" 
+download_and_rename "$MICROMAMBA_URL/linux-ppc64le/latest" "Linux-ppc64le" 
 
 # macOS Intel (x86_64)
-download_and_rename "$MICROMAMBA_URL/osx-64/latest" "osx-64"
+download_and_rename "$MICROMAMBA_URL/osx-64/latest" "Darwin-x86_64"
 
 # macOS Silicon/M1 (ARM64)
-download_and_rename "$MICROMAMBA_URL/osx-arm64/latest" "osx-arm64" 
+download_and_rename "$MICROMAMBA_URL/osx-arm64/latest" "Darwin-arm64" 
