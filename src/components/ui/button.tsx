09129009ad770fa6afc6fc9102fe39e87d900b8d
Joey Yu <joey@jyu.dev>
2023-09-29 15:47:53 -0400

    Small refactor

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -23,9 +23,8 @@ const buttonVariants = cva(
                 bg-background px-6 py-3 
                 font-semibold uppercase text-foreground 
                 transition-all duration-300 
-                hover:translate-x-[-4px] hover:translate-y-[-4px] hover:rounded-md 
-                hover:shadow-[4px_4px_0px_0px] hover:shadow-foreground  
-                active:translate-x-[0px] active:translate-y-[0px] 
+                hover:rounded-md shadow-[4px_4px_0px_0px] shadow-foreground  
+                active:translate-x-[4px] active:translate-y-[4px] 
                 active:rounded-2xl active:shadow-none`,
         // neumorphism:
         //   "px-4 py-2 rounded-full flex items-center gap-2 text-slate-500 shadow-[-5px_-5px_10px_rgba(255,_255,_255,_0.8),_5px_5px_10px_rgba(0,_0,_0,_0.25)] transition-all hover:shadow-[-1px_-1px_5px_rgba(255,_255,_255,_0.6),_1px_1px_5px_rgba(0,_0,_0,_0.3),inset_-2px_-2px_5px_rgba(255,_255,_255,_1),inset_2px_2px_4px_rgba(0,_0,_0,_0.3)] hover:text-violet-500",
