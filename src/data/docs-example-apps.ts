f52baa0179c606e520eb1244b45338dab4378c99
marvinirwin <marvin@marvinirwin.com>
2023-09-29 00:18:31 -0700

    added docs-example-apps.ts

---
+++ src/data/docs-example-apps.ts
@@ -0,0 +1,3 @@
+// This file is filled during cloud demo build script by cloning docs and reading its examples.  See Dockerfile and
+// scripts/build-cloud-demo.sh
+export const ExampleProjects = {}
