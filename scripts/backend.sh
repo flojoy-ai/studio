0c25384f66f0d811f3c483fc6695d77411082736
Joey Yu <joey@jyu.dev>
2023-10-08 19:02:32 -0400

    Working on macOS

--- scripts/backend.sh
+++ scripts/backend.sh
@@ -1,65 +1,63 @@
-#!/bin/bash
-
-feedback()
-{
-   is_successful=$1
-   message=$2
-   help_message=$3
-   if [ "$is_successful" -eq 0 ]; then
-      echo "$message"
-   else
-      echo "$help_message"
-      exit 1
-   fi
+feedback() {
+	is_successful=$1
+	message=$2
+	help_message=$3
+	if [ "$is_successful" -eq 0 ]; then
+		echo "$message"
+	else
+		echo "$help_message"
+		exit 1
+	fi
 }
+
+# Need to source them because the "conda" command is actually a bash function
+if [ -f ~/.zshrc ]; then
+	source ~/.zshrc
+	echo "Sourced ~/.zshrc"
+fi
+
+if [ -f ~/.bashrc ]; then
+	source ~/.bashrc
+	echo "Sourced ~/.bashrc"
+fi
 
 current_dir="$(dirname "$(readlink -f "$0")")"
 
+# Creating the .flojoy folder
 flojoy_dir="$HOME/.flojoy"
-mamba_dir="$flojoy_dir/mamba"
-python_exec="$mamba_dir/bin/python3"
-venv_name="flojoy_root"
-venv_dir="$mamba_dir/envs/$venv_name"
-venv_executable="$venv_dir/bin/python"
-platform=$(uname)
-arch=$(uname -m)
-mamba_executable="$current_dir/bin/micromamba-$platform-$arch"
-
 echo "flojoy dir: $flojoy_dir"
 
 if [ ! -d "$flojoy_dir" ]; then
-  echo "Flojoy directory doesn't exist, Creating $flojoy_dir ... "
-  mkdir "$flojoy_dir"
+	echo "Flojoy directory doesn't exist, Creating $flojoy_dir ... "
+	mkdir "$flojoy_dir"
 fi
 
-cd "$flojoy_dir"
-echo "Location set to $flojoy_dir"
+eval "$(conda shell.bash hook)" # configure the shell properly
+conda --version
 
-if [ ! -f "$venv_executable" ]; then
-  if [ -d "$venv_dir" ]; then
-    rm -rf "$venv_dir"
-  fi
-  echo "Creating micromamba env..."
-  "$mamba_executable" create -n $venv_name conda-forge::python=3.10 -r $mamba_dir -y
-  feedback $? "Micromamba env $venv_name created successfully." "Micromamba env creation failed."
+if [ ! $? -eq 0 ]; then
+	echo "Conda is not installed on your system."
+	echo "Please install Conda before proceeding."
+	exit 1
 fi
 
-if [ $platform == "Linux" ]; then
-  eval "$("$mamba_executable" shell hook --shell bash)"
-  export MAMBA_ROOT_PREFIX=$mamba_dir
-  micromamba activate $venv_name
-  feedback $? "Env $venv_name is activated!" "Failed to activate $venv_name env!"
-  cd "$current_dir"
-  echo "Installing pip dependencies..."
-  pip install -r requirements.txt
-  echo "Package installation completed, starting backend..."
-  export ELECTRON_MODE=packaged
-  python manage.py
-elif [ $platform == "Darwin" ]; then
-  cd "$current_dir"
-  echo "Installing pip dependencies..."
-  "$venv_executable" -m pip install -r requirements.txt
-  echo "Package installation completed, starting backend..."
-  export ELECTRON_MODE=packaged
-  "$venv_executable" manage.py
+cd $current_dir
+
+# Bootstrapping using conda
+ENV_NAME="flojoy-studio"
+YAML_FILE="environment.yml"
+
+conda activate $ENV_NAME
+# Check if the Conda environment exists
+if [ $? -eq 0 ]; then
+	# Environment exists, update it
+	conda env update --file $YAML_FILE --name $ENV_NAME
+else
+	# Environment doesn't exist, create it
+	conda env create --file $YAML_FILE --name $ENV_NAME
+	conda activate $ENV_NAME
 fi
+
+export ELECTRON_MODE=packaged
+poetry install
+poetry run python3 manage.py
