b9003e0ecdae0880752dae700170e3cf9636a1d2
JeffDotPng <zhangjeff2014@gmail.com>
2023-11-10 14:30:25 -0500

    allow passing custom cleanup function for hardware

--- pkgs/flojoy/flojoy/parameter_types.py
+++ pkgs/flojoy/flojoy/parameter_types.py
@@ -1,4 +1,4 @@
-from typing import Any, Union
+from typing import Any, Union, Callable
 from abc import ABC
 
 
@@ -18,12 +18,17 @@ class HardwareDevice(ABC):
 
 class HardwareConnection(ABC):
     _handle: Any
+    _cleanup: Callable[[Any], Any]
 
-    def __init__(self, handle: Any) -> None:
+    def __init__(self, handle: Any, cleanup: Callable[[Any], Any]) -> None:
         self._handle = handle
+        self.cleanup = cleanup
 
     def get_handle(self):
         return self._handle
+
+    def __del__(self):
+        self._cleanup(self._handle)
 
 
 class CameraDevice(HardwareDevice):
@@ -45,23 +50,39 @@ class NIDevice(HardwareDevice):
 
 
 class CameraConnection(HardwareConnection):
-    def __del__(self):
-        self._handle.release()
+    def __init__(
+        self, handle: Any, cleanup: Callable[[Any], Any] | None = None
+    ) -> None:
+        super().__init__(
+            handle, lambda conn: conn.release() if cleanup is None else cleanup
+        )
 
 
 class SerialConnection(HardwareConnection):
-    def __del__(self):
-        self._handle.close()
+    def __init__(
+        self, handle: Any, cleanup: Callable[[Any], Any] | None = None
+    ) -> None:
+        super().__init__(
+            handle, lambda conn: conn.close() if cleanup is None else cleanup
+        )
 
 
 class VisaConnection(HardwareConnection):
-    def __del__(self):
-        self._handle.close()
+    def __init__(
+        self, handle: Any, cleanup: Callable[[Any], Any] | None = None
+    ) -> None:
+        super().__init__(
+            handle, lambda conn: conn.close() if cleanup is None else cleanup
+        )
 
 
 class NIConnection(HardwareConnection):
-    def __del__(self):
-        self._handle.close()
+    def __init__(
+        self, handle: Any, cleanup: Callable[[Any], Any] | None = None
+    ) -> None:
+        super().__init__(
+            handle, lambda conn: conn.close() if cleanup is None else cleanup
+        )
 
 
 class NodeReference:
