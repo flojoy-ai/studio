b551b7c0a04cd3544a18ed0ea9b5efca5f587c75
JeffDotPng <zhangjeff2014@gmail.com>
2023-11-16 13:57:19 -0500

    fix types

--- src/renderer/data/RECIPES.ts
+++ src/renderer/data/RECIPES.ts
@@ -1,11 +1,9 @@
 import { ElementsData } from "@src/types";
-import { ReactFlowJsonObject, Node } from "reactflow";
+import { ReactFlowJsonObject } from "reactflow";
 import { defaultApp } from "./apps";
 
 export const NOISY_SINE =
   defaultApp.rfInstance as ReactFlowJsonObject<ElementsData>;
-
-export const bruh = defaultApp.rfInstance.nodes[0].data as ElementsData;
 
 export const EMPTY_CANVAS = {
   elements: [],
