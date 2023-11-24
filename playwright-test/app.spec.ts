44279b4530c8b19577369fafbb5cf924f82a44f9
smahmed776 <xsmahmed776@gmail.com>
2023-11-24 19:12:24 +0000

    get default executable path from dist folder

--- playwright-test/app.spec.ts
+++ playwright-test/app.spec.ts
@@ -7,12 +7,15 @@ const { version } = JSON.parse(
 );
 
 test("Startup test", async () => {
-  const electronApp = await electron.launch({ args: ["./out/main/index.js"] });
+  const executablePath = getExecutablePath();
+  const electronApp = await electron.launch({
+    executablePath,
+  });
   const isPackaged = await electronApp.evaluate(async ({ app }) => {
     return app.isPackaged;
   });
 
-  expect(isPackaged).toBe(false);
+  expect(isPackaged).toBe(true);
 
   const window = await electronApp.firstWindow();
   await window.waitForLoadState("domcontentloaded");
@@ -24,3 +27,17 @@ test("Startup test", async () => {
   // close app
   await electronApp.close();
 });
+
+const getExecutablePath = () => {
+  switch (process.platform) {
+    case "darwin":
+      return join(
+        process.cwd(),
+        "dist/mac-universal/Flojoy Studio.app/Contents/MacOS/Flojoy Studio",
+      );
+    case "win32":
+      return join(process.cwd(), "dist/resources/Flojoy Studio.exe");
+    default:
+      return "";
+  }
+};
