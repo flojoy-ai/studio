54b1ce2fd4089b7e622822c1fe8fcefa1f029916
JeffDotPng <zhangjeff2014@gmail.com>
2023-11-24 16:07:47 -0500

    use regex instead

--- src/renderer/utils/parse-error.ts
+++ src/renderer/utils/parse-error.ts
@@ -1,7 +1,7 @@
 export const parseElectronError = (err: string) => {
   const removeDefaultMsg = err
-    .replace("Error:", "")
-    .replace("Error occurred in handler for", "")
-    .replace("Error invoking remote method", "");
+    .replace(/^(Error:)/, "")
+    .replace(/^(Error occurred in handler for)/, "")
+    .replace(/^(Error invoking remote method)/, "");
   return removeDefaultMsg.split(":").slice(1).join(":") ?? "";
 };
