2a848cd95c1bd3ee4a46bfb94a00fea457d8d7e3
JeffDotPng <zhangjeff2014@gmail.com>
2023-11-24 17:46:52 -0500

    Add loading state for ping

--- src/renderer/routes/device_panel/components/PingTab.tsx
+++ src/renderer/routes/device_panel/components/PingTab.tsx
@@ -2,17 +2,23 @@ import { ScrollArea } from "@radix-ui/react-scroll-area";
 import { Button } from "@src/components/ui/button";
 import { Input } from "@src/components/ui/input";
 import { Label } from "@src/components/ui/label";
+import { Spinner } from "@src/components/ui/spinner";
 import { parseElectronError } from "@src/utils/parse-error";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-type PingResult = "success" | "failed";
+type PingResult = "success" | "failed" | "waiting";
 
 export const PingTab = () => {
   const [addr, setAddr] = useState("");
   const [output, setOutput] = useState<string | undefined>(undefined);
   const [status, setStatus] = useState<PingResult | undefined>(undefined);
 
+  useEffect(() => {
+    setStatus(undefined);
+  }, [])
+
   const ping = async () => {
+    setStatus("waiting");
     try {
       // TODO: Sanitize user input (security vulnerability)
       const out = await window.api.ping(addr);
@@ -43,16 +49,20 @@ export const PingTab = () => {
             value={addr}
           />
         </div>
-        <Button onClick={ping}>Ping</Button>
+        <Button onClick={ping} disabled={status === "waiting"}>Ping</Button>
       </div>
       <div className="py-1" />
-      <div>{statusStr}</div>
-      <div className="py-2" />
-      {output && (
-        <ScrollArea className="h-64 p-2 bg-popover">
-          <code className="whitespace-pre">{output}</code>
-        </ScrollArea>
-      )}
+      {status !== "waiting" ? (
+        <>
+          <div>{statusStr}</div>
+          <div className="py-2" />
+          {output && (
+            <ScrollArea className="h-64 p-2 bg-popover">
+              <code className="whitespace-pre">{output}</code>
+            </ScrollArea>
+          )}
+        </>
+      ) : <div className="flex justify-center items-center"><Spinner /></div>}
     </div>
   );
 };
