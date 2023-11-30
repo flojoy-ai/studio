4603e71ef080c878aa0595a362d24ad7c8ad5119
JeffDotPng <zhangjeff2014@gmail.com>
2023-11-30 12:59:11 -0500

    fix text positioning

--- src/renderer/routes/device_panel/components/ConnectionHelp.tsx
+++ src/renderer/routes/device_panel/components/ConnectionHelp.tsx
@@ -67,13 +67,15 @@ export const ConnectionHelp = () => {
                   <div className="text-center text-muted-foreground text-sm mt-1">Connect with an USB ("serial") cable</div>
                 </div>
               </div>
-              <div className="py-2" />
-              <div className="text-lg h-full">
-                <span className="font-bold">Step 1: </span>
-                Connect your computer to your hardware device with an Ethernet or USB cable
-              </div>
-              <div className="text-xs absolute bottom-0">
-                * Today, most hardware instruments have Ethernet or USB ports. If your instruments are older, they may only have <Link href="https://en.wikipedia.org/wiki/IEEE-488">GPIB (“IEEE-488”)</Link> or <Link href="https://en.wikipedia.org/wiki/RS-232">RS-232 ports</Link>. In this case, we highly recommend a GPIB-to-USB controller such the <Link href="https://prologix.biz/product/gpib-usb-controller/">Prologix</Link> or RS232-to-USB controller such as the <Link href="https://www.startech.com/en-ca/cards-adapters/icusb232v2">StarTech</Link>. Please see our guides on <Link href="https://docs.flojoy.ai/prologix/gpib-to-usb/">GPIB-to-USB</Link> and <Link href="https://docs.flojoy.ai/startech/rs232-to-usb/">RS232-to-USB</Link> for more info.
+              <div className="absolute bottom-0">
+                <div className="text-lg h-full">
+                  <span className="font-bold">Step 1: </span>
+                  Connect your computer to your hardware device with an Ethernet or USB cable
+                </div>
+                <div className="py-2" />
+                <div className="text-xs">
+                  * Today, most hardware instruments have Ethernet or USB ports. If your instruments are older, they may only have <Link href="https://en.wikipedia.org/wiki/IEEE-488">GPIB (“IEEE-488”)</Link> or <Link href="https://en.wikipedia.org/wiki/RS-232">RS-232 ports</Link>. In this case, we highly recommend a GPIB-to-USB controller such the <Link href="https://prologix.biz/product/gpib-usb-controller/">Prologix</Link> or RS232-to-USB controller such as the <Link href="https://www.startech.com/en-ca/cards-adapters/icusb232v2">StarTech</Link>. Please see our guides on <Link href="https://docs.flojoy.ai/prologix/gpib-to-usb/">GPIB-to-USB</Link> and <Link href="https://docs.flojoy.ai/startech/rs232-to-usb/">RS232-to-USB</Link> for more info.
+                </div>
               </div>
             </TabsContent>
             <TabsContent className="h-80 relative" value="find">
@@ -92,13 +94,15 @@ export const ConnectionHelp = () => {
                   </div>
                 </div>
               </div>
-              <div className="py-7" />
-              <div className="text-lg h-full">
-                <span className="font-bold">Step 2: </span>
-                Find your device under the "Hardware Devices" tab
-              </div>
-              <div className="text-xs absolute bottom-0">
-                After your instrument is physically connected to your computer via a USB or ethernet cable (see Step 1), then it should be discoverable by Flojoy and listed under the “Hardware Devices” tab. You may have to click the “Refresh” button for it to appear. Devices connected by Ethernet will show up under VISA; deviced connected by USB will show up under Serial.
+              <div className="absolute bottom-0">
+                <div className="text-lg h-full">
+                  <span className="font-bold">Step 2: </span>
+                  Find your device under the "Hardware Devices" tab
+                </div>
+                <div className="py-2" />
+                <div className="text-xs">
+                  After your instrument is physically connected to your computer via a USB or ethernet cable (see Step 1), then it should be discoverable by Flojoy and listed under the “Hardware Devices” tab. You may have to click the “Refresh” button for it to appear. Devices connected by Ethernet will show up under VISA; deviced connected by USB will show up under Serial.
+                </div>
               </div>
             </TabsContent>
             <TabsContent className="h-80 relative" value="debug">
@@ -106,26 +110,31 @@ export const ConnectionHelp = () => {
                 <img src="assets/connectionHelp/debug1.png" width={360} />
               </div>
               <div className="py-2" />
-              <div className="text-lg h-full">
-                <span className="font-bold">Step 3: </span>
-                Debug your connection
-              </div>
-              <div className="text-xs absolute bottom-0">
-                If your connected device does not show up under the Hardware Devices tab, then click the “Debug” button to troubleshoot. Please see our <Link href="https://docs.flojoy.ai/studio/visa-usb/">troubleshooting FAQ</Link> and join our free support chat where an engineer will help you through this initial hurdle.
+              <div className="absolute bottom-0">
+                <div className="text-lg h-full">
+                  <span className="font-bold">Step 3: </span>
+                  Debug your connection
+                </div>
+                <div className="py-4" />
+                <div className="text-xs">
+                  If your connected device does not show up under the Hardware Devices tab, then click the “Debug” button to troubleshoot. Please see our <Link href="https://docs.flojoy.ai/studio/visa-usb/">troubleshooting FAQ</Link> and join our free support chat where an engineer will help you through this initial hurdle.
+                </div>
               </div>
             </TabsContent>
             <TabsContent className="h-96 relative" value="import">
               <div className="flex justify-center">
                 <img src="assets/connectionHelp/import1.png" width={360} />
               </div>
-              <div className="py-4" />
-              <div className="text-lg h-full">
-                <span className="font-bold">Step 4: </span>
-                Import a connection block
-              </div>
-              <div className="text-xs absolute bottom-0">
-                Supported Flojoy hardware will have a connection block that creates the initial connection with the instrument. To find this block, go to the Add Block sidebar and search the instrument name or manufacturer (example searches: Rigol, Tektronix, Keysight, etc). USB-connected devices have general-purpose OPEN_SERIAL and SERIAL_WRITE blocks that can be used for any USB-connected device. If you don’t see blocks for your instrument, please inquire in our free support chat. <br /><br />
-                After, adding the connection block to your canvas, please click on it to verify that the correct connect device has been autoselected. If n/a is displayed under “DEVICE:”, please go back to Step 2 and ask an engineer on our free support chat.
+              <div className="absolute bottom-0">
+                <div className="text-lg h-full">
+                  <span className="font-bold">Step 4: </span>
+                  Import a connection block
+                </div>
+                <div className="py-2" />
+                <div className="text-xs">
+                  Supported Flojoy hardware will have a connection block that creates the initial connection with the instrument. To find this block, go to the Add Block sidebar and search the instrument name or manufacturer (example searches: Rigol, Tektronix, Keysight, etc). USB-connected devices have general-purpose OPEN_SERIAL and SERIAL_WRITE blocks that can be used for any USB-connected device. If you don’t see blocks for your instrument, please inquire in our free support chat. <br /><br />
+                  After, adding the connection block to your canvas, please click on it to verify that the correct connect device has been autoselected. If n/a is displayed under “DEVICE:”, please go back to Step 2 and ask an engineer on our free support chat.
+                </div>
               </div>
             </TabsContent>
           </Tabs>
