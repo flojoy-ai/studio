0c88113dcbf60d55e24f33b6f0267fa71ccbdc54
smahmed776 <xsmahmed776@gmail.com>
2023-11-30 22:59:57 +0000

    fix issues blocking e2e test

--- captain/utils/logger.py
+++ captain/utils/logger.py
@@ -8,8 +8,13 @@ logger = logging.getLogger("flojoy")
 
 def load_log_level_from_config():
     flojoy_config_path = os.path.join(get_flojoy_dir(), "flojoy.yaml")
-    with open(flojoy_config_path) as f:
-        data = yaml.safe_load(f)
+    if os.path.exists(flojoy_config_path):
+        with open(flojoy_config_path) as f:
+            data = yaml.safe_load(f)
+    else:
+        with open(flojoy_config_path, "w") as f:
+            data = {"LOG_LEVEL": "INFO"}
+            f.write(yaml.dump(data))
 
     log_level = data.get("LOG_LEVEL", "INFO")
     return log_level
