57fbac9d7618980d0544321d013ddbc430dd69b6
smahmed776 <xsmahmed776@gmail.com>
2023-12-04 17:19:21 +0000

    add helpful comments to default-app test file

--- playwright-test/default-app.spec.ts
+++ playwright-test/default-app.spec.ts
@@ -27,19 +27,38 @@ test.describe("Apps testing", () => {
   });
 
   test("Run default app", async () => {
+    // Set test timeout as this test is expected to be slow
     const timeoutSecond = 900000; // 15mins
     test.setTimeout(timeoutSecond);
+
+    // Get electron BrowserWindow
     const window = await app.firstWindow();
+
+    // Wait for DOM content to be loaded
     await window.waitForLoadState("domcontentloaded");
+
+    // Wait for standby status, this indicates backend is up
     const standbyStatus = "🐢 awaiting a new job";
     await window.getByText(standbyStatus).innerText({ timeout: timeoutSecond });
+
+    // Close the welcome modal
     await window.getByTestId(Selectors.closeWelcomeModalBtn).click();
+
+    // Wait for manifest file to be fetched from backend
     const playBtn = window.getByTestId(Selectors.playBtn);
     await playBtn.isEnabled({ timeout: 10000 });
+
+    // Click play to run the default app
     await playBtn.click({ delay: 500 });
+
+    // Check for cancel button to be visible, this indicates app is running.
     const cancelBtn = window.getByTestId(Selectors.cancelPlayBtn);
     await cancelBtn.waitFor({ state: "visible", timeout: 5000 });
+
+    // Check for standby status again, this indicates app run was successful
     await window.getByText(standbyStatus).innerText({ timeout: 60000 });
+
+    // Take a screenshot of the whole page
     await window.screenshot({
       fullPage: true,
       path: "test-results/default-app.jpg",
