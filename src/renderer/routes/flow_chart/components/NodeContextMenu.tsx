99fea7ea9f2524bc2aade34255a71bad91747fc7
Joey Yu <joey@jyu.dev>
2023-12-05 15:14:10 -0500

    merge

--- src/renderer/routes/flow_chart/components/NodeContextMenu.tsx
+++ src/renderer/routes/flow_chart/components/NodeContextMenu.tsx
@@ -1,8 +1,9 @@
-import { useFlowChartState } from '@src/hooks/useFlowChartState';
-import { ElementsData } from '@src/types';
-import { CopyPlus, Info, Pencil, X } from 'lucide-react';
-import { useCallback } from 'react';
-import { useStore, Node, useReactFlow } from 'reactflow';
+import { Button } from "@src/components/ui/button";
+import { useFlowChartState } from "@src/hooks/useFlowChartState";
+import { ElementsData } from "@src/types";
+import { CopyPlus, Info, Pencil, X } from "lucide-react";
+import { useCallback } from "react";
+import { useStore, Node, useReactFlow } from "reactflow";
 
 export type MenuInfo = {
   id: string;
@@ -10,7 +11,7 @@ export type MenuInfo = {
   left?: number;
   right?: number;
   bottom?: number;
-}
+};
 
 type ContextMenuProps = {
   id: string;
@@ -21,7 +22,7 @@ type ContextMenuProps = {
   onClick?: () => void;
   duplicateNode: (node: Node<ElementsData>) => void;
   setNodeModalOpen: (open: boolean) => void;
-}
+};
 
 export default function ContextMenu({
   id,
@@ -31,22 +32,25 @@ export default function ContextMenu({
   bottom,
   onClick,
   duplicateNode,
-  setNodeModalOpen
+  setNodeModalOpen,
 }: ContextMenuProps) {
   const { getNode, setNodes, setEdges } = useReactFlow();
 
   const { setIsEditMode } = useFlowChartState();
-  const { addSelectedNodes } = useStore(state => ({ resetSelectedElements: state.resetSelectedElements, addSelectedNodes: state.addSelectedNodes }));
+  const { addSelectedNodes } = useStore((state) => ({
+    resetSelectedElements: state.resetSelectedElements,
+    addSelectedNodes: state.addSelectedNodes,
+  }));
   const editNode = () => {
-    addSelectedNodes([id])
+    addSelectedNodes([id]);
     setIsEditMode(true);
-  }
+  };
 
   const openInfo = () => {
-    addSelectedNodes([id])
+    addSelectedNodes([id]);
     setIsEditMode(false);
     setNodeModalOpen(true);
-  }
+  };
 
   const duplicate = () => {
     const node = getNode(id);
@@ -54,7 +58,7 @@ export default function ContextMenu({
       return;
     }
     duplicateNode(node);
-  }
+  };
 
   const deleteNode = useCallback(() => {
     setNodes((nodes) => nodes.filter((node) => node.id !== id));
@@ -64,16 +68,47 @@ export default function ContextMenu({
   return (
     <div
       style={{ top, left, right, bottom }}
-      className="absolute z-50 bg-background border rounded-md"
+      className="absolute z-50 rounded-md border bg-background"
       onClick={onClick}
     >
-      <div onClick={editNode} className="hover:bg-muted/50 px-2 py-1 text-sm flex items-center gap-2 w-full"><Pencil size={14} />Edit Block</div>
-      <div onClick={duplicate} className="hover:bg-muted/50 px-2 py-1 text-sm flex items-center gap-2 w-full"><CopyPlus size={14} />Duplicate Block</div>
+      <Button
+        onClick={editNode}
+        variant="ghost"
+        size="sm"
+        className="flex w-full justify-start gap-2"
+      >
+        <Pencil size={14} />
+        Edit Block
+      </Button>
+      <Button
+        onClick={duplicate}
+        variant="ghost"
+        size="sm"
+        className="flex w-full justify-start gap-2"
+      >
+        <CopyPlus size={14} />
+        Duplicate Block
+      </Button>
       <hr />
-      <div onClick={openInfo} className="hover:bg-muted/50 px-2 py-1 text-sm flex items-center gap-2 w-full"><Info size={14} />Block Info</div >
+      <Button
+        onClick={openInfo}
+        variant="ghost"
+        size="sm"
+        className="flex w-full justify-start gap-2"
+      >
+        <Info size={14} />
+        Block Info
+      </Button>
       <hr />
-      <div onClick={deleteNode} className="hover:bg-muted/50 px-2 py-1 text-sm flex items-center gap-2 w-full"><X size={14} />Delete Block</div>
+      <Button
+        onClick={deleteNode}
+        variant="ghost"
+        size="sm"
+        className="flex w-full justify-start gap-2"
+      >
+        <X size={14} />
+        Delete Block
+      </Button>
     </div>
   );
 }
-
