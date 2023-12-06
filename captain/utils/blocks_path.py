550f9df7d53494c864eb4cbf3870a0d86378e427
Joey Yu <joey@jyu.dev>
2023-12-06 12:22:56 -0500

    fix: get path logic

--- captain/utils/blocks_path.py
+++ captain/utils/blocks_path.py
@@ -2,21 +2,18 @@ import os
 from pathlib import Path
 
 
-def get_blocks_path():
+def get_blocks_path() -> str:
     # This env is set from scripts/backend.(ps1/sh) file
     env = os.environ.get("ELECTRON_MODE", "dev")
 
-    if env == "test":
-        return Path(os.getcwd()).joinpath("blocks").__str__()
+    if env == "dev":
+        return os.path.join(os.getcwd(), "blocks")
 
     # Refer to electron/node-pack-save.ts line-8
     blocks_path_from_file = os.path.join(get_flojoy_dir(), "blocks_path.txt")
-    try:
-        with open(blocks_path_from_file, "r") as f:
-            blocks_path = f.read()
-            blocks_path = os.path.join(blocks_path, "blocks")
-    except Exception:
-        pass
+    with open(blocks_path_from_file, "r") as f:
+        blocks_path = f.read()
+        return os.path.join(blocks_path, "blocks")
 
 
 def get_flojoy_dir():
