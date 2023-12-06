a6f37511908dceda1f498608ad710782a35531e2
Joey Yu <joey@jyu.dev>
2023-12-06 12:32:58 -0500

    fix: prod blocks path

--- captain/utils/blocks_path.py
+++ captain/utils/blocks_path.py
@@ -3,17 +3,7 @@ from pathlib import Path
 
 
 def get_blocks_path() -> str:
-    # This env is set from scripts/backend.(ps1/sh) file
-    env = os.environ.get("ELECTRON_MODE", "dev")
-
-    if env == "dev":
-        return os.path.join(os.getcwd(), "blocks")
-
-    # Refer to electron/node-pack-save.ts line-8
-    blocks_path_from_file = os.path.join(get_flojoy_dir(), "blocks_path.txt")
-    with open(blocks_path_from_file, "r") as f:
-        blocks_path = f.read()
-        return os.path.join(blocks_path, "blocks")
+    return os.path.join(os.getcwd(), "blocks")
 
 
 def get_flojoy_dir():
