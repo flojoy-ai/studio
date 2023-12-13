fba552c75a77f8dac4fb00d05e3fe51676097677
Joey Yu <joey@jyu.dev>
2023-12-12 19:36:20 -0500

    feat: save file should work

--- src/renderer/routes/editor/EditorView.tsx
+++ src/renderer/routes/editor/EditorView.tsx
@@ -3,11 +3,14 @@ import { python } from "@codemirror/lang-python";
 import { vscodeDark } from "@uiw/codemirror-theme-vscode";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { Button } from "@src/components/ui/button";
 
 const EditorView = () => {
   const { id } = useParams<{ id: string }>();
 
   const [value, setValue] = useState("");
+
+  const [hasChanged, setHasChanged] = useState<boolean>(false);
 
   const loadFile = async () => {
     if (!id) {
@@ -17,9 +20,20 @@ const EditorView = () => {
     setValue(res);
   };
 
-  // const saveFile=async () => {
-  //
-  // }
+  const saveFile = async () => {
+    if (!id) {
+      return;
+    }
+    const res = await window.api.saveFileToFullPath(atob(id), value);
+    if (res) {
+      setHasChanged(false);
+    }
+  };
+
+  const handleChange = (value: string) => {
+    setValue(value);
+    setHasChanged(true);
+  };
 
   useEffect(() => {
     loadFile();
@@ -30,16 +44,18 @@ const EditorView = () => {
   }
 
   return (
-    <div className="h-screen">
-      <div>{atob(id)}</div>
+    <div>
+      <div className="absolute right-5 z-50 flex items-center gap-2 p-4">
+        {hasChanged && <div className="">Changed</div>}
+        <Button onClick={saveFile}>Save</Button>
+      </div>
       <CodeMirror
         value={value}
         height="100%"
-        style={{ height: "100%" }}
-        className="h-full"
+        className="h-screen"
         extensions={[python()]}
         theme={vscodeDark}
-        onChange={setValue}
+        onChange={handleChange}
       />
     </div>
   );
