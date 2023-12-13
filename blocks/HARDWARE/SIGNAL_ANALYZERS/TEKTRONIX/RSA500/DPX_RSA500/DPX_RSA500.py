7da6dadbfe276b91702937907d5c3d8ab04542d9
Dallas Strandell <dstrande@live.ca>
2023-12-13 08:16:29 -0800

    Add basic tutorials (#1005)
    * add tutorials
    
    * fix rsa500 and heatmap
    
    * add info and app creation
    
    * just sync
    
    * blockcard to linkcards
    
    * fix err
    
    ---------
    
    Co-authored-by: Dallas Strandell <41165864+argenate@users.noreply.github.com>

--- blocks/HARDWARE/SIGNAL_ANALYZERS/TEKTRONIX/RSA500/DPX_RSA500/DPX_RSA500.py
+++ blocks/HARDWARE/SIGNAL_ANALYZERS/TEKTRONIX/RSA500/DPX_RSA500/DPX_RSA500.py
@@ -1,7 +1,7 @@
 from flojoy import flojoy, DataContainer, Surface, File
 from typing import Optional, TypedDict
 from flojoy.instruments.tektronix.RSA_API import *  # noqa: F403
-from ctypes import cdll, c_int, c_bool, c_double, c_int32, byref
+from ctypes import cdll, c_int, c_bool, c_double, c_int32, byref, create_string_buffer
 import numpy as np
 
 
