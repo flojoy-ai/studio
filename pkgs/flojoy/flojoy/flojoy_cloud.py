39d181e946868ff87798ae3e975f2169f34635e7
JeffDotPng <zhangjeff2014@gmail.com>
2024-01-08 16:50:45 -0500

    feat: rewrite cloud client for new cloud

--- pkgs/flojoy/flojoy/flojoy_cloud.py
+++ pkgs/flojoy/flojoy/flojoy_cloud.py
@@ -4,9 +4,9 @@ import os
 from typing import Literal, Optional, TypeVar, overload
 
 import numpy as np
-from pydantic.utils import to_camel
 import requests
 from pydantic import BaseModel, ConfigDict, TypeAdapter
+from pydantic.alias_generators import to_camel
 
 
 class NumpyEncoder(json.JSONEncoder):
@@ -22,25 +22,39 @@ class NumpyEncoder(json.JSONEncoder):
         return json.JSONEncoder.default(self, o)
 
 
+MeasurementType = Literal["boolean", "dataframe"]
+
+
+class Boolean(BaseModel):
+    type: Literal["boolean"] = "boolean"
+    passed: bool
+
+
+class Dataframe(BaseModel):
+    type: Literal["dataframe"] = "dataframe"
+    dataframe: dict
+
+
+MeasurementData = Boolean | Dataframe
+
+
 T = TypeVar("T", bound=BaseModel)
 U = TypeVar("U")
 
 
 class CloudModel(BaseModel):
-    model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)
+    model_config = ConfigDict(alias_generator=to_camel)
     id: str
     created_at: datetime.datetime
 
 
 class Device(CloudModel):
     name: str
-    updated_at: datetime.datetime
+    updated_at: Optional[datetime.datetime]
     project_id: str
 
 
 StorageProvider = Literal["s3", "local"]
-
-MeasurementType = Literal["boolean", "dataframe"]
 
 
 class Measurement(CloudModel):
@@ -55,7 +69,7 @@ class Measurement(CloudModel):
 
 class Test(CloudModel):
     name: str
-    updated_at: datetime.datetime
+    updated_at: Optional[datetime.datetime]
     measurement_type: MeasurementType
     project_id: str
 
@@ -67,6 +81,7 @@ class Project(CloudModel):
 
 
 PlanType = Literal["hobby", "pro", "enterprise"]
+HttpMethod = Literal["GET", "POST", "PUT", "PATCH", "DELETE"]
 
 
 class Workspace(CloudModel):
@@ -76,11 +91,15 @@ class Workspace(CloudModel):
     updated_at: Optional[datetime.datetime]
 
 
+class FlojoyCloudException(Exception):
+    pass
+
+
 class FlojoyCloud:
-    api_key: str
+    auth: str
 
     def __init__(
-        self, api_key: Optional[str] = None, cloud_url="https://cloud.flojoy.ai"
+        self, api_key: Optional[str] = None, api_url="https://cloud.flojoy.ai/api/v1"
     ):
         if api_key is None:
             env = os.environ.get("FLOJOY_CLOUD_API_KEY")
@@ -89,8 +108,8 @@ class FlojoyCloud:
                     "Flojoy cloud api key not set, and no 'FLOJOY_CLOUD_API_KEY' environment variable was found."
                 )
             api_key = env
-        self.api_key = api_key
-        self.base_url = cloud_url + "/api"
+        self.auth = f"Bearer {api_key}"
+        self.base_url = api_url
 
     def __make_query_string(self, query_params: dict[str, int | str] | None):
         if not query_params:
@@ -114,159 +133,72 @@ class FlojoyCloud:
                 return model.model_validate_json(json_str)
 
     @overload
-    def __get(
+    def __req(
         self,
+        method: HttpMethod,
         endpoint: str,
-        query_params: dict[str, int | str] | None,
+        *,
+        query_params: dict[str, int | str] | None = None,
+        data: str | None = None,
+        body: dict | None = None,
         parse_as: type[T],
     ) -> T:
         ...
 
     @overload
-    def __get(
+    def __req(
         self,
+        method: HttpMethod,
         endpoint: str,
-        query_params: dict[str, int | str] | None,
-        parse_as: TypeAdapter[U],
-    ) -> U:
-        ...
-
-    def __get(
-        self,
-        endpoint: str,
-        query_params: dict[str, int | str] | None,
-        parse_as: type[T] | TypeAdapter[U],
-    ) -> T | U:
-        res = requests.get(
-            self.base_url + endpoint + self.__make_query_string(query_params),
-            headers={"Authorization": self.api_key},
-        )
-        return self.__parse(parse_as, res.text)
-
-    @overload
-    def __put(self, endpoint: str, body: dict, parse_as: type[T]) -> T:
-        ...
-
-    @overload
-    def __put(
-        self,
-        endpoint: str,
-        body: dict,
-        parse_as: TypeAdapter[U],
-    ) -> U:
-        ...
-
-    def __put(
-        self,
-        endpoint: str,
-        body: dict,
-        parse_as: type[T] | TypeAdapter[U],
-    ) -> T | U:
-        res = requests.put(
-            self.base_url + endpoint,
-            json=body,
-            headers={"Authorization": self.api_key},
-        )
-        return self.__parse(parse_as, res.text)
-
-    @overload
-    def __patch(self, endpoint: str, body: dict, parse_as: type[T]) -> T:
-        ...
-
-    @overload
-    def __patch(
-        self,
-        endpoint: str,
-        body: dict,
-        parse_as: TypeAdapter[U],
-    ) -> U:
-        ...
-
-    def __patch(
-        self,
-        endpoint: str,
-        body: dict,
-        parse_as: type[T] | TypeAdapter[U],
-    ) -> T | U:
-        res = requests.put(
-            self.base_url + endpoint,
-            json=body,
-            headers={"Authorization": self.api_key},
-        )
-        return self.__parse(parse_as, res.text)
-
-    @overload
-    def __delete(
-        self, endpoint: str, query_params: dict[str, int | str], parse_as: type[T]
-    ) -> T:
-        ...
-
-    @overload
-    def __delete(
-        self,
-        endpoint: str,
-        query_params: dict[str, int | str],
+        *,
+        query_params: dict[str, int | str] | None = None,
+        data: str | None = None,
+        body: dict | None = None,
         parse_as: TypeAdapter[U],
     ) -> U:
         ...
 
     @overload
-    def __delete(
-        self, endpoint: str, query_params: dict[str, int | str], parse_as: None = None
-    ) -> None:
-        ...
-
-    def __delete(
+    def __req(
         self,
+        method: HttpMethod,
         endpoint: str,
-        query_params: dict[str, int | str] | None,
-        parse_as: type[T] | TypeAdapter[U] | None = None,
-    ) -> T | U | None:
-        res = requests.delete(
-            self.base_url + endpoint + self.__make_query_string(query_params),
-            headers={"Authorization": self.api_key},
-        )
-        if parse_as is None:
-            return None
-
-        return self.__parse(parse_as, res.text)
-
-    @overload
-    def __post(self, endpoint: str, body: dict, parse_as: type[T]) -> T:
-        ...
-
-    @overload
-    def __post(
-        self,
-        endpoint: str,
-        body: dict,
-        parse_as: TypeAdapter[U],
-    ) -> U:
-        ...
-
-    @overload
-    def __post(
-        self,
-        endpoint: str,
-        body: dict,
+        *,
+        query_params: dict[str, int | str] | None = None,
+        data: str | None = None,
+        body: dict | None = None,
         parse_as: None = None,
     ) -> None:
         ...
 
-    def __post(
+    def __req(
         self,
+        method: HttpMethod,
         endpoint: str,
-        body: dict,
+        *,
+        query_params: dict[str, int | str] | None = None,
+        data: str | None = None,
+        body: dict | None = None,
         parse_as: type[T] | TypeAdapter[U] | None = None,
     ) -> T | U | None:
-        res = requests.post(
-            self.base_url + endpoint,
+        res = requests.request(
+            method,
+            self.base_url + endpoint + self.__make_query_string(query_params),
+            data=data,
             json=body,
-            headers={"Authorization": self.api_key},
+            headers={"Authorization": self.auth, "Content-Type": "application/json"},
         )
+
+        if res.status_code >= 400:
+            error_json = json.loads(res.text)
+            raise FlojoyCloudException(
+                f"An exception occurred when making a request to {endpoint}\n "
+                f"{error_json['code']}: {error_json['message']}"
+            )
 
         if parse_as is None:
             return None
+
         return self.__parse(parse_as, res.text)
 
     """Test Endpoints"""
@@ -274,9 +206,10 @@ class FlojoyCloud:
     def create_test(
         self, name: str, project_id: str, measurement_type: MeasurementType
     ) -> Test:
-        return self.__post(
+        return self.__req(
+            "POST",
             "/tests",
-            {
+            body={
                 "name": name,
                 "projectId": project_id,
                 "measurementType": measurement_type,
@@ -284,108 +217,128 @@ class FlojoyCloud:
             parse_as=Test,
         )
 
-    def get_test_by_id(self, test_id: str, create_if_not_exist=False):
-        return self.__get("/tests", {"testId": test_id}, parse_as=Test)
+    def get_test_by_id(self, test_id: str):
+        return self.__req("GET", f"/tests/{test_id}", parse_as=Test)
 
     def get_all_tests_by_project_id(self, project_id: str):
-        return self.__get(
-            "/tests", {"projectId": project_id}, parse_as=TypeAdapter(list[Test])
+        return self.__req(
+            "GET",
+            "/tests",
+            query_params={"projectId": project_id},
+            parse_as=TypeAdapter(list[Test]),
         )
 
     """Device Endpoints"""
 
     def create_device(self, name: str, project_id: str):
-        return self.__post(
-            "/devices", {"name": name, "projectId": project_id}, parse_as=Device
+        return self.__req(
+            "POST",
+            "/devices",
+            body={"name": name, "projectId": project_id},
+            parse_as=Device,
         )
 
-    def create_devices(self, name: str, project_id: str):
-        return self.__post(
+    def get_device_by_id(self, device_id: str):
+        return self.__req("GET", f"/devices/{device_id}", parse_as=Device)
+
+    def get_all_devices_by_project_id(self, project_id: str):
+        return self.__req(
+            "GET",
             "/devices",
-            {"name": name, "projectId": project_id},
+            query_params={"projectId": project_id},
             parse_as=TypeAdapter(list[Device]),
         )
 
-    def get_device_by_id(self, device_id: str, create_if_not_exist=False):
-        return self.__get("/devices", {"deviceId": device_id}, parse_as=Device)
-
-    def get_all_devices_by_project_id(self, project_id: str):
-        return self.__get(
-            "/devices", {"projectId": project_id}, parse_as=TypeAdapter(list[Device])
-        )
+    def delete_device_by_id(self, device_id: str):
+        return self.__req("DELETE", f"/devices/{device_id}", parse_as=Device)
 
     """Measurement Endpoints"""
 
+    # TODO: Fix
     def upload(
         self,
-        *,
-        name: str | None,
+        data: MeasurementData,
         test_id: str,
         device_id: str,
-        data: dict,
+        name: str | None = None,
+        created_at: datetime.datetime | None = None,
     ):
-        return self.__post(
+        body = {
+            "testId": test_id,
+            "deviceId": device_id,
+            "data": data.model_dump(),
+            "measurementType": data.type,
+        }
+        if name is not None:
+            body["name"] = name
+        if created_at is not None:
+            body["createdAt"] = created_at.isoformat()
+
+        return self.__req(
+            "POST",
             "/measurements",
-            {
-                "name": name,
-                "testId": test_id,
-                "deviceId": device_id,
-                "data": json.dumps(data, encoder=NumpyEncoder),
-            },
+            data=json.dumps(body, cls=NumpyEncoder),
         )
 
     def get_all_measurements_by_test_id(
-        self, test_id: str, start_date: datetime.datetime, end_date: datetime.datetime
+        self,
+        test_id: str,
+        start_date: datetime.datetime | None = None,
+        end_date: datetime.datetime | None = None,
     ):
-        return self.__get(
+        query_params: dict[str, int | str] = {
+            "testId": test_id,
+        }
+        if start_date is not None:
+            query_params["startDate"] = start_date.isoformat()
+        if end_date is not None:
+            query_params["endDate"] = end_date.isoformat()
+
+        return self.__req(
+            "GET",
             "/measurements",
-            {
-                "testId": test_id,
-                "startDate": start_date.isoformat(),
-                "endDate": end_date.isoformat(),
-            },
+            query_params=query_params,
             parse_as=TypeAdapter(list[Measurement]),
         )
 
     """Project Routes"""
 
     def create_project(self, name: str, workspace_id: str):
-        return self.__post(
-            "/projects", {"name": name, "workspaceId": workspace_id}, parse_as=Project
+        return self.__req(
+            "POST",
+            "/projects",
+            body={"name": name, "workspaceId": workspace_id},
+            parse_as=Project,
         )
 
     def get_project_by_id(self, project_id: str):
-        return self.__get("/projects", {"projectId": project_id}, parse_as=Project)
+        return self.__req("GET", f"/projects/{project_id}", parse_as=Project)
 
     def get_all_projects_by_workspace_id(self, workspace_id: str):
-        return self.__get(
+        return self.__req(
+            "GET",
             "/projects",
-            {"workspaceId": workspace_id},
+            query_params={"workspaceId": workspace_id},
             parse_as=TypeAdapter(list[Project]),
         )
 
     """Workspace Routes"""
 
-    def create_workspace(self, name: str):
-        return self.__post("/workspaces", {"name": name}, parse_as=Workspace)
-
     def update_workspace(self, workspace_id: str, name: str):
-        return self.__patch(
-            "/workspaces",
-            {"id": workspace_id, "name": name},
-            parse_as=Workspace,
+        return self.__req(
+            "PATCH",
+            f"/workspaces/{workspace_id}",
+            body={"name": name},
         )
 
     def delete_workspace_by_id(self, workspace_id: str):
-        return self.__delete(
-            "/workspaces",
-            {"id": workspace_id},
+        return self.__req(
+            "DELETE",
+            f"/workspaces/{workspace_id}",
         )
 
     def get_all_workspaces(self):
-        return self.__get("/workspaces", None, parse_as=TypeAdapter(list[Workspace]))
+        return self.__req("GET", "/workspaces", parse_as=TypeAdapter(list[Workspace]))
 
     def get_workspace_by_id(self, workspace_id: str):
-        return self.__get(
-            "/workspaces", {"workspaceId": workspace_id}, parse_as=Workspace
-        )
+        return self.__req("GET", f"/workspaces/{workspace_id}", parse_as=Workspace)
