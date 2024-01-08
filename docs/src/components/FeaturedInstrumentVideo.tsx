c883f1b7f9b6f3986cce55b525defc5251fd2866
smahmed776 <xsmahmed776@gmail.com>
2024-01-08 21:04:32 +0000

    refactory NodeCollection component

--- docs/src/components/FeaturedInstrumentVideo.tsx
+++ docs/src/components/FeaturedInstrumentVideo.tsx
@@ -1,62 +1,47 @@
-import React from "react";
-
 type FeaturedInstrumentVideoProps = {
   category: string;
-  manufacturer: string;
+};
+
+const categoryVideoMap = {
+  OSCILLOSCOPES: {
+    url: "https://www.youtube.com/embed/NqPW7pG3NZs?si=P374jJmZNKatbExq",
+    hero: "Measure signal width and phase with a Tektronix oscilloscope",
+  },
+
+  DAQ_BOARDS: {
+    url: "https://www.youtube.com/embed/ph64DXjX1-8?si=MdY5XjyGhpbTEswx",
+    hero: "Record temperature over time with a LabJack DAQ board",
+  },
+
+  MULTIMETERS: {
+    url: "https://www.youtube.com/embed/jaN89-ijxL0?si=ytXVo41QkjADx8us",
+    hero: "Record voltage over time with an Agilent 34401A multimeter",
+  },
+
+  POWER_SUPPLIES: {
+    url: "https://www.youtube.com/embed/OEzThwlCU1s?si=9HcGT7Kij5h9GoSe",
+    hero: "Measure a solar panel IV curve with a Keithley 2400",
+  },
+
+  MOTOR_CONTROLLERS: {
+    url: "https://www.youtube.com/embed/6NBlT6VWQW0?si=FqTfe1WJTMcoNilq",
+    hero: "Send commands to a Polulu stepper motor driver",
+  },
 };
 
 export default function FeaturedInstrumentVideo({
   category,
-  manufacturer,
 }: FeaturedInstrumentVideoProps) {
-  let VIDEO_URL =
-    "https://www.youtube.com/embed/xvFZjo5PgG0?si=oXQsWInrLBlhAoCB&mute=1";
-  let VIDEO_HERO = "NOT FOUND";
-
-  // console.warn('Instrument category: ', category);
-
-  switch (category) {
-    case "OSCILLOSCOPES":
-      VIDEO_URL =
-        "https://www.youtube.com/embed/NqPW7pG3NZs?si=P374jJmZNKatbExq";
-      VIDEO_HERO =
-        "Measure signal width and phase with a Tektronix oscilloscope";
-      break;
-
-    case "DAQ_BOARDS":
-      VIDEO_URL =
-        "https://www.youtube.com/embed/ph64DXjX1-8?si=MdY5XjyGhpbTEswx";
-      VIDEO_HERO = "Record temperature over time with a LabJack DAQ board";
-      break;
-
-    case "MULTIMETERS":
-      VIDEO_URL =
-        "https://www.youtube.com/embed/jaN89-ijxL0?si=ytXVo41QkjADx8us";
-      VIDEO_HERO = "Record voltage over time with an Agilent 34401A multimeter";
-      break;
-
-    case "POWER_SUPPLIES":
-      VIDEO_URL =
-        "https://www.youtube.com/embed/OEzThwlCU1s?si=9HcGT7Kij5h9GoSe";
-      VIDEO_HERO = "Measure a solar panel IV curve with a Keithley 2400";
-      break;
-
-    case "MOTOR_CONTROLLERS":
-      VIDEO_URL =
-        "https://www.youtube.com/embed/6NBlT6VWQW0?si=FqTfe1WJTMcoNilq";
-      VIDEO_HERO = "Send commands to a Polulu stepper motor driver";
-      break;
-  }
-
-  if (VIDEO_HERO == "NOT FOUND") {
+  if (!(category in categoryVideoMap)) {
     return <></>;
   }
+  const video = categoryVideoMap[category as keyof typeof categoryVideoMap];
 
   return (
     <div className="mb-20 mt-20">
-      <h2>Demo: {`${VIDEO_HERO}`}</h2>
+      <h2>Demo: {video.hero}</h2>
       <iframe
-        src={VIDEO_URL}
+        src={video.url}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowFullScreen
