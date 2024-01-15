7d45ffdd173193a41880421be2d80033a4e5e9e2
smahmed776 <xsmahmed776@gmail.com>
2024-01-15 21:34:28 +0000

    use bcrypt to compare pass in FastApi

--- captain/services/auth/auth_service.py
+++ captain/services/auth/auth_service.py
@@ -1,9 +1,16 @@
 import os
 import json
+import bcrypt
+
+
+def compare_pass(hashed_password: str, plain_password: str):
+    return bcrypt.checkpw(
+        plain_password.encode("utf-8"), hashed_password.encode("utf-8")
+    )
 
 
 def validate_credentials(username: str, password: str):
-    db_path = os.environ.get("LOCAL_DB_PATH")
+    db_path = os.environ.get("LOCAL_DB_PATH", None)
 
     if not db_path:
         return False
@@ -18,7 +25,7 @@ def validate_credentials(username: str, password: str):
     for user in users:
         if (
             user.get("name") == username
-            and user.get("password") == password
+            and compare_pass(user.get("password"), password)
             and user.get("role") == "Admin"
         ):
             return True
