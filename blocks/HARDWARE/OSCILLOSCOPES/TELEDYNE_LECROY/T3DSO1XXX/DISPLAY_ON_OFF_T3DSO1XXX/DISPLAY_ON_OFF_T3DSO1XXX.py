bebd76284d2d12a81b622bbf35b5a3a27717eade
Guillaume Thibault <guillaume.thibault.98@gmail.com>
2024-01-15 09:29:02 -0500

    [T3DSO1000(A)-2000] Better UX for display block

--- blocks/HARDWARE/OSCILLOSCOPES/TELEDYNE_LECROY/T3DSO1XXX/DISPLAY_ON_OFF_T3DSO1XXX/DISPLAY_ON_OFF_T3DSO1XXX.py
+++ blocks/HARDWARE/OSCILLOSCOPES/TELEDYNE_LECROY/T3DSO1XXX/DISPLAY_ON_OFF_T3DSO1XXX/DISPLAY_ON_OFF_T3DSO1XXX.py
@@ -5,10 +5,10 @@ from typing import Literal, Optional
 @flojoy(inject_connection=True)
 def DISPLAY_ON_OFF_T3DSO1XXX(
     connection: VisaConnection,
-    ch1: Literal["ON", "OFF"] = "ON",
-    ch2: Literal["ON", "OFF"] = "ON",
-    ch3: Literal["ON", "OFF"] = "ON",
-    ch4: Literal["ON", "OFF"] = "ON",
+    display_ch1: bool = True,
+    display_ch2: bool = True,
+    display_ch3: bool = True,
+    display_ch4: bool = True,
     default: Optional[DataContainer] = None,
 ) -> Optional[DataContainer]:
     """Turn the display of the channels on or off on an T3DSO1000(A)-2000 oscilloscope.
@@ -22,13 +22,13 @@ def DISPLAY_ON_OFF_T3DSO1XXX(
     ----------
     connection: VisaConnection
         The VISA address (requires the CONNECT_T3DSO1XXX block)
-    ch1: select
+    display_ch1: select
         Turn channel 1 on or off
-    ch2: select
+    display_ch2: select
         Turn channel 2 on or off
-    ch3: select
+    display_ch3: select
         Turn channel 3 on or off
-    ch4: select
+    display_ch4: select
         Turn channel 4 on or off
 
     Returns
@@ -39,9 +39,9 @@ def DISPLAY_ON_OFF_T3DSO1XXX(
 
     scope = connection.get_handle()
 
-    scope.write(f"C1:TRACE {ch1}")
-    scope.write(f"C2:TRACE {ch2}")
-    scope.write(f"C3:TRACE {ch3}")
-    scope.write(f"C4:TRACE {ch4}")
+    scope.write(f"C1:TRACE {'ON' if display_ch1 else 'OFF'}")
+    scope.write(f"C2:TRACE {'ON' if display_ch2 else 'OFF'}")
+    scope.write(f"C3:TRACE {'ON' if display_ch3 else 'OFF'}")
+    scope.write(f"C4:TRACE {'ON' if display_ch4 else 'OFF'}")
 
     return None
