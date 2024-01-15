3150d837ea5c2f659ac1192cbada8901977e18fc
Guillaume Thibault <guillaume.thibault.98@gmail.com>
2024-01-15 16:51:20 -0500

    [NI-CompactDAQ] Acquiring Analog input

---
+++ blocks/HARDWARE/DAQ_BOARDS/NATIONAL_INSTRUMENTS/COMPACT_DAQ/CONNECT_CDAQ/CONNECT_CDAQ.py
@@ -0,0 +1,35 @@
+from flojoy import flojoy, DataContainer, String, NIDevice, DeviceConnectionManager
+from typing import Optional
+import nidaqmx
+
+
+@flojoy(deps={"nidaqmx": "0.9.0"})
+def CONNECT_CDAQ(
+    targeted_device: str,
+    default: Optional[DataContainer] = None,
+) -> String:
+    """Connect to a National-Instrument compactDAQ device
+
+    Todo
+
+    Parameters
+    ----------
+    device: VisaDevice
+        The VISA address to connect to.
+
+    Returns
+    -------
+    DataContainer
+        None.
+    """
+
+    devices = nidaqmx.system.System().devices
+    device_names = devices.device_names
+
+    assert len(device_names) > 0, "No NI-cDAQ detected"
+    assert targeted_device in device_names, f"No NI-cDAQ with the name {targeted_device}, please select of of the following: {', '.join(device_names)}"
+
+    device = devices[targeted_device]
+    DeviceConnectionManager.register_connection(NIDevice(targeted_device), device, cleanup=lambda x: x)
+
+    return String(targeted_device)
