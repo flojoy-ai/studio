6d79a0972f3fafed1c1064c14fb5a28fa53e6957
Guillaume Thibault <guillaume.thibault.98@gmail.com>
2024-01-17 12:52:43 -0500

    [Block test] Adding fix from Dallas for LOG test

--- blocks/MATH/ARITHMETIC/LOG/LOG_test_.py
+++ blocks/MATH/ARITHMETIC/LOG/LOG_test_.py
@@ -1,25 +1,28 @@
 import numpy as np
-
 from flojoy import OrderedPair, Vector, Scalar
 
 
 def test_LOG_Vector_Vector(mock_flojoy_decorator):
     import LOG
-
+    
     x = Vector(v=np.arange(10, 20, 1))
     y = Vector(v=np.arange(20, 30, 1))
     res = LOG.LOG(a=x, b=[y], log_base="input")
+    test = np.log(x.v)
+    test /= np.log(y.v)
 
-    np.testing.assert_allclose(res.v, np.log(x.v, y.v))
+    np.testing.assert_allclose(res.v, test)
 
 
 def test_LOG_Vector_Scalar(mock_flojoy_decorator):
     import LOG
 
-    x = Vector(v=np.arange(-10, 10, 1))
+    x = Vector(v=np.arange(1, 11, 1))
     res = LOG.LOG(a=x, b=[Scalar(c=2)], log_base="input")
+    test = np.log(x.v)
+    test /= np.log(2)
 
-    np.testing.assert_allclose(res.v, np.log(x.v, 2))
+    np.testing.assert_allclose(res.v, test)
 
 
 def test_LOG_OrderedPair_Vector(mock_flojoy_decorator):
@@ -29,6 +32,9 @@ def test_LOG_OrderedPair_Vector(mock_flojoy_decorator):
     y = np.arange(20, 30, 1)
     z = np.arange(30, 40, 1)
     res = LOG.LOG(a=OrderedPair(x=x, y=y), b=[Vector(v=z)], log_base="input")
-
+    test = np.log(y)
+    test /= np.log(z)
+  
     np.testing.assert_allclose(res.x, x)
-    np.testing.assert_allclose(res.y, np.log(y, z))
+    np.testing.assert_allclose(res.y, test)
+
