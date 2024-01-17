18932f5fd125bec0313eaa80b93d2a97863db5f5
Dallas Strandell <dstrande@live.ca>
2024-01-17 09:53:29 -0800

    Update LOG_test_.py

--- blocks/MATH/ARITHMETIC/LOG/LOG_test_.py
+++ blocks/MATH/ARITHMETIC/LOG/LOG_test_.py
@@ -4,7 +4,7 @@ from flojoy import OrderedPair, Vector, Scalar
 
 def test_LOG_Vector_Vector(mock_flojoy_decorator):
     import LOG
-    
+
     x = Vector(v=np.arange(10, 20, 1))
     y = Vector(v=np.arange(20, 30, 1))
     res = LOG.LOG(a=x, b=[y], log_base="input")
@@ -34,7 +34,8 @@ def test_LOG_OrderedPair_Vector(mock_flojoy_decorator):
     res = LOG.LOG(a=OrderedPair(x=x, y=y), b=[Vector(v=z)], log_base="input")
     test = np.log(y)
     test /= np.log(z)
-  
+    print(res.y, flush=True)
+    print(test, flush=True)
+
     np.testing.assert_allclose(res.x, x)
     np.testing.assert_allclose(res.y, test)
-
