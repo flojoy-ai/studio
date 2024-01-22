652284ed6ba8f660143f0da8b6f4a53c862c33e3
Guillaume Thibault <guillaume.thibault.98@gmail.com>
2024-01-22 13:38:19 -0500

    [NI-CompactDAQ] Wait until done block | Small cleanup for PR

---
+++ blocks/HARDWARE/DAQ_BOARDS/NATIONAL_INSTRUMENTS/COMPACT_DAQ/TASK_WAIT_UNTIL_DONE/TASK_WAIT_UNTIL_DONE.py
@@ -0,0 +1,33 @@
+
+from flojoy import flojoy, DataContainer, Vector, Matrix, NIDAQmxConnection
+import nidaqmx
+from typing import Optional
+import numpy as np
+
+
+@flojoy(deps={"nidaqmx": "0.9.0"}, inject_connection=True)
+def TASK_WAIT_UNTIL_DONE(
+    connection: NIDAQmxConnection,
+    timeout: float = 10.0,
+    default: Optional[DataContainer] = None,
+) -> Optional[DataContainer]:
+    """Waits for the measurement or generation to complete.
+    
+    Use this method to ensure that the specified operation is complete before you stop the task.
+
+    Parameters
+    ----------
+    cDAQ_start_channel : NIDAQmxDevice
+        The first input channel for which a created task has been initialized.
+    timeout : float
+        Specifies the maximum amount of time in seconds to wait for the measurement or generation to complete. This method returns an error if the time elapses.
+
+    Returns
+    -------
+    Optional[DataContainer]
+        Return the input
+    """
+
+    task: nidaqmx.task.Task = connection.get_handle()
+    task.wait_until_done(timeout=timeout)
+    return default
--- captain/routes/devices.py
+++ captain/routes/devices.py
@@ -4,12 +4,9 @@ from captain.types.devices import DeviceInfo
 
 router = APIRouter(tags=["devices"])
 
-import logging
 @router.get("/devices")
 async def get_devices() -> dict[str, str] | DeviceInfo:
     device_finder = get_device_finder()
-    logging.info("Getting devices")
-    logging.info(device_finder.get_nidaqmx_devices())
     return DeviceInfo(
         cameras=device_finder.get_cameras(),
         serialDevices=device_finder.get_serial_devices(),
