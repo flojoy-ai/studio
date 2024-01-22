1548eb4c0cf0354e4557cb354dfc93148f35787b
Guillaume Thibault <guillaume.thibault.98@gmail.com>
2024-01-22 17:46:00 -0500

    [NI-CompactDAQ] Read from input stream | Splitted From buffer and Auto creation of Buffer

--- blocks/HARDWARE/DAQ_BOARDS/NATIONAL_INSTRUMENTS/COMPACT_DAQ/READ_INPUT_STREAM_INTO_BUFFER/READ_INPUT_STREAM_INTO_BUFFER.py
+++ blocks/HARDWARE/DAQ_BOARDS/NATIONAL_INSTRUMENTS/COMPACT_DAQ/READ_INPUT_STREAM_INTO_BUFFER/READ_INPUT_STREAM_INTO_BUFFER.py
@@ -1,28 +1,26 @@
 from flojoy import flojoy, DataContainer, NIDAQmxConnection, Vector
 import nidaqmx
 from typing import Optional
+import logging
 
 
 @flojoy(deps={"nidaqmx": "0.9.0"}, inject_connection=True)
-def READ_INPUT_STREAM(
+def READ_INPUT_STREAM_INTO_BUFFER(
+    buffer: Vector,
     connection: NIDAQmxConnection,
-    read_all: bool = False,
-    number_of_samples_per_channel: int = 1000,
     default: Optional[DataContainer] = None,
 ) -> Vector:
-    """Reads raw samples from the task or virtual channels you specified.
+    """Reads raw samples from the task or virtual channels you specified in the buffer.
 
     Raw samples constitute the internal representation of samples in a device, read directly from the device or buffer without scaling or reordering.
-    This block determines a Vector of appropriate size and data type to create and return based on your device specifications.
 
     Parameters
     ----------
     cDAQ_start_channel : NIDAQmxDevice
         The first input channel for which a created task has been initialized.
-    read_all : bool
-        If True, reads all available samples in the buffer. If False, reads the number of samples you specify in number_of_samples_per_channel.
-    number_of_samples_per_channel : int
-        The number of samples, per channel, to read.
+    buffer: Optional[Vector]
+        Reads raw samples from the task or virtual channels you specify into this pre-allocated buffer.
+        → number_of_samples_per_channel = math.floor(buffer_size_in_bytes / (number_of_channels_to_read * raw_sample_size_in_bytes)).
 
     Returns
     -------
@@ -32,5 +30,8 @@ def READ_INPUT_STREAM(
 
     task: nidaqmx.task.Task = connection.get_handle()
 
-    raw_data = task.in_stream.readall() if read_all else task.in_stream.read(number_of_samples_per_channel=number_of_samples_per_channel)
-    return Vector(raw_data)
+    logging.info(f"Reading {len(buffer)} bytes from {task.name}...")
+    task.in_stream.readinto(buffer.v)
+
+    return buffer
+
