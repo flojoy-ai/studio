181745e7b016e19606d225ad311ed75cd959d59f
Gui <gui@latentdream.dev>
2024-01-24 15:31:58 -0500

    [STU-222_CAN-BUS] Adding Utils Block to decode raw CAN bus messages

---
+++ blocks/HARDWARE/PROTOCOLS/CAN/UTILS/LOAD_DBC/LOAD_DBC.py
@@ -0,0 +1,27 @@
+from flojoy import flojoy, File, Stateful
+from cantools import database
+
+
+@flojoy(deps={"python-can": "4.2.2", "cantools": "39.4.2"})
+def LOAD_DBC(
+    file_path: File
+) -> Stateful:
+    """Load a DBC file.
+
+    Open and parse a given database file. The database format is inferred from the file extension.
+    This block is compatible with the following database format. `dbc`, `arxml`, `kcd`, `sym`, `cdd`.
+
+    Parameters
+    ----------
+    file_path : File
+        The path to the DBC file.
+
+    Returns
+    -------
+    Stateful : cantools.database.Database
+        Return a cantools database object.
+    """
+    
+    db = database.load_file(file_path.unwrap())
+
+    return Stateful(db)
