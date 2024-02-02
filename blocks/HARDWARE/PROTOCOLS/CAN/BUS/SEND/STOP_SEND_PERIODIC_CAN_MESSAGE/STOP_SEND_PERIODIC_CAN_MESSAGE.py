bfe908e32dfb17bf4bc7579d8bfdb400e496bd3a
Guillaume Thibault <guillaume.thibault.98@gmail.com>
2024-02-02 13:57:04 -0500

    [CAN-Utils] Send Periodic Message | Restructured Can Bus Block Tree

---
+++ blocks/HARDWARE/PROTOCOLS/CAN/BUS/SEND/STOP_SEND_PERIODIC_CAN_MESSAGE/STOP_SEND_PERIODIC_CAN_MESSAGE.py
@@ -0,0 +1,31 @@
+from typing import Optional
+from flojoy import flojoy, DeviceConnectionManager, Stateful, DataContainer
+import can
+
+
+@flojoy(deps={"python-can": "4.3.1"})
+def STOP_SEND_PERIODIC_CAN_MESSAGE(
+    CAN_address: str,
+    default: Optional[DataContainer]
+) -> Optional[DataContainer]:
+    """Stop sending periodic a message to a CAN Bus.
+
+    Stop sending a message to a CAN device. This block should be compatible with all devices that support the CAN interface.
+
+    Parameters
+    ----------
+    CAN_address : str
+        The CAN device address to connect to.
+
+    Returns
+    -------
+    DataContainer
+        Optional: None
+    """
+    connection: can.interface.Bus = DeviceConnectionManager.get_connection(
+        CAN_address
+    ).get_handle()
+
+    connection.stop_all_periodic_tasks()
+
+    return None
