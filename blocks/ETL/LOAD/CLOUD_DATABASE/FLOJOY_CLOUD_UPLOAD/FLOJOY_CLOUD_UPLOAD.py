6baffbe6e30dbc27c37d08767fcb0fc88210f643
Gui <guillaume.thibault.98@gmail.com>
2024-02-07 14:45:28 -0500

    STU-251 | New cloud block integration (#1071)
    * [New-Cloud-Integration] Upload Block
    
    * [New-Cloud-Integration] Remove cloud api from flojoy pck
    
    * [New-Cloud-Integration] Optimist casting to Dataframe
    
    * [New-Cloud-Integration] Download Block | Returning Dataframe or Boolean
    
    * [New-Cloud-Integration] Docs & Formatting
    
    * [New-Cloud-Integration] Better Docs

--- blocks/ETL/LOAD/CLOUD_DATABASE/FLOJOY_CLOUD_UPLOAD/FLOJOY_CLOUD_UPLOAD.py
+++ blocks/ETL/LOAD/CLOUD_DATABASE/FLOJOY_CLOUD_UPLOAD/FLOJOY_CLOUD_UPLOAD.py
@@ -1,7 +1,25 @@
 import os
-
-from flojoy import DataContainer, flojoy, get_env_var, node_preflight
-from flojoy.cloud import FlojoyCloud
+import pandas as pd
+from flojoy import (
+    DataContainer,
+    flojoy,
+    get_env_var,
+    node_preflight,
+    Boolean,
+    DataFrame,
+    OrderedPair,
+    OrderedTriple,
+)
+from flojoy.data_container import (
+    ParametricOrderedPair,
+    ParametricOrderedTriple,
+    ParametricSurface,
+    Surface,
+    Vector,
+)
+import flojoy_cloud
+from typing import Optional
+from datetime import datetime
 
 FLOJOY_CLOUD_URI: str = os.environ.get("FLOJOY_CLOUD_URI") or "https://cloud.flojoy.ai"
 
@@ -19,8 +37,10 @@ def preflight():
 @flojoy
 def FLOJOY_CLOUD_UPLOAD(
     default: DataContainer,
-    hardware_device_id: str,
-    name: str,
+    hardware_id: str,
+    test_id: str,
+    name: str | None = None,
+    pass_fail: Optional[Boolean] = None,
 ) -> DataContainer:
     """Upload a DataContainer to Flojoy Cloud (beta).
 
@@ -28,10 +48,16 @@ def FLOJOY_CLOUD_UPLOAD(
 
     Parameters
     ----------
+    default : DataContainer
+        The data to be uploaded to Flojoy Cloud.
     hardware_device_id : str
         The measurement id of the data to be uploaded to Flojoy Cloud.
+    test_id : str
+        The test id of the data to be uploaded to Flojoy Cloud.
     name: str
-        The name of the data to be uploaded to Flojoy Cloud.
+        A name describing the test.
+    pass_fail: Boolean
+        Optional parameter to define if the test passed or failed.
 
     Returns
     -------
@@ -46,16 +72,59 @@ def FLOJOY_CLOUD_UPLOAD(
             "Flojoy Cloud key is not found! You can set it under Settings -> Environment Variables."
         )
 
-    if hardware_device_id is None or not hardware_device_id.startswith("meas_"):
-        raise KeyError(
-            "You must provide a valid measurement id in order to upload to Flojoy Cloud!"
-        )
-
-    cloud = FlojoyCloud(api_key=api_key)
-
     if default:
-        # This will stream the data to the cloud
-
-        cloud.store_dc(default, default.type, hardware_device_id, name)
+        # Only upload if the data is not empty, otherwise pass through
+        cloud = flojoy_cloud.FlojoyCloud(workspace_secret=api_key)
+        data = None
+        # Optimist approach, assume that the data is at a castable dimension
+        if isinstance(default, DataFrame):
+            data = default.m
+        elif isinstance(default, Boolean):
+            data = default.b
+        elif isinstance(default, Vector):
+            data = pd.DataFrame(default.v)
+        elif isinstance(default, OrderedPair):
+            data = pd.DataFrame(
+                {
+                    "x": default.x,
+                    "y": default.y,
+                }
+            )
+        elif isinstance(default, ParametricOrderedPair):
+            data = pd.DataFrame(
+                {
+                    "x": default.x,
+                    "y": default.y,
+                    "t": default.t,
+                }
+            )
+        elif isinstance(default, OrderedTriple):
+            data = pd.DataFrame({"x": default.x, "y": default.y, "z": default.z})
+        elif isinstance(default, ParametricOrderedTriple):
+            data = pd.DataFrame(
+                {"x": default.x, "y": default.y, "z": default.z, "t": default.t}
+            )
+        elif isinstance(default, Surface):
+            data = pd.DataFrame({"x": default.x, "y": default.y, "z": default.z})
+        elif isinstance(default, ParametricSurface):
+            data = pd.DataFrame(
+                {
+                    "x": default.x,
+                    "y": default.y,
+                    "z": default.z,
+                    "t": default.t,
+                }
+            )
+        else:
+            # TODO: Add other data types as they become available
+            raise TypeError(f"Unsupported data type: {type(default)}")
+        cloud.upload(
+            data=data,
+            test_id=test_id,
+            hardware_id=hardware_id,
+            name=name,
+            created_at=datetime.now(),
+            passed=pass_fail.b if pass_fail is not None else None,
+        )
 
     return default
