75207ece0831d059af8331a99be8d4f0fbe88c78
Guillaume Thibault <guillaume.thibault.98@gmail.com>
2024-02-07 13:43:38 -0500

    [New-Cloud-Integration] Download Block | Returning Dataframe or Boolean

--- blocks/ETL/LOAD/CLOUD_DATABASE/FLOJOY_CLOUD_DOWNLOAD/FLOJOY_CLOUD_DOWNLOAD.py
+++ blocks/ETL/LOAD/CLOUD_DATABASE/FLOJOY_CLOUD_DOWNLOAD/FLOJOY_CLOUD_DOWNLOAD.py
@@ -1,7 +1,8 @@
 import os
-
-from flojoy import DataContainer, flojoy, get_env_var, node_preflight
-from flojoy.cloud import FlojoyCloud
+import logging
+from flojoy import DataContainer, flojoy, get_env_var, node_preflight, DataFrame, Boolean
+import flojoy_cloud
+import pandas as pd
 
 FLOJOY_CLOUD_URI: str = os.environ.get("FLOJOY_CLOUD_URI") or "https://cloud.flojoy.ai"
 
@@ -40,11 +41,14 @@ def FLOJOY_CLOUD_DOWNLOAD(
             "Flojoy Cloud key is not found! You can set it under Settings -> Environment Variables."
         )
 
-    if measurement_id is None or not measurement_id.startswith("dc_"):
-        raise KeyError(
-            "You must provide a valid data container id in order to download from Flojoy Cloud!"
-        )
+    cloud = flojoy_cloud.FlojoyCloud(workspace_secret=api_key)
 
-    cloud = FlojoyCloud(api_key=api_key)
-
-    return cloud.to_dc(cloud.fetch_dc(measurement_id))
+    measurement = cloud.get_measurement(measurement_id)
+    logging.info(measurement)
+    match measurement.data["type"]:
+        case "dataframe":
+            return DataFrame(pd.DataFrame(measurement.data["value"]))
+        case "boolean":
+            return Boolean(measurement.data["value"])
+        case _:
+            raise NotImplementedError(f"Type {measurement.data['type']} is not implemented")
