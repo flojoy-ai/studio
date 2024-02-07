866424aba28bc0ead26f262b6831cc8c690aefba
Guillaume Thibault <guillaume.thibault.98@gmail.com>
2024-02-07 12:10:32 -0500

    [New-Cloud-Integration] Upload Block

--- blocks/ETL/LOAD/CLOUD_DATABASE/FLOJOY_CLOUD_UPLOAD/FLOJOY_CLOUD_UPLOAD.py
+++ blocks/ETL/LOAD/CLOUD_DATABASE/FLOJOY_CLOUD_UPLOAD/FLOJOY_CLOUD_UPLOAD.py
@@ -1,7 +1,8 @@
 import os
-
-from flojoy import DataContainer, flojoy, get_env_var, node_preflight
-from flojoy.cloud import FlojoyCloud
+from flojoy import DataContainer, flojoy, get_env_var, node_preflight, Boolean, DataFrame
+import flojoy_cloud
+from typing import Optional
+from datetime import datetime
 
 FLOJOY_CLOUD_URI: str = os.environ.get("FLOJOY_CLOUD_URI") or "https://cloud.flojoy.ai"
 
@@ -19,8 +20,10 @@ def preflight():
 @flojoy
 def FLOJOY_CLOUD_UPLOAD(
     default: DataContainer,
-    hardware_device_id: str,
-    name: str,
+    hardware_id: str,
+    test_id: str,
+    name: str | None = None,
+    pass_fail: Optional[Boolean] = None
 ) -> DataContainer:
     """Upload a DataContainer to Flojoy Cloud (beta).
 
@@ -28,10 +31,16 @@ def FLOJOY_CLOUD_UPLOAD(
 
     Parameters
     ----------
+    default : DataContainer
+        The data to be uploaded to Flojoy Cloud. Currently only support DataFrame and Boolean.
     hardware_device_id : str
         The measurement id of the data to be uploaded to Flojoy Cloud.
+    test_id : str
+        The test id of the data to be uploaded to Flojoy Cloud.(In Flojoy Cloud, right click on the desired test to select its "Copy ID"!)
     name: str
-        The name of the data to be uploaded to Flojoy Cloud.
+        A custom name describing the test.
+    pass_fail: Boolean
+        Optional parameter to define if the test passed or failed.
 
     Returns
     -------
@@ -46,16 +55,22 @@ def FLOJOY_CLOUD_UPLOAD(
             "Flojoy Cloud key is not found! You can set it under Settings -> Environment Variables."
         )
 
-    if hardware_device_id is None or not hardware_device_id.startswith("meas_"):
-        raise KeyError(
-            "You must provide a valid measurement id in order to upload to Flojoy Cloud!"
-        )
-
-    cloud = FlojoyCloud(api_key=api_key)
-
     if default:
-        # This will stream the data to the cloud
-
-        cloud.store_dc(default, default.type, hardware_device_id, name)
+        # Only upload if the data is not empty, otherwise pass through
+        cloud = flojoy_cloud.FlojoyCloud(workspace_secret=api_key)
+        data = None
+        if isinstance(default, DataFrame):
+            data = default.m
+        elif isinstance(default, Boolean):
+            data = default.b
+        else:
+            raise TypeError(f"Unsupported data type: {type(default)}")
+        cloud.upload(data=data,
+                     test_id=test_id,
+                     hardware_id=hardware_id,
+                     name=name,
+                     created_at=datetime.now(),
+                     passed=pass_fail.b if pass_fail is not None else None
+                     )
 
     return default
