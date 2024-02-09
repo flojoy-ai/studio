0eaa41c1d1b2b7f73eb307644a105b1c564c6033
Gui <guillaume.thibault.98@gmail.com>
2024-02-09 11:31:52 -0500

    Revert "CAN-BUS Support"

--- blocks/AI_ML/NLP/COUNT_VECTORIZER/COUNT_VECTORIZER_test_.py
+++ blocks/AI_ML/NLP/COUNT_VECTORIZER/COUNT_VECTORIZER_test_.py
@@ -2,7 +2,6 @@ import numpy
 from flojoy import Matrix, DataFrame, Vector
 import pytest
 
-sklearn_installed = True
 try:
     import sklearn  # noqa: F401
 except ImportError:
--- blocks/HARDWARE/PROTOCOLS/CAN/CSS_ELECTRONICS/READ_LOG_FILE_CL2000/example.md
+++
@@ -1,1 +0,0 @@
-This example show you how to convert the log file of the CL2000 to a the ASAM measurement file format (MDF4) and interpret the data.
