f1dfc8b7143c81c19182869b656ce936a8d7ac25
Joey Yu <joey@jyu.dev>
2024-02-18 21:47:04 -0500

    feat(docs): support raspberry-pi and chromebook

--- docs/src/components/DownloadButton.tsx
+++ docs/src/components/DownloadButton.tsx
@@ -3,20 +3,45 @@ import {
   faApple,
   faWindows,
   faLinux,
+  type IconDefinition,
+  faRaspberryPi,
+  faChrome,
 } from "@fortawesome/free-brands-svg-icons";
 import { useEffect, useState } from "react";
 
-type Distro = "mac" | "linux" | "windows";
+type Distro = "mac" | "linux" | "windows" | "raspberry-pi" | "chromebook";
 
 type Props = {
   distro: Distro;
+  label?: string;
+};
+
+type GithubAPIResponse = {
+  tag_name: string;
+  assets: {
+    // url: string;
+    // id: number;
+    // node_id: string;
+    // name: string;
+    // label: string;
+    // uploader: {
+    //   login: string;
+    //   id: number;
+    //   node_id: string;
+    // };
+    // content_type: string;
+    // state: string;
+    // size: number;
+    // download_count: number;
+    browser_download_url: string;
+  }[];
 };
 
 function capitalize(str: string): string {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-function getIcon(distro: Distro) {
+function getIcon(distro: Distro): IconDefinition {
   switch (distro) {
     case "mac":
       return faApple;
@@ -24,32 +49,60 @@ function getIcon(distro: Distro) {
       return faLinux;
     case "windows":
       return faWindows;
+    case "raspberry-pi":
+      return faRaspberryPi;
+    case "chromebook":
+      return faChrome;
   }
 }
 
-function getLink(distro: Distro, tagName: string): string {
-  const version = tagName.slice(1);
-  const baseLink =
-    "https://github.com/flojoy-ai/studio/releases/download/v" + version;
-
+function getLink(distro: Distro, response: GithubAPIResponse): string {
+  let link;
   switch (distro) {
     case "mac":
-      return baseLink + "Flojoy-Studio-" + version + "-universal.dmg";
+      link = response.assets.find((a) =>
+        a.browser_download_url.endsWith(".dmg"),
+      );
+      break;
     case "linux":
-      return baseLink + "Flojoy-Studio-" + version + ".AppImage";
+      link = response.assets.find(
+        (a) =>
+          a.browser_download_url.endsWith(".AppImage") &&
+          !a.browser_download_url.includes("arm"),
+      );
+      break;
     case "windows":
-      return baseLink + "Flojoy-Studio-Setup-" + version + ".exe";
+      link = response.assets.find((a) =>
+        a.browser_download_url.endsWith(".exe"),
+      );
+      break;
+    case "chromebook":
+      link = response.assets.find((a) =>
+        a.browser_download_url.endsWith("armv7l.deb"),
+      );
+      break;
+    case "raspberry-pi":
+      link = response.assets.find((a) =>
+        a.browser_download_url.endsWith("arm64.deb"),
+      );
+      break;
   }
+
+  if (link) {
+    return link.browser_download_url;
+  }
+  return "";
 }
-const DownloadButton = ({ distro }: Props) => {
-  const [data, setData] = useState<{ tag_name: string } | undefined>();
+
+const DownloadButton = ({ distro, label }: Props) => {
+  const [data, setData] = useState<GithubAPIResponse | undefined>();
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch(
         "https://api.github.com/repos/flojoy-ai/studio/releases/latest",
       );
-      const jsonData = (await response.json()) as { tag_name: string };
+      const jsonData = (await response.json()) as GithubAPIResponse;
       setData(jsonData);
     };
 
@@ -60,15 +113,32 @@ const DownloadButton = ({ distro }: Props) => {
     return null;
   }
 
+  const link = getLink(distro, data);
+
+  if (link === "") {
+    return (
+      <div className="not-content">
+        <a
+          href={"#"}
+          className="not-content bg-accent-600 dark:bg-accent-200 flex cursor-not-allowed items-center justify-center gap-4 rounded p-4 text-lg text-gray-100 no-underline dark:text-gray-900"
+        >
+          <FontAwesomeIcon className="h-6 w-6" icon={getIcon(distro)} />{" "}
+          Download for {label ?? capitalize(distro)} is currently not available
+          :(
+        </a>
+      </div>
+    );
+  }
+
   return (
     <div className="not-content">
       <a
-        href={getLink(distro, data.tag_name)}
+        href={link}
         target="_blank"
         className="not-content bg-accent-600 dark:bg-accent-200 flex items-center justify-center gap-4 rounded p-4 text-lg text-gray-100 no-underline dark:text-gray-900"
       >
         <FontAwesomeIcon className="h-6 w-6" icon={getIcon(distro)} /> Download
-        for {capitalize(distro)} ({data.tag_name})
+        for {label ?? capitalize(distro)} ({data.tag_name})
       </a>
     </div>
   );
