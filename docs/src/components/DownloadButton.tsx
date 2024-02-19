f14af52a988830e86d65295eca69c8d731cdf655
Joey Yu <joey@jyu.dev>
2024-02-18 21:08:00 -0500

    fix: make download button dynamic

--- docs/src/components/DownloadButton.tsx
+++ docs/src/components/DownloadButton.tsx
@@ -3,20 +3,20 @@ import {
   faApple,
   faWindows,
   faLinux,
-  faPython,
 } from "@fortawesome/free-brands-svg-icons";
+import { useEffect, useState } from "react";
+
+type Distro = "mac" | "linux" | "windows";
 
 type Props = {
-  distro: "mac" | "linux" | "windows";
-  link: string;
-  version: string;
+  distro: Distro;
 };
 
 function capitalize(str: string): string {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-function getIcon(distro: string) {
+function getIcon(distro: Distro) {
   switch (distro) {
     case "mac":
       return faApple;
@@ -24,22 +24,54 @@ function getIcon(distro: string) {
       return faLinux;
     case "windows":
       return faWindows;
-    default:
-      // this casd should never be reached
-      return faPython;
   }
 }
 
-const DownloadButton = ({ distro, link, version }: Props) => {
+function getLink(distro: Distro, tagName: string): string {
+  const version = tagName.slice(1);
+  const baseLink =
+    "https://github.com/flojoy-ai/studio/releases/download/v" +
+    version +
+    "/Flojoy-Studio-Setup-" +
+    version;
+
+  switch (distro) {
+    case "mac":
+      return baseLink + "-universal.dmg";
+    case "linux":
+      return baseLink + ".AppImage";
+    case "windows":
+      return baseLink + ".exe";
+  }
+}
+const DownloadButton = ({ distro }: Props) => {
+  const [data, setData] = useState<{ tag_name: string } | undefined>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://api.github.com/repos/flojoy-ai/studio/releases/latest",
+      );
+      const jsonData = (await response.json()) as { tag_name: string };
+      setData(jsonData);
+    };
+
+    fetchData();
+  }, []);
+
+  if (!data) {
+    return null;
+  }
+
   return (
     <div className="not-content">
       <a
-        href={link}
+        href={getLink(distro, data.tag_name)}
         target="_blank"
         className="not-content bg-accent-600 dark:bg-accent-200 flex items-center justify-center gap-4 rounded p-4 text-lg text-gray-100 no-underline dark:text-gray-900"
       >
         <FontAwesomeIcon className="h-6 w-6" icon={getIcon(distro)} /> Download
-        for {capitalize(distro)} ({version})
+        for {capitalize(distro)} ({data.tag_name})
       </a>
     </div>
   );
