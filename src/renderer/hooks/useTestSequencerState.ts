0a0ca2dec3384c67b58b47566881eba95eb79394
Guillaume <g.thibault@polymtl.ca>
2024-02-20 22:49:51 -0500

    formatting

--- src/renderer/hooks/useTestSequencerState.ts
+++ src/renderer/hooks/useTestSequencerState.ts
@@ -17,7 +17,11 @@ import {
 } from "@/renderer/utils/TestSequenceValidator";
 import useWithPermission from "./useWithPermission";
 
-export const testSequenceTree = atom<TestRootNode>({type: "root", children: [], identifiers: []} as TestRootNode);
+export const testSequenceTree = atom<TestRootNode>({
+  type: "root",
+  children: [],
+  identifiers: [],
+} as TestRootNode);
 
 export const curRun = atom<string[]>([]);
 
