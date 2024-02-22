84e8eecf935c15e48c84dfd7b6076bade32d4923
Guillaume <g.thibault@polymtl.ca>
2024-02-22 10:23:05 -0500

    Create test in Cloud when it doesn't exist

--- captain/utils/test_sequencer/run_test_sequence.py
+++ captain/utils/test_sequencer/run_test_sequence.py
@@ -232,35 +232,25 @@ async def _case_test_upload(node: TestNode, hardware_id, project_id) -> Extract:
     status = node.status
     if status != StatusTypes.pending:
         passed = True if status == StatusTypes.pass_ else False
+        test_name = node.test_name.split("::")[-1]
         try:
             await _stream_result_to_frontend(MsgState.RUNNING, test_id=node.id)
             node.is_saved_to_cloud = False
-            test_name = node.test_name.split("::")[-1]
-            cloud.upload(
-                data=passed,
-                test_id=reverse_id(test_name),
-                hardware_id=hardware_id,
-                name=test_name,
-                passed=passed,
-            )
+            cloud.upload(data=passed, test_id=reverse_id(test_name), hardware_id=hardware_id, name=test_name, passed=passed)
             node.is_saved_to_cloud = True
             logger.info(f"{test_name}: Uploaded to cloud")
-        except KeyError as err:
-            # TODO: Return error to user
-            logger.error(err)
+        except KeyError:
+            cloud.create_test(test_name, project_id, measurement_type="boolean")
+            cloud.upload(data=passed, test_id=reverse_id(test_name), hardware_id=hardware_id, name=test_name, passed=passed)
+            node.is_saved_to_cloud = True
+            logger.info(f"{test_name}: Uploaded to cloud")
         except FlojoyCloudException as err:
             logger.error(err)
             raise FlojoyCloudException("Failed to upload to the cloud.") from err
         finally:
-            await _stream_result_to_frontend(
-                state=MsgState.TEST_DONE,
-                test_id=node.id,
-                result=passed,
-                time_taken=node.completion_time,
-                is_saved_to_cloud=node.is_saved_to_cloud,
-            )
+            await _stream_result_to_frontend(state=MsgState.TEST_DONE, test_id=node.id, result=passed, time_taken=node.completion_time, is_saved_to_cloud=node.is_saved_to_cloud)
     else:
-        logger.error("Should Never Reach Here")
+        raise ValueError("Uploading a pending test is not allowed.")
     return lambda _: None, TestResult(
         node, True if status == StatusTypes.pass_ else False, node.completion_time
     )
