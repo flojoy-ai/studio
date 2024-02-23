e20325b42aa33b51978039047b38408f2cd362e7
Guillaume <g.thibault@polymtl.ca>
2024-02-23 17:25:25 -0500

    Formatting

--- src/renderer/routes/test_sequencer_panel/components/AddWriteConditionalModal.tsx
+++ src/renderer/routes/test_sequencer_panel/components/AddWriteConditionalModal.tsx
@@ -11,12 +11,8 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/renderer/components//ui/dropdown-menu"
-import {
-  Test,
-} from "@/renderer/types/testSequencer";
-
-
+} from "@/renderer/components//ui/dropdown-menu";
+import { Test } from "@/renderer/types/testSequencer";
 
 export const WriteConditionalModal = ({
   isConditionalModalOpen,
@@ -32,7 +28,9 @@ export const WriteConditionalModal = ({
   const [value, setValue] = useState("");
   const { elems } = useTestSequencerState();
   // @ts-ignore
-  const [tests, setTests] = useState(elems.filter(elem => elem.type === "test").map(elem => elem.testName));
+  const [tests, setTests] = useState(
+    elems.filter((elem) => elem.type === "test").map((elem) => elem.testName),
+  );
   function addToValue(v: string) {
     const leader = value[value.length - 1] === " " ? "" : " ";
     setValue(value + leader + v);
@@ -48,29 +46,93 @@ export const WriteConditionalModal = ({
           <div>
             <p className="text-muted-foreground"> Boolean Operation </p>
             <div className="flex pt-2">
-              <Badge variant={"outline"} onClick={() => {addToValue("&");}} className="hover:cursor-pointer flex-initial mr-2">And</Badge>
-              <Badge variant={"outline"} onClick={() => {addToValue("|");}} className="hover:cursor-pointer flex-initial mr-2">Or</Badge>
-              <Badge variant={"outline"} onClick={() => {addToValue("!");}} className="hover:cursor-pointer flex-initial mr-2">Not</Badge>
-              <Badge variant={"outline"} onClick={() => {addToValue("(");}} className="hover:cursor-pointer flex-initial mr-2">(</Badge>
-              <Badge variant={"outline"} onClick={() => {addToValue(")");}} className="hover:cursor-pointer flex-initial mr-2">)</Badge>
+              <Badge
+                variant={"outline"}
+                onClick={() => {
+                  addToValue("&");
+                }}
+                className="mr-2 flex-initial hover:cursor-pointer"
+              >
+                And
+              </Badge>
+              <Badge
+                variant={"outline"}
+                onClick={() => {
+                  addToValue("|");
+                }}
+                className="mr-2 flex-initial hover:cursor-pointer"
+              >
+                Or
+              </Badge>
+              <Badge
+                variant={"outline"}
+                onClick={() => {
+                  addToValue("!");
+                }}
+                className="mr-2 flex-initial hover:cursor-pointer"
+              >
+                Not
+              </Badge>
+              <Badge
+                variant={"outline"}
+                onClick={() => {
+                  addToValue("(");
+                }}
+                className="mr-2 flex-initial hover:cursor-pointer"
+              >
+                (
+              </Badge>
+              <Badge
+                variant={"outline"}
+                onClick={() => {
+                  addToValue(")");
+                }}
+                className="mr-2 flex-initial hover:cursor-pointer"
+              >
+                )
+              </Badge>
             </div>
           </div>
           <div className="pl-10">
-            <p className="text-muted-foreground pb-1"> Test Result </p>
+            <p className="pb-1 text-muted-foreground"> Test Result </p>
             <DropdownMenu>
-            <DropdownMenuTrigger className="inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2">Get Pass/Fail result of a test</DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuLabel>Return the boolean</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              { tests.map((test) => {
-                return <DropdownMenuItem onClick={() => {addToValue(`$${test}`);}}>{test}</DropdownMenuItem>
-              })}
-            </DropdownMenuContent>
-          </DropdownMenu>
+              <DropdownMenuTrigger className="inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2">
+                Get Pass/Fail result of a test
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>Return the boolean</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                {tests.map((test) => {
+                  return (
+                    <DropdownMenuItem
+                      onClick={() => {
+                        addToValue(`$${test}`);
+                      }}
+                    >
+                      {test}
+                    </DropdownMenuItem>
+                  );
+                })}
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
         </div>
-        <Input type="text" placeholder="$file.py::test_name & $file.py::test_name" value={value} onChange={(e) => {setValue(e.target.value)}} />
-        <Button onClick={() => {handleWrite(value); setConditionalModalOpen(false)}}>Submit</Button>
+        <Input
+          type="text"
+          placeholder="$file.py::test_name & $file.py::test_name"
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        />
+        <Button
+          onClick={() => {
+            handleWrite(value);
+            setConditionalModalOpen(false);
+          }}
+        >
+          Submit
+        </Button>
       </DialogContent>
     </Dialog>
   );
