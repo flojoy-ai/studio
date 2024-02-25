329938850381b3b65cf31982b8288aa14788b7da
Gui <guillaume.thibault.98@gmail.com>
2024-02-24 23:58:19 -0500

    Pi performance (#1091)
    * Reduce blur conditional
    
    * Testing agent name
    
    * Testing conditional CSS
    
    * better cond
    
    * Adding same behavior for all modal
    
    * Formating
    
    ---------
    
    Co-authored-by: Dallas Strandell <dstrande@live.ca>

--- src/renderer/components/ui/alert-dialog.tsx
+++ src/renderer/components/ui/alert-dialog.tsx
@@ -14,16 +14,23 @@ const AlertDialogOverlay = React.forwardRef<
   React.ElementRef<typeof AlertDialogPrimitive.Overlay>,
   React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Overlay>
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
->(({ className, children, ...props }, ref) => (
-  <AlertDialogPrimitive.Overlay
-    className={cn(
-      "fixed inset-0 z-50 bg-background/80 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
-      className,
-    )}
-    {...props}
-    ref={ref}
-  />
-));
+>(({ className, children, ...props }, ref) => {
+  const isLinux = navigator.userAgent.toLowerCase().includes("linux");
+  const isArm = navigator.userAgent.toLowerCase().includes("aarch64");
+  const isLinuxArm = isLinux && isArm;
+  const commonClassName =
+    "fixed inset-0 z-50 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0";
+  const additionalClassName = isLinuxArm
+    ? "bg-background/80"
+    : "bg-background/80 backdrop-blur-sm motion-reduce:backdrop-blur-none";
+  return (
+    <AlertDialogPrimitive.Overlay
+      className={cn(commonClassName, additionalClassName, className)}
+      {...props}
+      ref={ref}
+    />
+  );
+});
 AlertDialogOverlay.displayName = AlertDialogPrimitive.Overlay.displayName;
 
 const AlertDialogContent = React.forwardRef<
