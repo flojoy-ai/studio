c7e69d00faecb64e2e83c7fa55e824bff2e9eca7
smahmed776 <xsmahmed776@gmail.com>
2024-03-01 01:37:45 +0000

    feat: show block func name for logic blocks in place of svg

--- src/renderer/components/blocks/logic-block.tsx
+++ src/renderer/components/blocks/logic-block.tsx
@@ -2,11 +2,16 @@ import { memo } from "react";
 import { CustomNodeProps } from "@/renderer/types/node";
 import DefaultBlock from "./default-block";
 import appendSvg from "@/renderer/assets/blocks/append-svg";
+import { BlockLabel } from "../common/block-label";
 
 const LogicBlock = (props: CustomNodeProps) => {
   return (
     <DefaultBlock {...props} variant="accent3">
-      <>{props.data.func === "APPEND" ? appendSvg : undefined}</>
+      {props.data.func === "APPEND" ? (
+        appendSvg
+      ) : (
+        <BlockLabel label={props.data.func} variant="accent3" />
+      )}
     </DefaultBlock>
   );
 };
