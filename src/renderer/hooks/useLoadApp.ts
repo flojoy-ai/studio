0fc6f876a350852f810ff75fe89aedd31ff26d57
JeffDotPng <zhangjeff2014@gmail.com>
2024-03-05 10:38:58 -0500

    parse electron error

--- src/renderer/hooks/useLoadApp.ts
+++ src/renderer/hooks/useLoadApp.ts
@@ -4,10 +4,11 @@ import { Project } from "@/renderer/types/project";
 import { useShallow } from "zustand/react/shallow";
 import { useLoadProject } from "@/renderer/stores/project";
 import { tryParse } from "@/types/result";
-import { fromPromise, ok } from "neverthrow";
+import { fromPromise } from "neverthrow";
 import { toast } from "sonner";
 import { ZodError } from "zod";
 import { fromZodError } from "zod-validation-error";
+import { parseElectronError } from "@/renderer/utils/parse-error";
 
 export const useLoadApp = () => {
   const loadProject = useLoadProject();
@@ -17,16 +18,15 @@ export const useLoadApp = () => {
   );
 
   const openFilePicker = async () => {
-    const res = await fromPromise(
-      window.api.openFilePicker(),
-      (e) => e as Error,
+    const res = await fromPromise(window.api.openFilePicker(), (e) =>
+      parseElectronError((e as Error).message),
     );
     if (res.isErr()) {
-      toast.error("Failed to open file", { description: res.error.message });
+      toast.error("Failed to open file", { description: res.error });
       return;
     }
     if (res.value === undefined) {
-      return ok(undefined);
+      return;
     }
     const { fileContent, filePath } = res.value;
     const loadRes = tryParse(Project)(JSON.parse(fileContent))
