ff9bd6f3a1db6484a30ab22463b978bb8647db32
Joey Yu <joey@jyu.dev>
2024-03-11 16:21:24 -0400

    feat: combobox done

--- src/renderer/components/controls/combobox-node.tsx
+++ src/renderer/components/controls/combobox-node.tsx
@@ -1,7 +1,7 @@
 import { useControlBlock } from "@/renderer/hooks/useControlBlock";
 import { WidgetProps } from "@/renderer/types/control";
-import { ChangeEvent } from "react";
 import { toast } from "sonner";
+import { Combobox } from "@/renderer/components/ui/combobox";
 
 export const ComboboxNode = ({ data }: WidgetProps) => {
   const { block, updateBlockParameter } = useControlBlock(data.blockId);
@@ -12,12 +12,8 @@ export const ComboboxNode = ({ data }: WidgetProps) => {
   const name = block.data.label;
   const paramVal = block.data.ctrls[data.blockParameter].value;
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const res = updateBlockParameter(
-      block.id,
-      data.blockParameter,
-      parseInt(e.target.value, 10),
-    );
+  const handleChange = (val: string) => {
+    const res = updateBlockParameter(block.id, data.blockParameter, val);
     if (res.isErr()) {
       toast.error("Error updating block parameter", {
         description: res.error.message,
@@ -30,15 +26,17 @@ export const ComboboxNode = ({ data }: WidgetProps) => {
       <div className="text-muted-foreground">
         {name} ({data.blockParameter})
       </div>
-      {/* <Input */}
-      {/*   type="range" */}
-      {/*   className="nodrag" */}
-      {/*   min={data.config.min} */}
-      {/*   max={data.config.max} */}
-      {/*   step={data.config.step} */}
-      {/*   value={paramVal as number} */}
-      {/*   onChange={handleChange} */}
-      {/* /> */}
+      <Combobox
+        options={
+          block.data.ctrls[data.blockParameter].options?.map(
+            (v) => v.toString() ?? "",
+          ) ?? []
+        }
+        value={paramVal?.toString() ?? ""}
+        onValueChange={handleChange}
+        valueSelector={(v) => v}
+        displaySelector={(v) => v}
+      />
       <div className="text-xl font-bold">{paramVal}</div>
     </div>
   );
