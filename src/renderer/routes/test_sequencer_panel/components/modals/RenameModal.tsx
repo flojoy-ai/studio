a8b626a76527bbc51d149a44f1c54db5f155e424
Guillaume <g.thibault@polymtl.ca>
2024-04-11 12:57:47 -0400

    chore(sequencer): rename modal - Sequence Name and Sequence Description

--- src/renderer/routes/test_sequencer_panel/components/modals/RenameModal.tsx
+++ src/renderer/routes/test_sequencer_panel/components/modals/RenameModal.tsx
@@ -1,24 +1,28 @@
 import { Button } from "@/renderer/components/ui/button";
 import { Dialog, DialogContent } from "@/renderer/components/ui/dialog";
 import { Input } from "@/renderer/components/ui/input";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 export const RenameModal  = ({
+  title,
   isModalOpen,
   setModalOpen,
   initialName,
   handleSubmit,
   target,
 }: {
+  title: string;
   isModalOpen: boolean;
   setModalOpen: (value: boolean) => void;
   initialName: string;
   handleSubmit: (newName: string) => void;
-  target: string | undefined;
+  target: string;
 }) => {
   const [newName, setNewName] = useState<string>(initialName);
-
+  useEffect(() => {
+    setNewName(initialName);
+  }, [initialName]);
 
   return (
     <Dialog
@@ -26,12 +30,10 @@ export const RenameModal  = ({
       onOpenChange={setModalOpen}
     >
       <DialogContent>
-        <h2 className="text-lg font-bold text-accent1 ">Rename</h2>
-        { target && 
-          <div className="pb-1 text-muted-foreground">
-            <h2>{target}</h2>
-          </div>
-        }
+        <h2 className="text-lg font-bold text-accent1">{title}</h2>
+        <div className="pb-1 text-muted-foreground">
+          <h2>{target}</h2>
+        </div>
         <Input
           placeholder="New Name"
           value={newName}
