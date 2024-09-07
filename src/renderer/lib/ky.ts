ec326e1810ce13fbc1b09b5b6601ae04b16b745d
Piotr Rosiak <51720506+rosiakpiotr@users.noreply.github.com>
2024-09-07 04:21:10 +0200

    Increased timeout on captain requests using ky (#1192)

--- src/renderer/lib/ky.ts
+++ src/renderer/lib/ky.ts
@@ -4,4 +4,5 @@ import ky from "ky";
 export const captain = ky.create({
   prefixUrl: "http://" + env.VITE_BACKEND_HOST + ":" + env.VITE_BACKEND_PORT,
   credentials: "include",
+  timeout: 30000,
 });
